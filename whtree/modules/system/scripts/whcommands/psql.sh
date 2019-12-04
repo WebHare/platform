5815f365c626c75dfa2d8fa28ce0f9f10fa0c3cf
Rob Hulswit <rob@webhare.nl>
2019-12-04 14:20:35 +0100

    Fix using wh psql in interactive mode

--- whtree/modules/system/scripts/whcommands/psql.sh
+++ whtree/modules/system/scripts/whcommands/psql.sh
@@ -6,4 +6,11 @@ if [ -z "$WEBHARE_DBASENAME" ]; then
   exit 1
 fi
 
-exec psql --host $WEBHARE_DATAROOT/postgresql $WEBHARE_DBASENAME -c "SET SESSION CHARACTERISTICS AS TRANSACTION READ WRITE" "$@"
+# -c in arguments? Then make transactions writable
+if [[ " $@ " =~ " -c " ]]; then
+  ARGS=("-c" "SET SESSION CHARACTERISTICS AS TRANSACTION READ WRITE")
+else
+  echo "Opening psql with readonly transactions by default. To start a writable transaction, use"
+  echo "  START TRANSACTION READ WRITE;"
+fi
+exec psql --host $WEBHARE_DATAROOT/postgresql $WEBHARE_DBASENAME "${ARGS[@]}" "$@"
