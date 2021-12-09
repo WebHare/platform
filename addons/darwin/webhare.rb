5279b94002e982fd4e7d421e26f85c91dfc1365d
Herman Borger <>
2021-12-09 11:49:20 +0100

    exception opensearch arm (not yet supported)

--- addons/darwin/webhare.rb
+++ addons/darwin/webhare.rb
@@ -26,5 +26,5 @@ class Webhare < Formula
   depends_on "postgresql@13"
   depends_on "rapidjson"
   depends_on "unixodbc"
-  depends_on "opensearch"
+  depends_on "opensearch" unless Hardware::CPU.arm?
 end
