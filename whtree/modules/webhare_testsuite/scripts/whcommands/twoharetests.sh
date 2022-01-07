6a078e05d78e4c14dd40939743bfc26b747eef0e
Arnold Hendriks <arnold@webhare.nl>
2022-01-07 16:54:58 +0100

    switch twohare to port 80 too

--- whtree/modules/webhare_testsuite/scripts/whcommands/twoharetests.sh
+++ whtree/modules/webhare_testsuite/scripts/whcommands/twoharetests.sh
@@ -34,18 +34,18 @@ HERE
 if [ -n "$TESTENV_CONTAINER1" ]; then
   TH_EXEC1="docker exec $TESTENV_CONTAINER1 wh"
   TH_IP1=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $TESTENV_CONTAINER1)
-  TH_WEBINTERFACE1="http://$TH_IP1:8000"
+  TH_WEBINTERFACE1="http://$TH_IP1"
 fi
 if [ -n "$TESTENV_CONTAINER2" ]; then
   TH_EXEC2="docker exec $TESTENV_CONTAINER2 wh"
   TH_IP2=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $TESTENV_CONTAINER2)
-  TH_WEBINTERFACE2="http://$TH_IP2:8000"
+  TH_WEBINTERFACE2="http://$TH_IP2"
 fi
 
 [ -n "$TH_EXEC1" -a -n "$TH_WEBINTERFACE1" ] || die_help "missing config for container 1"
 [ -n "$TH_EXEC2" -a -n "$TH_WEBINTERFACE2" ] || die_help "missing config for container 2"
 
-# Make sure the testserver knows it's supposed to be listening here (testfw configures in virtualhosting and assumes you want 127.0.0.1:8000, and TH_WEBINTERFACE2 will often be 172.0.2.2:8000)
+# Make sure the testserver knows it's supposed to be listening here (testfw configures in virtualhosting and assumes you want 127.0.0.1, and TH_WEBINTERFACE2 will often be 172.0.2.2)
 $TH_EXEC2 webserver addbackend $TH_WEBINTERFACE2
 
 #$TH_EXEC1 run mod::webhare_testsuite/tests/system/twohare/prepare1.whscr
