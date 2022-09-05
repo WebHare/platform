6a2301c990aab980f9dd2b059510f6c422ee3160
Arnold Hendriks <arnold@webhare.nl>
2022-09-05 21:36:55 +0200

    No need to filter _cat/count, consilio calalog screen has stopped spamming that query

--- whtree/modules/consilio/scripts/whcommands/watchindexrpc.sh
+++ whtree/modules/consilio/scripts/whcommands/watchindexrpc.sh
@@ -18,4 +18,4 @@ if [ -z "$INDEXREGEX" ]; then
   exit 1 #getindexnames should have printed an error
 fi
 
-wh watchlog rpc | grep --line-buffered -E "${INDEXREGEX}" | grep -v --line-buffered _cat/count | wh logreader --format rpc
+wh watchlog rpc | grep --line-buffered -E "${INDEXREGEX}" | wh logreader --format rpc
