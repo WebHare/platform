76efc5ed7ba6e80890c7a737593774ab47d59051
Rob Hulswit <rob@webhare.nl>
2022-11-28 15:23:54 +0100

    Add function to format javascript/typescript file

---
+++ whtree/modules/system/js/internal/tsfmt.ts
@@ -0,0 +1,35 @@
+import * as typescriptFormat from "typescript-formatter";
+
+/// Format of our incoming commands
+interface FormattingCommand {
+  path: string
+  data: string
+  basedir: string
+  tsfmtfile: string
+}
+
+export async function handleFormattingCommand(indata: FormattingCommand) {
+  const options: typescriptFormat.Options = {
+    baseDir: indata.basedir,
+    replace: false,
+    verify: false,
+    tsconfig: true,
+    tsconfigFile: null,
+    tslint: false,
+    tslintFile: null,
+    editorconfig: false,
+    vscode: false,
+    vscodeFile: null,
+    tsfmt: true,
+    tsfmtFile: indata.tsfmtfile
+  };
+
+  const contents = Buffer.from(indata.data, "base64").toString("utf-8");
+  const result = await typescriptFormat.processString(indata.path, contents, options);
+
+  return {
+    path: indata.path,
+    error: "",
+    output: Buffer.from(result.dest, "utf-8").toString("base64")
+  };
+}
