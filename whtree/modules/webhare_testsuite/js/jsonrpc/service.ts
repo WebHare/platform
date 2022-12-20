549dd0972a644e4f112ab1ded31573b9bafb8c31
Arnold Hendriks <arnold@webhare.nl>
2022-12-20 11:26:16 +0100

    Fix jsonrpc error handling

--- whtree/modules/webhare_testsuite/js/jsonrpc/service.ts
+++ whtree/modules/webhare_testsuite/js/jsonrpc/service.ts
@@ -4,4 +4,7 @@ export class TestNoAuthJS implements MyService {
   async validateEmail(langcode: string, emailaddress: string): Promise<boolean> {
     return Boolean(emailaddress.match(/webhare.dev$/));
   }
+  async serverCrash() {
+    throw new Error("this is a server crash");
+  }
 }
--- whtree/modules/webhare_testsuite/tests/wh/webserver/remoting/test_jsonrpc2.ts
+++ whtree/modules/webhare_testsuite/tests/wh/webserver/remoting/test_jsonrpc2.ts
@@ -4,16 +4,29 @@ import { JSONAPICall } from '@mod-system/js/internal/jsonrpccaller';
 
 async function testRPCCaller() {
   await services.ready();
+  const servicedef = { service: "mod::webhare_testsuite/js/jsonrpc/service.ts#TestNoAuthJS" };
+  const request = {
+    url: "",
+    headers: [],
+    body: JSON.stringify({ id: 5, method: "validateEmail", params: ["nl", "pietje@webhare.net"] })
+  };
 
-  const callres = await JSONAPICall({ service: "mod::webhare_testsuite/js/jsonrpc/service.ts#TestNoAuthJS" },
-    {
-      url: "",
-      headers: [],
-      body: JSON.stringify({ id: 5, method: "validateEmail", params: ["nl", "pietje@webhare.net"] })
-
-    });
+  let callres = await JSONAPICall(servicedef, request);
   test.eq(200, callres.statusCode);
   test.eq(false, JSON.parse(callres.body).result);
+  test.eq(null, JSON.parse(callres.body).error, "It must be null if there was no error.");
+
+  request.body = JSON.stringify({ id: 42, method: "noSuchAPI", params: [] });
+  callres = await JSONAPICall(servicedef, request);
+  test.eq(404, callres.statusCode);
+
+  test.eq({ id: 42, error: { code: -32601, message: `Method 'noSuchAPI' not found` }, result: null }, JSON.parse(callres.body));
+
+  request.body = JSON.stringify({ id: 77, method: "serverCrash", params: [] });
+  callres = await JSONAPICall(servicedef, request);
+  test.eq(500, callres.statusCode);
+
+  test.eq({ id: 77, error: { code: -32000, message: `this is a server crash` }, result: null }, JSON.parse(callres.body));
 }
 
 test.run([testRPCCaller]);
