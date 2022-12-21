d08fd6aae0094f525cd66615894ec3e7ab69016f
Arnold Hendriks <arnold@webhare.nl>
2022-12-21 13:55:46 +0100

    Fix missing flag classes

--- whtree/modules/system/js/dompack/src/debug.ts
+++ whtree/modules/system/js/dompack/src/debug.ts
@@ -1,5 +1,7 @@
 import { flags } from "@webhare/env";
 export { flags as debugflags } from "@webhare/env";
 
+//Add all flags as a dompack-debug-- class
+document.documentElement.classList.add(...Object.keys(flags).map(flagname => "dompack--debug-" + flagname));
 if (flags.dompack)
   console.log('[dompack] debugging flags: ' + Object.keys(flags).join(', '));
