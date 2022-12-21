4ca465fe87b5ecf2d049da491db160a88049e788
Rob Hulswit <rob@webhare.nl>
2022-12-21 13:51:15 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- whtree/jssdk/env/src/env.ts
+++ whtree/jssdk/env/src/env.ts
@@ -12,7 +12,7 @@ type DebugFlags = WellKnownFlags & { [key: string]: boolean };
 export const flags: DebugFlags = envsupport.getWHDebugFlags() as DebugFlags;
 
 /** Get the default base URL for RPCs
-    @returns In the browser this returns the current root, in the backend it returns primary WebHare url. Always ends with a slahs */
+    @returns In the browser this returns the current root, in the backend it returns primary WebHare url. Always ends with a slash */
 export function getDefaultRPCBase() {
   return envsupport.getDefaultRPCBase();
 }
