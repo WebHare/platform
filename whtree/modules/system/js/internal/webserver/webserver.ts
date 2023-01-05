f4e22301636afc30ba190c8f328822201bb6ef62
Arnold Hendriks <arnold@webhare.nl>
2023-01-05 23:53:25 +0100

    Setup basic JS based webserver

---
+++ whtree/modules/system/js/internal/webserver/webserver.ts
@@ -0,0 +1,73 @@
+import { coreWebHareRouter } from '@webhare/router/src/corerouter';
+import { WebRequest, WebResponse } from '@webhare/router/src/router';
+import * as http from 'node:http';
+import * as https from 'node:https';
+import { Configuration, Port, initialconfig } from "./webconfig";
+
+class WebServer {
+  config: Configuration;
+  ports: Array<{
+    server: http.Server | https.Server;
+  }> = [];
+
+  constructor() {
+    this.config = initialconfig;
+  }
+
+  async onRequest(port: Port, req: http.IncomingMessage, res: http.ServerResponse) {
+    try {
+      if (!req.method || !req.url)
+        throw new Error("Incomplete request?");
+
+      console.log(`${req.method} ${req.headers.host} ${req.url}`);
+      //FIXME verify whether host makes sense given the incoming port (ie virtualhost or force to IP ?)
+      const finalurl = (port.privatekey ? "https://" : "http://") + req.headers.host + req.url;
+
+      //Translate nodejs request to our Router stuff
+      const webreq = new WebRequest(req.method, finalurl);
+      const webresp = new WebResponse;
+      //TODO timeouts, separate VMs, whatever a Robust webserver Truly Requires
+      await coreWebHareRouter(webreq, webresp);
+      //TODO freeze the WebResponse, log errors if any modification still occurs after we're supposedly done
+      const final = await webresp.getFinalPage();
+      res.write(final.body);
+      res.end();
+    } catch (e) {
+      res.statusCode = 500;
+      res.end();
+    }
+  }
+
+  reconfigure(config: Configuration) {
+    for (const port of config.ports) {
+      const serveroptions: https.ServerOptions = {};
+      if (port.privatekey) {
+        serveroptions.key = port.privatekey;
+        serveroptions.cert = port.certificatechain;
+      }
+
+      const callback = (req: http.IncomingMessage, res: http.ServerResponse) => this.onRequest(port, req, res);
+      const server = port.privatekey ? https.createServer(serveroptions, callback)
+        : http.createServer(serveroptions, callback);
+      server.on('error', e => console.log("Server error", e)); //TODO deal with EADDRINUSE for listen falures
+      server.listen(port.port, port.ip);
+
+      this.ports.push({ server });
+    }
+  }
+
+  unref() {
+    this.ports.forEach(_ => _.server.unref());
+  }
+
+  close() {
+    this.ports.forEach(_ => _.server.close());
+    this.ports = [];
+  }
+}
+
+export async function launch(config: Configuration) {
+  const ws = new WebServer();
+  ws.reconfigure(config);
+  return ws;
+}
---
+++ whtree/modules/webhare_testsuite/tests/system/nodejs/test_jswebserver.ts
@@ -0,0 +1,54 @@
+import * as test from "@webhare/test";
+import * as whfs from "@webhare/whfs";
+import * as services from "@webhare/services";
+import { Configuration } from "@mod-system/js/internal/webserver/webconfig";
+import * as webserver from "@mod-system/js/internal/webserver/webserver";
+import * as net from "node:net";
+
+async function getAvailableServerPort() {
+  //have the OS select a free port
+  const server = net.createServer({});
+  const listenwaiter = new Promise(resolve => server.once("listening", resolve));
+  server.listen({ port: 0, host: "127.0.0.1" });
+  await listenwaiter;
+
+  const port = (server.address() as net.AddressInfo).port;
+  await new Promise(resolve => server.close(resolve));
+  if (!port)
+    throw new Error(`Failed to find an available port`);
+  return port;
+}
+
+
+async function testOurWebserver() {
+  await services.ready();
+
+  const port = await getAvailableServerPort();
+  const config = (await services.callHareScript("mod::system/lib/internal/webserver/config.whlib#DownloadWebserverConfig", [], { openPrimary: true })) as Configuration;
+  config.ports = [
+    {
+      port: port,
+      certificatechain: "",
+      ciphersuite: "",
+      id: -1,
+      ip: "127.0.0.1",
+      istrustedport: true,
+      keypair: 0,
+      privatekey: "",
+      virtualhost: true
+    }
+  ];
+
+  const ws = await webserver.launch(config);
+  ws.unref();
+
+  const markdowndoc = await whfs.openFile("site::webhare_testsuite.testsite/testpages/markdownpage");
+  const markdowndocurl = new URL(markdowndoc.link);
+
+  const response = await (await fetch("http://127.0.0.1:" + port + markdowndocurl.pathname, { headers: { host: markdowndocurl.host } })).text();
+  test.eqMatch(/h2.*>.*Markdown file</, response);
+
+  ws.close(); //without explicitly closing the servers we linger for 4 seconds if we did a request ... but not sure why.
+}
+
+test.run([testOurWebserver]);
