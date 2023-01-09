018d8c43407ce0c95ba2e2d69f59e1a08fe32a0f
Rob Hulswit <rob@webhare.nl>
2023-01-09 14:50:22 +0100

    Improve printing of lingering resources when node process doesn't exit after tests finish

--- whtree/jssdk/test/src/test.ts
+++ whtree/jssdk/test/src/test.ts
@@ -1,3 +1,5 @@
+import { dumpActiveIPCMessagePorts } from '@mod-system/js/internal/whmanager/transport';
+
 // Want more than the default 10 stack frames in errors
 Error.stackTraceLimit = 25;
 
@@ -31,6 +33,7 @@ function dumpHandlesAndRequests() {
   const p: ProcessUndocumented = process as unknown as ProcessUndocumented;
   console.error('\nTest process is not shutting down after tests, there are probably still active handles or requests');
   console.error('Active resource types:', p.getActiveResourcesInfo());
+  dumpActiveIPCMessagePorts();
   process.exit(1);
 }
 
@@ -38,14 +41,18 @@ export async function run(tests: Array<() => unknown>, options?: object) {
   //TODO register once in case we're loaded as a module ?
   process.on("exit", onTestExit);
 
-  for (let idx = 0; idx < tests.length; ++idx) {
-    const result = await tests[idx]();
-    if (typeof result !== "undefined") {
-      // this may be accidentally passing a non test-function eg testing `() => myTest` instead of `() => myTest()`
-      console.error(`Unexpected return value from test #${idx} (${tests[idx].name}). Make sure tests never return anything`);
-      break;
+  try {
+    for (let idx = 0; idx < tests.length; ++idx) {
+      const result = await tests[idx]();
+      if (typeof result !== "undefined") {
+        // this may be accidentally passing a non test-function eg testing `() => myTest` instead of `() => myTest()`
+        console.error(`Unexpected return value from test #${idx} (${tests[idx].name}). Make sure tests never return anything`);
+        break;
+      }
     }
+    testscompleted = true;
+  } finally {
+    // Dump all resources keeping the script alive after 5 seconds after finishing the tests
+    setTimeout(() => dumpHandlesAndRequests(), 5000).unref();
   }
-  testscompleted = true;
-  setTimeout(() => dumpHandlesAndRequests(), 60000).unref();
 }
