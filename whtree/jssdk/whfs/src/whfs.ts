a9b7ea5d7ae108dead76cdb4115873039369e336
Arnold Hendriks <arnold@webhare.nl>
2023-01-17 23:39:28 +0100

    split off db structure

--- whtree/jssdk/whfs/src/whfs.ts
+++ whtree/jssdk/whfs/src/whfs.ts
@@ -1,54 +1,6 @@
 import { sql } from "@webhare/whdb";
 import * as siteprofiles from "./siteprofiles";
-
-interface SiteRow {
-  id: number;
-  cdnbaseurl: string;
-  description: string;
-  isversioned: boolean;
-  locked: boolean;
-  lockreason: string;
-  name: string;
-  outputfolder: string;
-  outputweb: number;
-  versioningpolicy: string;
-
-  //manually added
-  webroot: string;
-}
-
-interface FsObjectRow {
-  id: number;
-  // creationdate: timestamp;
-  // data: webhare_internal.webhare_blob;
-  description: string;
-  errordata: string;
-  externallink: string;
-  filelink: number;
-  // firstpublishdate: timestamp;
-  indexdoc: number;
-  isfolder: boolean;
-  ispinned: boolean;
-  keywords: string;
-  // lastpublishdate: timestamp;
-  lastpublishsize: number;
-  lastpublishtime: number;
-  // modificationdate: timestamp;
-  modifiedby: number;
-  name: string;
-  ordering: number;
-  parent: number;
-  published: number;
-  scandata: string;
-  title: string;
-  type: number;
-
-  //manually added
-  link: string;
-  fullpath: string;
-  whfspath: string;
-  parentsite: number;
-}
+import { FsObjectRow, SiteRow } from "./dbschema";
 
 class WHFSObject {
   protected readonly dbrecord: FsObjectRow;
