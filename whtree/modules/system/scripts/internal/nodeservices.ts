fe2638b53214aed8f465e571e97c970e7286f149
Arnold Hendriks <arnold@webhare.nl>
2023-01-31 23:31:36 +0100

    Fix nodeservice shutting down immediately

--- whtree/modules/system/scripts/internal/nodeservices.ts
+++ whtree/modules/system/scripts/internal/nodeservices.ts
@@ -55,6 +55,9 @@ async function main() {
   //Launch all services in parallel
   for (const service of backendservices)
     launchService(service); //we don't await this, we just launch it and let it run in the background
+
+  //TODO remove this as soon as *all* WebHares have something to do. A bare WebHare at this moment doesn't have any backendservices yet
+  await new Promise(resolve => setTimeout(resolve, 86400 * 1000));
 }
 
 hmr.activate();
