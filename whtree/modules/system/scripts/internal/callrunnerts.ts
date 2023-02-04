443cd368746c6b1cdf46567239778d7bf59a61f5
Rob Hulswit <rob@webhare.nl>
2023-02-04 16:36:57 +0100

    Rewrite callasync to use whmanager bridge

--- whtree/modules/system/scripts/internal/callrunnerts.ts
+++ whtree/modules/system/scripts/internal/callrunnerts.ts
@@ -1,12 +1,18 @@
-import WHBridge, { IPCLink } from '@mod-system/js/internal/bridge';
+import bridge, { IPCLinkType } from "@mod-system/js/internal/whmanager/bridge";
 import * as resourcetools from '@mod-system/js/internal/resourcetools';
 
 interface InvokeTask {
   cmd: "invoke";
-  id: number;
   func: string;
   args: unknown[];
 }
+
+interface InvokeResponse {
+  cmd: "response";
+  value: string;
+}
+
+type CallRunnerLinkType = IPCLinkType<InvokeResponse, InvokeTask>;
 
 async function runInvoke(task: InvokeTask): Promise<unknown> {
   return await (await resourcetools.loadJSFunction(task.func))(...task.args);
@@ -14,35 +20,26 @@ async function runInvoke(task: InvokeTask): Promise<unknown> {
 
 function connectIPC(name: string) {
   try {
-    const link = new IPCLink;
+    const link = bridge.connect<CallRunnerLinkType>(name, { global: true });
     link.on("message", async (msg) => {
-      const task = msg.message as InvokeTask;
-      const msgid = msg.msgid;
-      switch (task.cmd) {
+      switch (msg.message.cmd) {
         case "invoke": {
           try {
-            const value = await runInvoke(task);
+            let value = await runInvoke(msg.message);
+            if (value === undefined)
+              value = false;
             link.send({
               cmd: "response",
-              id: task.id,
               value: JSON.stringify(value)
-            }, msgid);
+            }, msg.msgid);
           } catch (e: unknown) {
-            link.send({
-              cmd: "response",
-              id: task.id,
-              error: {
-                type: "exception",
-                what: (e as Error).message || "Unknown error",
-                trace: WHBridge.getStructuredTrace(e as Error)
-              }
-            }, msgid);
+            link.sendException(e as Error, msg.msgid);
           }
         }
       }
     });
     link.on("close", () => process.exit()); //FIXME are we sure this is fired? it's not tested yet at least!
-    link.connect(process.argv[2], true);
+    link.activate();
   } catch (e) {
     console.error(`got error: ${e}`);
   }
