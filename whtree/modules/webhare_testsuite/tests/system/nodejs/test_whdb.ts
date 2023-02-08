35b3b316816b320c1dd13b40d2a154da23e5b890
Arnold Hendriks <arnold@webhare.nl>
2023-02-08 16:57:02 +0100

    Implements WHDB finish  handlers

--- whtree/modules/webhare_testsuite/tests/system/nodejs/test_whdb.ts
+++ whtree/modules/webhare_testsuite/tests/system/nodejs/test_whdb.ts
@@ -1,5 +1,7 @@
+import { BackendEvent, BackendEventSubscription, subscribe } from "@webhare/services";
 import * as test from "@webhare/test";
-import { db, beginWork, commitWork, rollbackWork } from "@webhare/whdb";
+import { sleep } from "@webhare/std";
+import { db, beginWork, commitWork, rollbackWork, onFinishWork, broadcastOnCommit, isWorkOpen } from "@webhare/whdb";
 import type { WebHareTestsuiteDB } from "wh:internal/generated/whdb/webhare_testsuite";
 
 async function testQueries() {
@@ -20,4 +22,104 @@ async function testQueries() {
   await test.throws(/already been closed/, () => rollbackWork());
 }
 
-test.run([testQueries]);
+async function testFinishHandlers() {
+  const handlerresult: string[] = [];
+  const allevents: BackendEvent[] = [];
+
+  async function onEvents(events: BackendEvent[], subscription: BackendEventSubscription) {
+    allevents.push(...events);
+  }
+
+  const push_result_callback = {
+    onCommit: async () => { await sleep(20); handlerresult.push("commit"); },
+    onRollback: async () => { await sleep(20); handlerresult.push("rollback"); },
+    onBeforeCommit: async () => { await sleep(20); handlerresult.push("beforecommit"); }
+  };
+
+  const klaversymbol = Symbol("klaver");
+  await subscribe("webhare_testsuite:worktest.*", onEvents);
+
+  test.throws(/work has already been closed/i, () => onFinishWork(push_result_callback));
+
+  //Test that tag reuse properly ignores the second provided commithandler, test that callbacks are invoked to register handlers
+  await beginWork();
+  test.eq(push_result_callback, onFinishWork(push_result_callback, { uniqueTag: klaversymbol }));
+  test.eq(push_result_callback, onFinishWork({ onCommit: () => { throw new Error("should not be invoked!"); } }, { uniqueTag: klaversymbol }));
+  //Register it twice, should dedupe
+  broadcastOnCommit("webhare_testsuite:worktest.1");
+  broadcastOnCommit("webhare_testsuite:worktest.1");
+  broadcastOnCommit("webhare_testsuite:worktest.2");
+  onFinishWork(() => ({ onCommit: () => handlerresult.push('first') }));
+
+  await commitWork();
+  test.eq(["beforecommit", "first", "commit"], handlerresult);
+  await test.wait(() => allevents.length >= 2);
+
+  //ensure both expected events are there
+  test.assert(allevents.find(_ => _.name === "webhare_testsuite:worktest.1"));
+  test.assert(allevents.find(_ => _.name === "webhare_testsuite:worktest.2"));
+  test.eq(2, allevents.length);
+
+  //clear event logs and prepare to test explicit rollback
+  handlerresult.splice(0, handlerresult.length);
+  allevents.splice(0, allevents.length);
+
+  await beginWork();
+  broadcastOnCommit("webhare_testsuite:worktest.3");
+  onFinishWork(push_result_callback);
+  await rollbackWork();
+
+  await beginWork();
+  broadcastOnCommit("webhare_testsuite:worktest.4"); //extra event so we can see if whether any broadcasts should have been processed
+  await commitWork();
+
+  test.eq(["rollback"], handlerresult);
+  await test.wait(() => allevents.length >= 1);
+  test.assert(allevents.find(_ => _.name === "webhare_testsuite:worktest.4"));
+  test.eq(1, allevents.length);
+
+  //clear event logs and prepare to test failed commit
+  handlerresult.splice(0, handlerresult.length);
+  allevents.splice(0, allevents.length);
+
+  await beginWork();
+  broadcastOnCommit("webhare_testsuite:worktest.5");
+  await db<WebHareTestsuiteDB>().insertInto("webhare_testsuite.rights_tree").values({ parent: -100, name: "test_whdb__dummyinsert" }).execute();
+  onFinishWork(push_result_callback);
+  await test.throws(/violates foreign key constraint/, () => commitWork());
+  test.eq(false, isWorkOpen());
+
+  await beginWork();
+  broadcastOnCommit("webhare_testsuite:worktest.6"); //extra event so we can see if whether any broadcasts should have been processed
+  await commitWork();
+
+  test.eq(["beforecommit"], handlerresult);
+  await test.wait(() => allevents.length >= 1);
+  test.assert(allevents.find(_ => _.name === "webhare_testsuite:worktest.6"));
+  test.eq(1, allevents.length);
+
+  //clear event logs and prepare to test failed precommits. these still turn into a visible rollback
+  handlerresult.splice(0, handlerresult.length);
+  allevents.splice(0, allevents.length);
+
+  await beginWork();
+  broadcastOnCommit("webhare_testsuite:worktest.7");
+  onFinishWork({ ...push_result_callback, onBeforeCommit: () => { throw new Error("beforecommit failed"); } });
+  await test.throws(/beforecommit failed/, () => commitWork());
+  test.eq(false, isWorkOpen());
+
+  await beginWork();
+  broadcastOnCommit("webhare_testsuite:worktest.8"); //extra event so we can see if whether any broadcasts should have been processed
+  await commitWork();
+
+  test.eq(["rollback"], handlerresult);
+  await test.wait(() => allevents.length >= 1);
+  test.assert(allevents.find(_ => _.name === "webhare_testsuite:worktest.8"));
+  test.eq(1, allevents.length);
+
+}
+
+test.run([
+  testQueries,
+  testFinishHandlers
+]);
