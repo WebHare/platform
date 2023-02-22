76cbaefcf85a60c3c83d971b674cf5d6efe46e21
Arnold Hendriks <arnold@webhare.nl>
2023-02-22 13:15:23 +0100

    Show screenshot on chat tab

--- whtree/modules/publisher/web/backend/feedback/issues.js
+++ whtree/modules/publisher/web/backend/feedback/issues.js
@@ -1,2 +1,10 @@
 // Scroll to bottom upon page load
 window.addEventListener("load", () => document.body.lastChild.scrollIntoView({ block: "end" }));
+
+// Move screenshotdata into a frame
+const screenshotholder = document.querySelector("#screenshotholder");
+if(screenshotholder) {
+  const frame = document.createElement("iframe");
+  frame.srcdoc = atob(screenshotholder.querySelector("template").content.textContent);
+  screenshotholder.replaceChildren(frame);
+}
