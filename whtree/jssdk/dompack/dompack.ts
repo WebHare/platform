935f8bf4e1a0ec8c8236f1007ff1fad3ea790e18
Arnold Hendriks <arnold@webhare.nl>
2023-02-22 09:16:46 +0100

    Export more non-deprecated dompack from @webhare/dompack

--- whtree/jssdk/dompack/dompack.ts
+++ whtree/jssdk/dompack/dompack.ts
@@ -1,4 +1,9 @@
 /* This is our public api */
 
+export { flagUIBusy } from '../../modules/system/js/dompack/src/busy';
+export { dispatchCustomEvent, dispatchDomEvent, fireModifiedEvents, changeValue, stop } from '../../modules/system/js/dompack/src/events';
 export { qS, qR, qSA } from './impl/tree';
+export { isDomReady, onDomReady } from '../../modules/system/js/dompack/src/tree';
+export { create, jsxcreate, jsxfragment } from '../../modules/system/js/dompack/src/create';
 export { focus, register, registerMissed } from '../../modules/system/js/dompack/src/components';
+export { getLocal, setLocal, getSession, setSession } from '../../modules/system/js/dompack/extra/storage';
