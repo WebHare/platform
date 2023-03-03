73614883d4930ab5cfbd9e0302472cf0ba99c7fd
Arnold Hendriks <arnold@webhare.nl>
2023-03-03 13:21:50 +0100

    reformat to typescript

--- whtree/modules/webhare_testsuite/tests/publisher/forms/testimgedit.ts
+++ whtree/modules/webhare_testsuite/tests/publisher/forms/testimgedit.ts
@@ -3,70 +3,74 @@ import * as preload from 'dompack/extra/preload';
 import { readBackgroundUrl } from '@mod-publisher/js/forms/fields/imgedit';
 
 test.registerTests(
-  [ async function()
-    {
+  [
+    async function () {
       await test.invoke('mod::webhare_testsuite/lib/internal/testsite.whlib#SnoozeRateLimits');
       await test.load(test.getTestSiteRoot() + 'testpages/formtest/?rtd=1&store=testrte');
-    }
-  , 'Reset image'
-  , async function()
-    {
-      test.prepareUpload( [{ url: '/tollium_todd.res/webhare_testsuite/tollium/portrait_8.jpg'
-                           , filename: 'portrait_8.jpg'
-                           }]);
+    },
+    'Reset image',
+    async function () {
+      test.prepareUpload([
+        {
+          url: '/tollium_todd.res/webhare_testsuite/tollium/portrait_8.jpg',
+          filename: 'portrait_8.jpg'
+        }
+      ]);
       test.assert(!test.qS('[data-wh-form-group-for=img]').classList.contains("wh-form--uploading"));
       test.qS('#rtdtest-img').click();
 
       //click handler processing is async, so give it a chance to run
-      await test.wait( () => test.qS('[data-wh-form-group-for=img]').classList.contains("wh-form--uploading"));
+      await test.wait(() => test.qS('[data-wh-form-group-for=img]').classList.contains("wh-form--uploading"));
       await test.wait('ui');
-    }
-  , { test: async function()
-      {
+    },
+    {
+      test: async function () {
         test.assert(!test.qS('[data-wh-form-group-for=img]').classList.contains("wh-form--uploading"));
-        let img = test.qS('#rtdtest-img .wh-form__imgeditimg');
+        const img = test.qS('#rtdtest-img .wh-form__imgeditimg');
         test.assert(img, 'no image present');
         test.assert(test.qS('#rtdtest-img .wh-form__imgeditdelete'), 'no delete button');
         test.assert(test.qS('#rtdtest-img').classList.contains('wh-form__imgedit--hasimage'));
-        let imgurl = readBackgroundUrl(img);
+        const imgurl = readBackgroundUrl(img);
         test.assert(imgurl, 'no image url');
-        let imginfo = await preload.promiseImage(imgurl);
+        const imginfo = await preload.promiseImage(imgurl);
         test.eq(450, Math.floor(imginfo.width)); //should be portrait even though we uploaded landscape
         test.click('#submitbutton');
-      }
-    , waits:['ui']
-    }
-  , { test: function()
-      {
-        let serverreponse = JSON.parse(test.qS('#rtdformresponse').textContent);
+      },
+      waits: ['ui']
+    },
+    {
+      test: function () {
+        const serverreponse = JSON.parse(test.qS('#rtdformresponse').textContent);
         test.eq('.jpg', serverreponse.img.extension);
         test.eq(600, serverreponse.img.width);
         test.eq(90, serverreponse.img.rotation);
       }
-    }
-  , { loadpage: test.getTestSiteRoot() + 'testpages/formtest/?rtd=1&store=testrte'
-    }
-  , { name: 'Verify reloaded image'
-    , test: async function()
-      {
+    },
+    {
+      loadpage: test.getTestSiteRoot() + 'testpages/formtest/?rtd=1&store=testrte'
+    },
+    {
+      name: 'Verify reloaded image',
+      test: async function () {
         //wait for image to load
-        let img = test.qS('#rtdtest-img .wh-form__imgeditimg');
+        const img = test.qS('#rtdtest-img .wh-form__imgeditimg');
         test.assert(img, 'no image present #2');
         test.assert(test.qS('#rtdtest-img').classList.contains('wh-form__imgedit--hasimage'));
-        let imgurl = readBackgroundUrl(img);
+        const imgurl = readBackgroundUrl(img);
         test.assert(imgurl, 'no image url');
-        let imginfo = await preload.promiseImage(imgurl);
+        const imginfo = await preload.promiseImage(imgurl);
         test.eq(450, Math.floor(imginfo.width)); //should be portrait even though we uploaded landscape
         //save loaded image again
         test.click('#submitbutton');
-      }
-    , waits:['ui']
-    }
-  , { loadpage: test.getTestSiteRoot() + 'testpages/formtest/?rtd=1&store=testrte'
-    }
-  , { name: 'Verify re-reloaded image'
-    , test: async function()
-      {
+      },
+      waits: ['ui']
+    },
+    {
+      loadpage: test.getTestSiteRoot() + 'testpages/formtest/?rtd=1&store=testrte'
+    },
+    {
+      name: 'Verify re-reloaded image',
+      test: async function () {
         //wait for image to load
         let img = test.qS('#rtdtest-img .wh-form__imgeditimg');
         test.assert(img, 'no image present #3');
@@ -89,36 +93,39 @@ test.registerTests(
         test.assert(!test.qS('#rtdtest-img .wh-form__imgeditdelete'), 'delete button still present');
         test.assert(!test.qS('#rtdtest-img').classList.contains('wh-form__imgedit--hasimage'));
       }
-    }
-  , { loadpage: test.getTestSiteRoot() + 'testpages/formtest/?rtd=1&store=testrte&imgrequired=1'
-    }
-  , { name: 'Test error handling'
-    , test: async function()
-      {
+    },
+    {
+      loadpage: test.getTestSiteRoot() + 'testpages/formtest/?rtd=1&store=testrte&imgrequired=1'
+    },
+    {
+      name: 'Test error handling',
+      test: async function () {
         test.qS('#rtdtest-img').focus();
         test.click('.wh-form__imgeditdelete'); //kill image
         test.click('#submitbutton'); //image should be removed. submit
         await test.wait('ui');
 
-        let imggroup = test.qS('#rtdtest-img').closest('.wh-form__fieldgroup');
+        const imggroup = test.qS('#rtdtest-img').closest('.wh-form__fieldgroup');
         test.assert(imggroup.classList.contains('wh-form__fieldgroup--error'), 'field should be in error');
 
         //upload an image
-        test.prepareUpload( [{ url: '/tollium_todd.res/webhare_testsuite/tollium/portrait_8.jpg'
-                             , filename: 'portrait_8.jpg'
-                             }]);
+        test.prepareUpload([
+          {
+            url: '/tollium_todd.res/webhare_testsuite/tollium/portrait_8.jpg',
+            filename: 'portrait_8.jpg'
+          }
+        ]);
         test.qS('#rtdtest-img').click();
         await test.wait('ui');
 
         test.assert(!imggroup.classList.contains('wh-form__fieldgroup--error'), 'field should be out of error');
       }
-    }
+    },
 
-  , { loadpage: test.getTestSiteRoot() + 'testpages/formtest/?rtd=1&store=testrte&disabled=1' }
+    { loadpage: test.getTestSiteRoot() + 'testpages/formtest/?rtd=1&store=testrte&disabled=1' },
 
-  , 'Initially disabled imgedit'
-  , async function()
-    {
+    'Initially disabled imgedit',
+    async function () {
       test.assert(test.qS('[data-wh-form-group-for=img] .wh-form__imgedit[data-wh-form-disabled]'));
     }
   ]);
