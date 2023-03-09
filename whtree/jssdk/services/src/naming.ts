eb8b67847b2d86877251c6c79fe930f0d5ca2e97
Rob Hulswit <rob@webhare.nl>
2023-03-09 14:22:37 +0100

    Add splitFileReference

--- whtree/jssdk/services/src/naming.ts
+++ whtree/jssdk/services/src/naming.ts
@@ -22,3 +22,11 @@ export function isValidBackendEventName(eventname: string) {
   const split = splitModuleScopedName(eventname);
   return split && /^[a-z][a-z0-9.]*$/.test(split[1]);
 }
+
+/** Splits a reference in the form file#name-within-file */
+export function splitFileReference(ref: string): { file: string; name: string } | null {
+  const parts = ref.split("#");
+  if (parts.length !== 2)
+    return null;
+  return { file: parts[0], name: parts[1] };
+}
