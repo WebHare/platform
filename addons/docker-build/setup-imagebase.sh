0db6145627f54bb5897e46e0d5437996b6459faf
Arnold Hendriks <arnold@webhare.nl>
2023-03-22 11:44:14 +0100

    Drop ODBC docs (we intend to deprecate it), remove mariadb odbc driver

--- addons/docker-build/setup-imagebase.sh
+++ addons/docker-build/setup-imagebase.sh
@@ -37,7 +37,6 @@ echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' > /etc/apt/apt.conf.d/
 # Chrome headless sometimes crashes if fonts are missing. Not sure why, but see https://bugs.chromium.org/p/chromium/issues/detail?id=695212
 # Note that in the end, this still didn't seem to fix it, so perhaps fonts-open-sans can go away again
 ( curl -sL https://deb.nodesource.com/setup_18.x | bash - )
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8 #MariaDB key
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 7FCC7D46ACCC4CF8 #Postgres key
 add-apt-repository 'deb http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main'
 # ( curl -sL https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - )
@@ -108,7 +107,6 @@ PACKAGES="ccache
     locales-all
     make
     nodejs
-    odbc-mariadb
     openssh-server
     openssl
     libpixman-1-dev
@@ -169,13 +167,6 @@ if (( CHROMEMAJOR < REQUIRECHROMEMAJOR )) ; then
   echo "We picked up a too old Chrome version. Required $REQUIRECHROMEMAJOR got $CHROMEMAJOR ($CHROMEVERSION)"
   exit 1
 fi
-
-# Setup odbc
-cat >> /etc/odbcinst.ini << HERE
-[MariaDB]
-Driver = /usr/lib/x86_64-linux-gnu/odbc/libmaodbc.so
-Description = MariaDB ODBC Connector
-HERE
 
 # Cleanup (?)
 apt-get -qy autoremove
