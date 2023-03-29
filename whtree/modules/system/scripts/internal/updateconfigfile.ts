fba6e5654ac618c300067c893e779c0ca579ba72
Arnold Hendriks <arnold@webhare.nl>
2023-03-29 15:53:33 +0200

    Add --verbose option to updategeneratedfiles (to be extended/supported further but at least now I know it's WRD that's the bottleneck)

--- whtree/modules/system/scripts/internal/updateconfigfile.ts
+++ whtree/modules/system/scripts/internal/updateconfigfile.ts
@@ -1,3 +1,4 @@
+// TODO we need to add --verbose support (but let's check if we can't fold ourselves back into updategeneratedfiles.ts first ?
 import { updateWebHareConfigFile } from "../../js/internal/generation/gen_config";
 
 const withdb = !process.argv.includes("--nodb");
