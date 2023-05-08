d0d5d240cc80fd43d0d1e6b57284fbe704c937a0
Rob Hulswit <rob@webhare.nl>
2023-05-08 11:20:42 +0200

    Cache co-vms

--- whtree/jssdk/services/src/co-hsvm.ts
+++ whtree/jssdk/services/src/co-hsvm.ts
@@ -34,20 +34,72 @@ class CoVM {
   }
 }
 
+const cache = new Array<CoVM>;
+
+const covmsymbol = Symbol("WHCoVM");
+
+let activecovms = 0, maxactivecovms = 0, silentintervals = 0;
+let startedcovm = false;
+let cleanupinterval: NodeJS.Timer | undefined;
+
 async function promiseVM() {
   const vm = await openHSVM();
   const database = vm.loadlib("mod::system/lib/database.whlib");
   const primary = await database.openPrimary() as HSVMObject;
   return new CoVM(vm, primary);
 }
-const covmsymbol = Symbol("WHCoVM");
+
+function cleanupCoVMCache() {
+  if (startedcovm)
+    silentintervals = 0;
+  else
+    ++silentintervals;
+
+  /* Keep enough covms in cache to scale up to maxactivecovms of the last interval, plus 4
+     extra. Unless last covm was allocated 5 minutes ago, then clear the cache.
+  */
+  const keepcached = silentintervals < 10 ? 4 : 0;
+  const todelete = cache.splice(maxactivecovms - activecovms + keepcached);
+
+  for (const elt of todelete) {
+    elt.close();
+  }
+  maxactivecovms = activecovms;
+  startedcovm = false;
+}
 
 async function getCurrentCoVM() {
+  if (!cleanupinterval) {
+    cleanupinterval = setInterval(cleanupCoVMCache, 30 * 1000);
+    cleanupinterval.unref();
+  }
+
   return ensureScopedResource(covmsymbol, (context) => {
-    const retval = promiseVM();
+    const covmFromCache = cache.shift();
+    const retval = covmFromCache ? Promise.resolve(covmFromCache) : promiseVM();
+    startedcovm = true;
+    ++activecovms;
+    //console.log(`activecovms: ${activecovms}, testing ${testing}, cache: ${cache.length}: ${covmFromCache ? "from cache" : "new"}`);
+    if (maxactivecovms < activecovms)
+      maxactivecovms = activecovms;
     const cbid = context.on("close", () => {
+      --activecovms;
+
       context.off(cbid);
-      retval.then(vm => vm.close());
+      retval.then(async covm => {
+        try {
+          if (await covm.vm.loadlib("mod::system/lib/internal/jshelpers.whlib").TestCoVMReusable() === true) {
+            /* round-robin through the first 4 cache items, then use fifo for the rest of the items - so the last
+               items in the cache (that will be discarded first) are used the least
+            */
+            cache.splice(4, 0, covm);
+          } else {
+            covm.close();
+          }
+        } catch (e) {
+          covm.close();
+        }
+      });
     });
     return retval;
   });
