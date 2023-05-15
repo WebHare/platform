9cee6e1c9a96f0d83c00ecbdb833587e3d55a7de
Arnold Hendriks <arnold@webhare.nl>
2023-05-16 00:10:21 +0200

    merge waitperiod stuff into datetime.ts

--- whtree/jssdk/std/datetime.ts
+++ whtree/jssdk/std/datetime.ts
@@ -1,3 +1,6 @@
+/** A relative (up to a week) or absolute wait period. Use 0 for 'polling' and Infinity to indicate anendless waits */
+export type WaitPeriod = 0 | number | Date;
+
 export interface Duration {
   sign: "+" | "-";
   years: number;
@@ -62,4 +65,24 @@ export function addDuration(startingdate: Date, duration: Partial<Duration> | st
   date.setTime(date.getTime() + direction * timeoffset);
 
   return date;
+}
+
+/** Convert a WaitPeriod parameter to a Date
+ *  @param wait - Wait time as milliseconds or a Date
+*/
+export function convertWaitPeriodToDate(wait: WaitPeriod): Date {
+  if (wait instanceof Date) {
+    return wait;
+  } else if (typeof wait === "number") {
+    if (wait === 0)
+      return new Date(-864000 * 1000 * 10000000);
+    if (wait === Infinity)
+      return new Date(864000 * 1000 * 10000000);
+    if (wait > 7 * 86400 * 1000)
+      throw new Error("Invalid wait duration - a wait may not be longer than a week"); //prevents you from passing in Date.now() based values
+    if (wait > 0)
+      return new Date(Date.now() + wait);
+  }
+
+  throw new Error("Invalid wait duration - it must either be an absolute date, 0, a number of milliseconds or Infinity");
 }
--- whtree/jssdk/std/std.ts
+++ whtree/jssdk/std/std.ts
@@ -1,8 +1,7 @@
 // Adding APIs may also require an update to https://www.webhare.dev/manuals/typescript/harescript-conversion-guide/
-export { convertWaitPeriodToDate, WaitPeriod } from "./api";
 export { DeferredPromise, sleep, createDeferred, wrapInTimeout } from "./promises";
 export { encodeString, decodeString, escapeRegExp, wildcardsToRegExp, StringEncodings } from "./strings";
 export { generateRandomId } from "./platformbased";
 export { emplace, EmplaceHandler } from "./collections";
 export { Money, MoneyRoundingMode } from "./money";
-export { addDuration, parseDuration, Duration } from "./datetime";
+export { addDuration, parseDuration, Duration, convertWaitPeriodToDate, WaitPeriod } from "./datetime";
