d6fe1cf0dfa087244a299cb02fc2e2568e5c4c0e
Arnold Hendriks <arnold@webhare.nl>
2023-05-23 00:51:32 +0200

    typescriptify utf8 a bit

--- whtree/modules/system/js/internal/utf8.ts
+++ whtree/modules/system/js/internal/utf8.ts
@@ -1,15 +1,11 @@
-/* eslint-disable */
-/// @ts-nocheck -- Bulk rename to enable TypeScript validation
-
-
-/* Return the length of the string in bytes when UTF8-encoded */
-function getUTF8Length(str) {
+/** Return the length of the string in bytes when UTF8-encoded */
+export function getUTF8Length(str: string) {
   // encodeURIComponent encodes every utf-8 byte into a character or a %<hex>. unescape translates back, but
   // but unescapes every %<hex> into a single character.
   return unescape(encodeURIComponent(str)).length;
 }
 
-function limitUTF8Length(str, len) {
+export function limitUTF8Length(str: string, len: number) {
   let retval = "";
   while (str) {
     const middle = (str.length + 1) / 2;
@@ -25,6 +21,3 @@ function limitUTF8Length(str, len) {
   }
   return retval;
 }
-
-exports.getUTF8Length = getUTF8Length;
-exports.limitUTF8Length = limitUTF8Length;
