9df12c49fe5041a16a3252a1e747e411143d1bd7
Arnold Hendriks <arnold@webhare.nl>
2023-06-14 23:46:32 +0200

    Properly wrap JSON log reader API.

---
+++ whtree/modules/system/js/internal/logging.ts
@@ -0,0 +1,10 @@
+import { readLogLines } from "@webhare/services";
+import { GenericLogLine } from "@webhare/services/src/logging";
+
+export async function readJSONLogLines(name: string, since: Date, limit: Date | null = null): Promise<GenericLogLine[]> {
+  const lines = [];
+  for await (const val of readLogLines(name, { start: since, limit })) {
+    lines.push(val);
+  }
+  return lines;
+}
