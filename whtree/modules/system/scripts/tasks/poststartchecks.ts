c4b2697529c37cb1e8eaba10068e87a56c0cafa7
Arnold Hendriks <arnold@webhare.nl>
2023-06-29 13:18:28 +0200

    Try not to backup image cache

---
+++ whtree/modules/system/scripts/tasks/poststartchecks.ts
@@ -0,0 +1,20 @@
+// Various checks to run after startup (but not block poststartdone)
+
+import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { join } from "path";
+import { config, toFSPath } from "@webhare/services";
+
+// TODO tikacache should perhaps be droppable too, but it has little churn and we're not guaranteed to quickly recover opensearch databases right now..
+
+
+// Ensure cache dirs are tagged
+for (const cachefolder of [
+  toFSPath("storage::system/output/uc"),
+  join(config.dataroot, "ephemeral")
+]) {
+  mkdirSync(cachefolder, { recursive: true });
+
+  const cachetag = join(cachefolder, "CACHEDIR.TAG");
+  if (!existsSync(cachetag))
+    writeFileSync(cachetag, "Signature: 8a477f597d28d172789f06886806bc55\n# Created by WebHare - the contents of this directory are easily recreated and do not need to be backed up\n", { encoding: "utf8" });
+}
