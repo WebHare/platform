ffb7c07190fa24a023348a124f30071f04db09e6
Arnold Hendriks <arnold@webhare.nl>
2023-08-16 13:09:33 +0200

    [backport] rename @webhare/env flags to debugFlags

--- whtree/jssdk/env/src/env.ts
+++ whtree/jssdk/env/src/env.ts
@@ -1,15 +1,17 @@
-import { flags, registerDebugConfigChangedCallback } from "./envbackend";
+import { debugFlags, registerDebugConfigChangedCallback } from "./envbackend";
 import { hookFetch } from "./fetchdebug";
 import { DTAPStage } from "./concepts";
 import * as envsupport from "./envsupport";
 
-export { flags } from "./envbackend";
+export { debugFlags } from "./envbackend";
 export { DTAPStage } from "./concepts";
 
 /** DTAP stage set for this WebHare */
 export const dtapstage: DTAPStage = envsupport.getDtapStage();
 /** Whether we should (pretend) to be live/production ... true on production and acceptance */
 export const islive = dtapstage == "production" || dtapstage == "acceptance";
+
+export const flags = debugFlags; //TODO Deprecate once 5.4 is the expected baseline everywhere
 
 /** Get the default base URL for RPCs
     @returns In the browser this returns the current root, in the backend it returns primary WebHare url. Always ends with a slash */
@@ -19,11 +21,11 @@ export function getDefaultRPCBase() {
 
 // Hook fetch if requested
 if (globalThis["fetch"]) {
-  if (flags.wrq)
+  if (debugFlags.wrq)
     hookFetch();
 
   registerDebugConfigChangedCallback(() => {
-    if (flags.wrq)
+    if (debugFlags.wrq)
       hookFetch();
   });
 }
