5fa90890ca623e7d665ca0beb03ddf757a381b2c
Arnold Hendriks <arnold@webhare.nl>
2023-08-16 13:22:08 +0200

    [backport] also add camel versions of isLive and dtapStage

--- whtree/jssdk/env/src/env.ts
+++ whtree/jssdk/env/src/env.ts
@@ -7,11 +7,13 @@ export { debugFlags } from "./envbackend";
 export { DTAPStage } from "./concepts";
 
 /** DTAP stage set for this WebHare */
-export const dtapstage: DTAPStage = envsupport.getDtapStage();
+export const dtapStage: DTAPStage = envsupport.getDtapStage();
 /** Whether we should (pretend) to be live/production ... true on production and acceptance */
-export const islive = dtapstage == "production" || dtapstage == "acceptance";
+export const isLive = dtapStage == "production" || dtapStage == "acceptance";
 
 export const flags = debugFlags; //TODO Deprecate once 5.4 is the expected baseline everywhere
+export const dtapstage = dtapStage; //TODO Deprecate once 5.4 is the expected baseline everywhere
+export const islive = isLive; //TODO Deprecate once 5.4 is the expected baseline everywhere
 
 /** Get the default base URL for RPCs
     @returns In the browser this returns the current root, in the backend it returns primary WebHare url. Always ends with a slash */
