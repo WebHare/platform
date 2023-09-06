da6d88d6e7a4dc0a49262bc61ae61534010bcf71
Rob Hulswit <rob@webhare.nl>
2023-09-06 22:40:21 +0200

    Move pick en omit to @webhare/std (and re-export from the old location)

--- whtree/modules/system/js/internal/util/algorithms.ts
+++ whtree/modules/system/js/internal/util/algorithms.ts
@@ -1,3 +1,5 @@
+export { pick, omit } from "@webhare/std";
+
 /** Maps every key of an object with a mapping function to a new value
     @typeParam T - Type of the object to map
     @typeParam K - Type of the mapped value
@@ -17,64 +19,6 @@ export function mapObject<T extends object, N extends (v: T[keyof T], k?: keyof 
     }
   }
   return retval;
-}
-
-/** Returns an object with a selection of properties
-    @typeParam T - Type of the supplied object
-    @typeParam K - Type of the property keys to return
-    @param obj - Object to pick properties out of
-    @param keys - Names of the properties to pick
-    @returns Resulting object
-*/
-export function pick<T extends object, K extends string & keyof T>(obj: T, keys: readonly K[]): Pick<T, K>;
-
-/** Returns an array with a selection of properties
-    @typeParam T - Type of the supplied array
-    @typeParam K - Type of the property keys to return
-    @param obj - Array to pick properties out of
-    @param keys - Names of the properties to pick
-    @returns Resulting array
-*/
-export function pick<T extends object, K extends string & keyof T>(arr: T[], keys: readonly K[]): Array<Pick<T, K>>;
-
-export function pick<T extends object, K extends string & keyof T>(value: T | T[], keys: readonly K[]): Pick<T, K> | Array<Pick<T, K>> {
-  if (Array.isArray(value))
-    return value.map((elt: T) => pick(elt, keys));
-  const ret = {} as Pick<T, K>;
-  keys.forEach((key: K) => {
-    if (key in value)
-      ret[key] = value[key];
-  });
-  return ret;
-}
-
-/** Returns an object with a selection of properties left out
-    @typeParam T - Type of the supplied object
-    @typeParam K - Type of the property keys to leave out
-    @param obj - Object to leave properties out of
-    @param keys - Names of the properties to remove
-    @returns Resulting object
-*/
-export function omit<T extends object, K extends string & keyof T>(obj: T, keys: readonly K[]): Omit<T, K>;
-
-/** Returns an array with a selection of properties left out
-    @typeParam T - Type of the supplied array
-    @typeParam K - Type of the property keys to leave out
-    @param obj - Array to leave properties out of
-    @param keys - Names of the properties to leave out
-    @returns Resulting array
-*/
-export function omit<T extends object, K extends string & keyof T>(arr: T[], keys: readonly K[]): Array<Omit<T, K>>;
-
-export function omit<T extends object, K extends string & keyof T>(value: T | T[], keys: readonly K[]): Omit<T, K> | Array<Omit<T, K>> {
-  if (Array.isArray(value))
-    return value.map((elt: T) => omit(elt, keys));
-  const ret = {} as Omit<T, K>;
-  for (const [key, val] of Object.entries(value)) {
-    if (!keys.includes(key as K))
-      ret[key as Exclude<keyof T, K>] = val;
-  }
-  return ret;
 }
 
 /** Recursively freezes a value
