b48830c7257828b64980c28b1b3f1f1275e25721
Arnold Hendriks <arnold@webhare.nl>
2023-10-13 15:56:39 +0200

    Support dev's browse module

---
+++ whtree/modules/platform/js/devsupport/devbridge.ts
@@ -0,0 +1,34 @@
+/* This is an import specifically for APIs needed by the 'dev' module, allowing us to manage version differences a bit */
+
+import { listAllGeneratedFiles } from "@mod-system/js/internal/generation/generator";
+import { whconstant_builtinmodules } from "@mod-system/js/internal/webhareconstants";
+import { backendConfig, toResourcePath } from "@webhare/services";
+import { pick } from "@webhare/std";
+
+function stripJSTSExtension(importPath: string) {
+  if (importPath.endsWith(".js") || importPath.endsWith(".ts"))
+    return importPath.substring(0, importPath.length - 3);
+  if (importPath.endsWith(".jsx") || importPath.endsWith(".tsx"))
+    return importPath.substring(0, importPath.length - 4);
+  return importPath;
+}
+
+export function getImportPath(resource: string) {
+  const generatedbase = backendConfig.dataroot + "storage/system/generated/";
+  if (resource.startsWith(generatedbase))
+    return "wh:" + stripJSTSExtension(resource.slice(generatedbase.length));
+
+  const tryresourcepath = toResourcePath(resource, { allowUnmatched: true });
+  if (tryresourcepath) {
+    if (tryresourcepath.startsWith("mod::"))
+      return "@mod-" + stripJSTSExtension(tryresourcepath.slice(5));
+  }
+
+  throw new Error(`Don't know importPath for: ${resource}`);
+}
+
+export async function getGeneratedFiles({ module }: { module: string }) {
+  const mappedtomodule = whconstant_builtinmodules.includes(module) ? "platform" : module;
+  const files = (await listAllGeneratedFiles()).filter(file => file.module === mappedtomodule);
+  return pick(files, ["path", "type"]).map(file => ({ ...file, importPath: getImportPath(file.path) }));
+}
---
+++ whtree/modules/webhare_testsuite/tests/system/nodejs/test_devsupport.ts
@@ -0,0 +1,16 @@
+import * as test from "@webhare/test";
+import * as devbridge from "@mod-platform/js/devsupport/devbridge";
+import { backendConfig } from "@webhare/services";
+
+async function testDevBridge() {
+  test.eq("wh:wrd/example", devbridge.getImportPath(backendConfig.dataroot + "storage/system/generated/wrd/example.ts"));
+  test.eq("@mod-system/js/internal/generated/whdb/webhare", devbridge.getImportPath(backendConfig.installationroot + "modules/system/js/internal/generated/whdb/webhare.ts"));
+
+  const test_platform_files = await devbridge.getGeneratedFiles({ module: "consilio" });
+  test.eqProps({ importPath: '@mod-system/js/internal/generated/whdb/webhare' }, test_platform_files.find(_ => _.type == "whdb"));
+
+  const test_whts_files = await devbridge.getGeneratedFiles({ module: "webhare_testsuite" });
+  test.eqProps({ importPath: 'wh:wrd/webhare_testsuite' }, test_whts_files.find(_ => _.type == "wrd"));
+}
+
+test.run([testDevBridge]);
