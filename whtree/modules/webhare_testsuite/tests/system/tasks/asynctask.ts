f25b693fd0f7d8d0b03874089b56d63562b5187a
Arnold Hendriks <arnold@webhare.nl>
2023-10-14 00:24:22 +0200

    Fix CallAsync uncaught rejections showing 'JavaScript runner process terminated without giving a response'

--- whtree/modules/webhare_testsuite/tests/system/tasks/asynctask.ts
+++ whtree/modules/webhare_testsuite/tests/system/tasks/asynctask.ts
@@ -1,3 +1,5 @@
+import { sleep } from "@webhare/std";
+
 export async function ping(arg: unknown) {
   return arg;
 }
@@ -9,5 +11,9 @@ export async function callAsyncTest(action: number) {
     throw new Error(`js-error`);
   if (action == 5)
     await new Promise(resolve => setTimeout(resolve, 100));
+  if (action == 6) {
+    new Promise((resolve, reject) => reject(new Error("This will be an uncaught rejection")));
+    await sleep(60000);
+  }
   return { action, source: "js" };
 }
