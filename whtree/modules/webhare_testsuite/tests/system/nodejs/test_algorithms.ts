559106744e602c4d9b83e0088630b33d3ba1f935
Rob Hulswit <rob@webhare.nl>
2023-11-07 17:00:34 +0100

    Implement executeEnrichment in TypeScript

--- whtree/modules/webhare_testsuite/tests/system/nodejs/test_algorithms.ts
+++ whtree/modules/webhare_testsuite/tests/system/nodejs/test_algorithms.ts
@@ -1,5 +1,5 @@
 import { omit, pick } from "@webhare/std";
-import { freezeRecursive, RecursivePartial } from "@mod-system/js/internal/util/algorithms";
+import { executeEnrichment, freezeRecursive, Merge, OptionalKeys, RecursivePartial, RequiredKeys, Simplify } from "@mod-system/js/internal/util/algorithms";
 import * as test from "@webhare/test";
 import { RecursiveReadOnly } from "@webhare/js-api-tools";
 
@@ -37,11 +37,115 @@ function testTypes() {
   test.typeAssert<test.Equals<{ readonly a: ReadonlyArray<{ readonly b: number }> }, RecursiveReadOnly<{ a: Array<{ b: number }> }>>>();
 
   test.typeAssert<test.Equals<{ a?: Array<{ b?: number }> }, RecursivePartial<{ a: Array<{ b: number }> }>>>();
+
+  test.typeAssert<test.Equals<"a" | "c", RequiredKeys<{ a: 1; b?: undefined; c: undefined }>>>();
+  test.typeAssert<test.Equals<"b", OptionalKeys<{ a: 1; b?: undefined; c: undefined }>>>();
+  test.typeAssert<test.Equals<{ a: 1 } & { b: 2; c: 2 }, Merge<{ a: 1; b: 1 }, { b: 2; c: 2 }>>>();
+  test.typeAssert<test.Equals<{ a: 1; b: 2 } | { a: 2; b: 3 }, Simplify<{ a: 1 } & { b: 2 } | { a: 2 } & { b: 3 }>>>();
+  test.typeAssert<test.Equals<{ a: 1; b: 2 } | { a: 2; b: 3 }, Simplify<{ a: 1 } & { b: 2 } | { a: 2 } & { b: 3 }>>>();
+}
+
+async function testExecuteEnrichment() {
+  const innerJoinTest = await executeEnrichment([
+    { a: 2 },
+    { a: 1 }
+  ], "a", {
+  }, async (ids, lor, cs) => {
+    return new Map([
+      [2, { c: 2 }],
+      [1, { a: 5, c: 3 }], // overrides a!
+    ]);
+  }, null, null);
+
+  test.eq([
+    { a: 2, c: 2 },
+    { a: 5, c: 3 }
+  ], innerJoinTest);
+
+  test.typeAssert<test.Equals<Array<{ a: number; c: number }>, typeof innerJoinTest>>();
+
+  const rightOuterJoinTest = await executeEnrichment([
+    { a: 2 },
+    { a: 1 }
+  ], "a", {
+  }, async (ids, lor, cs) => {
+    return new Map([[2, { c: 2 }]]);
+  }, null, () => ({ c: 4 }));
+
+  test.eq([
+    { a: 2, c: 2 },
+    { a: 1, c: 4 }
+  ], rightOuterJoinTest);
+
+  test.typeAssert<test.Equals<Array<{ a: number; c: number } | { a: number; c: number }>, typeof rightOuterJoinTest>>();
+
+  const leftOuterJoinTest = await executeEnrichment([{ a: 2 }], "a", {
+  }, async (ids, lor, cs) => {
+    return new Map([
+      [2, { c: 2 }],
+      [3, { c: 4 }],
+    ]);
+  },
+    () => ({ a: 3 }), null);
+
+  test.eq([
+    { a: 2, c: 2 },
+    { a: 3, c: 4 }
+  ], leftOuterJoinTest);
+
+  test.typeAssert<test.Equals<Array<{ a: number; c: number }|{ a: number; c: number }>, typeof leftOuterJoinTest>>();
+
+  const allTest = await executeEnrichment([{ a: 1, b: 2 }, { a: 2, b: 3 }], "a", {
+  }, async (ids, lor, cs) => {
+    // Need an explicit type here, otherwise `{c:number;type:string} | {c:number;type?:undefined}` is inferred.
+    const data: Array<[number, { c: number; type?: string }]> = [
+      [1, { c: 2, type: "innerJoin" }],
+      [3, { c: 3 }],
+    ];
+    return new Map(data);
+  },
+    () => ({ a: 3, b: 4, type: "leftOuterJoin" }),
+    () => ({ c: 4, type: "rightOuterJoin" }));
+
+  test.eq([
+    { a: 1, b: 2, c: 2, type: "innerJoin" },
+    { a: 2, b: 3, c: 4, type: "rightOuterJoin" },
+    { a: 3, b: 4, c: 3, type: "leftOuterJoin" },
+  ], allTest);
+
+  test.typeAssert<test.Equals<Array<
+    { a: number; b: number; c: number; type: string } |
+    { a: number; b: number; c: number; type?: string } // caused by the missing type in the second getBulkFields return value
+  >, typeof allTest>>();
+
+  const presentFieldTest = await executeEnrichment([{ a: 1, b: 2 }, { a: 2, b: 3 }], "a", {
+    presentfield: "present"
+  }, async (ids, lor, cs) => {
+    return new Map([
+      [1, { c: 2 }],
+      [3, { c: 3 }],
+    ]);
+  },
+    () => ({ a: 3, b: 4 }),
+    () => ({ c: 4 }));
+
+  test.eq([
+    { a: 1, b: 2, c: 2, present: "both" },
+    { a: 2, b: 3, c: 4, present: "left" },
+    { a: 3, b: 4, c: 3, present: "right" },
+  ], presentFieldTest);
+
+  test.typeAssert<test.Equals<Array<
+    { a: number; b: number; c: number; present: "both" } |
+    { a: number; b: number; c: number; present: "left" } |
+    { a: number; b: number; c: number; present: "right" } // caused by the missing type in the second getBulkFields return value
+  >, typeof presentFieldTest>>();
 }
 
 test.run([
   testPick,
   testOmit,
   testFreezeRecursive,
-  testTypes
+  testTypes,
+  testExecuteEnrichment,
 ]);
