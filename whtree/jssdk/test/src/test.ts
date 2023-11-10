dcf699543e28e897010d62692349d30735e4ce73
Arnold Hendriks <arnold@webhare.nl>
2023-11-10 12:22:53 +0100

    Cleanup test.ts, deprecate sleep

--- whtree/jssdk/test/src/test.ts
+++ whtree/jssdk/test/src/test.ts
@@ -1,6 +1,5 @@
-import { triggerGarbageCollection } from './testsupport';
-import { run } from './testrunner';
-export { startTime, triggerGarbageCollection, run };
+export { triggerGarbageCollection } from './testsupport';
+export { run } from './testrunner';
 
 export {
   assert,
@@ -19,11 +18,14 @@ export {
   typeAssert,
 } from './checks';
 
-export { sleep } from "@webhare/std";
-
 export type TestList = Array<string | (() => void | Promise<void>)>;
 
 // Want more than the default 10 stack frames in errors
 Error.stackTraceLimit = 25;
 
-const startTime = new Date;
+export const startTime = new Date;
+
+// the 'sleep' alias doesn't add much and might only confuse users if test.sleep is 'special'. let's mark it as obsolete
+import { sleep as stdSleep } from "@webhare/std";
+/** @deprecated Use std.sleep */
+export const sleep = stdSleep;
