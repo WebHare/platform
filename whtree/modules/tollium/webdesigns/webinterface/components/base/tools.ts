69eca0313bd2e29a93cc253fa8aa20be40f20028
Arnold Hendriks <arnold@webhare.nl>
2023-11-13 23:30:10 +0100

    TS cleanup

--- whtree/modules/tollium/webdesigns/webinterface/components/base/tools.ts
+++ whtree/modules/tollium/webdesigns/webinterface/components/base/tools.ts
@@ -1,24 +1,35 @@
-/* eslint-disable */
-/// @ts-nocheck -- Bulk rename to enable TypeScript validation
-
 import * as dompack from 'dompack';
 import * as domfocus from "dompack/browserfix/focus";
 import * as $todd from "@mod-tollium/web/ui/js/support";
 
-export function getBorderWidth(borders) {
+export interface Borders {
+  top?: boolean;
+  bottom?: boolean;
+  left?: boolean;
+  right?: boolean;
+}
+
+export interface Spacers {
+  top?: boolean;
+  bottom?: boolean;
+  left?: boolean;
+  right?: boolean;
+}
+
+export function getBorderWidth(borders: Borders): number {
   return (borders && borders.left ? $todd.settings.border_left : 0) + (borders && borders.right ? $todd.settings.border_right : 0);
 }
-export function getBorderHeight(borders) {
+export function getBorderHeight(borders: Borders): number {
   return (borders && borders.top ? $todd.settings.border_top : 0) + (borders && borders.bottom ? $todd.settings.border_bottom : 0);
 }
-export function getSpacerWidth(spacers) {
+export function getSpacerWidth(spacers: Spacers): number {
   return (spacers && spacers.left ? $todd.settings.spacer_left : 0) + (spacers && spacers.right ? $todd.settings.spacer_right : 0);
 }
-export function getSpacerHeight(spacers) {
+export function getSpacerHeight(spacers: Spacers): number {
   return (spacers && spacers.top ? $todd.settings.spacer_top : 0) + (spacers && spacers.bottom ? $todd.settings.spacer_bottom : 0);
 }
 
-export function copyValueToClipboard(node) {
+export function copyValueToClipboard(node: HTMLInputElement) {
   const alreadyfocused = node == domfocus.getCurrentlyFocusedElement();
   node.select();
   if (!alreadyfocused)
@@ -26,8 +37,7 @@ export function copyValueToClipboard(node) {
 
   document.execCommand("copy");
 
-  if (alreadyfocused) //flash if already focused
-  {
+  if (alreadyfocused) { //flash if already focused
     node.selectionStart = 0;
     node.selectionEnd = 0;
     window.setTimeout(() => node.select(), 100);
