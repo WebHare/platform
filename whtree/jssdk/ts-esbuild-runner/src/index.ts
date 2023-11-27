3cf122feceab964d1618e6f22ec65d4af408271b
Arnold Hendriks <arnold@webhare.nl>
2023-11-27 23:01:51 +0100

    Improve tsbuild plugin reusability

--- whtree/jssdk/ts-esbuild-runner/src/index.ts
+++ whtree/jssdk/ts-esbuild-runner/src/index.ts
@@ -4,12 +4,13 @@
    Changes to us don't take effect immediately. Rerun bin/prepare.sh to rebuild the plugin when needed.
    (In webhare/platform, invoke wh fixmodules to rebuild and reset the compile cache)
 
-   To debug, use WEBHARE_DEBUG=runner ...
+   To debug, use ESBUILDRUNNER=debug ...
 */
 
+import path from "node:path";
+import os from "node:os";
 import { installResolveHook } from "./resolvehook";
-import { debugFlags } from "@webhare/env/src/envbackend";
 import "./polyfills";
 
-const debug = Boolean(debugFlags.runner);
-installResolveHook(debug);
+const debug = process.env.ESBUILDRUNNER === "debug";
+installResolveHook({ debug, cachePath: path.join(os.homedir(), ".ts-esbuild-runner-cache") });
