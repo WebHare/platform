80c43720e8845c8ab23dd06025069b532905cb09
Rob Hulswit <rob@webhare.nl>
2023-12-04 13:27:30 +0100

    Fix test.eqProps losing enumeration types in expected value

--- whtree/jssdk/test/src/checks.ts
+++ whtree/jssdk/test/src/checks.ts
@@ -14,12 +14,18 @@ export type Annotation = string | (() => string);
 
 type LoggingCallback = (...args: unknown[]) => void;
 
+// Disallows type inferences when a parameter type is wrapped with this type. From: https://github.com/Microsoft/TypeScript/issues/14829#issuecomment-504042546
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type NoInfer<T> = [T][T extends any ? 0 : never];
+
+type PrimitiveType = Money | Date | RegExp;
+
 /** Recursively apply `Partial<>`  on records in a type but also allow Regexps to match strings
  * @typeParam T - Type to convert
 */
-type RecursivePartialOrRegexp<T> = T extends Array<infer U> ? Array<RecursivePartialOrRegexp<U>> : T extends string ? string | RegExp : T extends object ? { [K in keyof T]?: RecursivePartialOrRegexp<T[K]> } : T;
+type RecursivePartialOrRegexp<T> = T extends Array<infer U> ? Array<RecursivePartialOrRegexp<U>> : T extends string ? T | RegExp : T extends PrimitiveType ? T : T extends object ? { [K in keyof T]?: RecursivePartialOrRegexp<T[K]> } : T;
 
-type RecursiveOrRegexp<T> = T extends Array<infer U> ? Array<RecursiveOrRegexp<U>> : T extends string ? string | RegExp : T extends object ? { [K in keyof T]: RecursiveOrRegexp<T[K]> } : T;
+type RecursiveOrRegexp<T> = T extends Array<infer U> ? Array<RecursiveOrRegexp<U>> : T extends string ? (string & T & { __?: false }) | RegExp : T extends PrimitiveType ? T : T extends object ? { [K in keyof T]: RecursiveOrRegexp<T[K]> } : T;
 
 let onLog: LoggingCallback = console.log.bind(console) as LoggingCallback;
 
@@ -214,12 +220,13 @@ function toTestableString(val: unknown): string {
 }
 
 /** Verify deep equality of two values (to compare object identity, you need to use {@link assert} with ===)
- * @typeparam T - The type of the values (both values are expected to be of the same type)
+ * @typeparam T - The type of the values (both values are expected to be of the same type). This type is only inferred
+ * from the 'actual' parameter.
  * @param expected - The expected value
  * @param actual - The actual value
  * @throws If the values are not equal
  */
-export function eq<T>(expected: RecursiveOrRegexp<T>, actual: T, annotation?: Annotation): void {
+export function eq<T>(expected: NoInfer<RecursiveOrRegexp<T>>, actual: T, annotation?: Annotation): void {
   if (arguments.length < 2)
     throw new Error("Missing argument to test.eq");
 
@@ -352,16 +359,18 @@ export function throws(expect: RegExp, func_or_promise: Promise<unknown> | (() =
 }
 
 /** Compare specific cells of two values (recursive)
-    @param expected - Expected value
-    @param actual - Actual value
-    @param ignore - List of properties to ignore
-    @param annotation - Message to display when the test fails */
-export function eqProps<T>(expect: RecursivePartialOrRegexp<T>, actual: T, ignore: string[] = [], annotation?: Annotation) {
+ * @typeparam T - The type of the values (both values are expected to be of the same type). This type is only inferred
+ * from the 'actual' parameter.
+ *  @param expected - Expected value
+ *  @param actual - Actual value
+ *  @param ignore - List of properties to ignore
+ *  @param annotation - Message to display when the test fails */
+export function eqProps<T>(expect: NoInfer<RecursivePartialOrRegexp<T>>, actual: T, ignore: string[] = [], annotation?: Annotation) {
   eqPropsRecurse(expect, actual, "root", ignore, annotation);
   return actual;
 }
 
-function eqPropsRecurse<T>(expect: RecursivePartialOrRegexp<T>, actual: T, path: string, ignore: string[], annotation?: Annotation) {
+function eqPropsRecurse<T>(expect: NoInfer<RecursivePartialOrRegexp<T>>, actual: T, path: string, ignore: string[], annotation?: Annotation) {
   switch (typeof expect) {
     case "undefined": return;
     case "object":
