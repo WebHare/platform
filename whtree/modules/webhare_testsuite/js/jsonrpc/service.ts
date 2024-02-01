0acc3d0d93963d1c4d4ab3c02ab9240ac281df63
Arnold Hendriks <arnold@webhare.nl>
2024-02-01 14:49:51 +0100

    test global flag setting

--- whtree/modules/webhare_testsuite/js/jsonrpc/service.ts
+++ whtree/modules/webhare_testsuite/js/jsonrpc/service.ts
@@ -1,8 +1,10 @@
 import { getJSONAPICallWebRequest } from "@mod-system/js/internal/jsonrpccaller";
 import { MyService } from "./type";
+import { debugFlags } from "@webhare/env/src/envbackend";
 
 export class TestNoAuthJS implements MyService {
-  async validateEmail(langcode: string, emailaddress: string): Promise<boolean> {
+  async validateEmail(langcode: string,
+    emailaddress: string): Promise<boolean> {
     return Boolean(emailaddress.match(/webhare.dev$/));
   }
   async serverCrash() {
@@ -10,7 +12,12 @@ export class TestNoAuthJS implements MyService {
   }
   async describeMyRequest() {
     const info = getJSONAPICallWebRequest();
-    return { baseURL: info.baseURL, url: info.url.toString(), requestHeaders: Object.fromEntries(info.headers.entries()) };
+    return {
+      baseURL: info.baseURL,
+      url: info.url.toString(),
+      requestHeaders: Object.fromEntries(info.headers.entries()),
+      debugFlags: Object.keys(debugFlags).filter((flag) => debugFlags[flag])
+    };
   }
   async doConsoleLog() {
     console.log(`This log statement was generated on the server by the TestNoAuthJS service`);
