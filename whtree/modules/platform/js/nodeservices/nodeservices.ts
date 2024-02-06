8c36adde5c99ad7fb3161bed496fce2e1de3a5d0
Arnold Hendriks <arnold@webhare.nl>
2024-02-06 16:17:00 +0100

    support debugging individual nodeservices

--- whtree/modules/platform/js/nodeservices/nodeservices.ts
+++ whtree/modules/platform/js/nodeservices/nodeservices.ts
@@ -1,31 +1,93 @@
-import { activateHMR, runBackendService } from '@webhare/services';
-import type { ServiceClientFactoryFunction, ServiceControllerFactoryFunction } from '@webhare/services/src/backendservicerunner';
+/* To debug an individual backend service normally hosted by nodesercices:
+   wh run mod::platform/js/nodeservices/nodeservices.ts <servicename>
+
+*/
+
+import { BackendServiceConnection, activateHMR, openBackendService, runBackendService } from '@webhare/services';
+import type { ServiceClientFactoryFunction, ServiceControllerFactoryFunction, WebHareService } from '@webhare/services/src/backendservicerunner';
 import * as resourcetools from '@mod-system/js/internal/resourcetools';
 import { getExtractedConfig } from "@mod-system/js/internal/configuration";
 import { BackendServiceDescriptor } from "@mod-system/js/internal/generation/gen_extracts";
+import { program } from 'commander';
+
+const activeServices: Record<string, WebHareService> = {};
+
+program.name("nodeservices").
+  option("--debug", "Enable debugging").
+  argument("[service]", "Service to debug").
+  parse(process.argv);
 
 async function createServiceClient(service: BackendServiceDescriptor, args: unknown[]) {
   const client = await (await resourcetools.loadJSFunction<ServiceClientFactoryFunction>(service.clientFactory))(...args);
   return client;
 }
 
-async function launchService(service: BackendServiceDescriptor) {
+async function launchService(service: BackendServiceDescriptor): Promise<WebHareService | null> {
   try {
     if (service.controllerFactory) {
       const servicecontroller = await (await resourcetools.loadJSFunction<ServiceControllerFactoryFunction>(service.controllerFactory))();
-      runBackendService(service.name, (...args) => servicecontroller.createClient(...args));
+      return runBackendService(service.name, (...args) => servicecontroller.createClient(...args));
     } else if (service.clientFactory)
-      runBackendService(service.name, (...args) => createServiceClient(service, args));
+      return runBackendService(service.name, (...args) => createServiceClient(service, args));
+
+    throw new Error(`Don't know how to start service ${service.name}`);
   } catch (e) {
     console.error("Error starting service " + service.name, e);
+    return null;
+  }
+}
+
+class Client extends BackendServiceConnection {
+  suppressing: string[] = [];
+
+  async suppress(service: string) {
+    if (this.suppressing.includes(service))
+      throw new Error(`Already suppressing service ${service}`);
+    if (!activeServices[service])
+      throw new Error(`Not controlling ${service}`);
+
+    console.log(`Stopping handling of ${service}`);
+    this.suppressing.push(service);
+    activeServices[service].close();
+  }
+
+  async onClose() {
+    console.log("onclose");
+    const backendservices = getExtractedConfig("services").backendServices;
+    for (const service of this.suppressing) {
+      const srvinfo = backendservices.find((s) => s.name == service);
+      if (!srvinfo)
+        continue;
+
+      console.log(`Restarting ${service}`);
+      const srv = await launchService(srvinfo); //we don't await this, we just launch it and let it run in the background
+      if (srv)
+        activeServices[srvinfo.name] = srv;
+    }
   }
 }
 
 async function main() {
   const backendservices = getExtractedConfig("services").backendServices;
-  //Launch all services in parallel
-  for (const service of backendservices)
-    launchService(service); //we don't await this, we just launch it and let it run in the background
+
+  if (program.args[0]) { //debug this specific service
+    const service = backendservices.find((s) => s.name == program.args[0]);
+    if (!service)
+      throw new Error(`Unknown service ${service}`);
+
+    //TODO make this optional?
+    //Suppress the service in nodeservices
+    const nodeservices = await openBackendService("platform:nodeservices");
+    await nodeservices.suppress(service.name);
+    launchService(service);
+  } else {
+    runBackendService("platform:nodeservices", client => new Client, { dropListenerReference: true });
+    for (const service of backendservices) {
+      const srv = await launchService(service); //we don't await this, we just launch it and let it run in the background
+      if (srv)
+        activeServices[service.name] = srv;
+    }
+  }
 }
 
 activateHMR();
