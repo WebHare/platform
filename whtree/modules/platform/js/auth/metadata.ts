328e5b42a29ac5b64970a16f609260dc5dc62040
Arnold Hendriks <arnold@webhare.nl>
2024-02-14 13:22:42 +0100

    Test publishing openid metadata

---
+++ whtree/modules/platform/js/auth/metadata.ts
@@ -0,0 +1,36 @@
+import { WebHareRouter, WebRequest, WebResponse, createJSONResponse } from "@webhare/router";
+import { lookupPublishedTarget } from "@webhare/router/src/corerouter";
+import { getApplyTesterForObject } from "@webhare/whfs/src/applytester";
+//TOOD make this a public export somewhere? but should it include wrdOrg and wrdPerson though
+import type { Platform_BasewrdschemaSchemaType } from "@mod-system/js/internal/generated/wrd/webhare";
+import { WRDSchema } from "@webhare/wrd";
+import { getSchemaSettings } from "@webhare/wrd/src/settings";
+
+export async function wellKnownRouter(req: WebRequest): Promise<WebResponse> {
+  const target = await lookupPublishedTarget(req.url.toString()); //TODO can't we use 'obj' directly instead of going through a URL lookup?
+  if (!target?.targetObject)
+    throw new Error(`Unable to trace rqeuest back to a target`);
+
+  const tester = await getApplyTesterForObject(target.targetObject);
+  const wrdSchemaTag = (await tester.getWRDAuth())?.wrdSchema;
+  if (!wrdSchemaTag)
+    throw new Error(`No WRD schema defined for this location`);
+
+  const authSchema = new WRDSchema<Platform_BasewrdschemaSchemaType>(wrdSchemaTag);
+  const settings = await getSchemaSettings(authSchema, ["issuer"]);
+  if (!settings.issuer)
+    throw new Error(`WRD schema '${wrdSchemaTag}' is not configured with a JWKS issuer`);
+
+  const oidc_baseurl = new URL(`/.wh/wrd/openid/${encodeURIComponent(wrdSchemaTag)}/`, req.baseURL).toString();
+  return createJSONResponse(200, {
+    issuer: settings.issuer,
+    jwks_uri: oidc_baseurl + 'jwks',
+    authorization_endpoint: oidc_baseurl + "authorization",
+    token_endpoint: oidc_baseurl + "token"
+  });
+
+}
+
+// validate signatures
+wellKnownRouter satisfies WebHareRouter;
+
