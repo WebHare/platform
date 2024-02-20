14684dc1d89518df29ea1330be0043b0140253dc
Arnold Hendriks <arnold@webhare.nl>
2024-02-20 09:49:50 +0100

    Add @webhare/deps

---
+++ whtree/jssdk/deps/README.md
@@ -0,0 +1,13 @@
+# WebHare deps
+
+`@webhare/deps` provides access to a select few WebHare dependencies for your own use. These are modules we hope to be able to ship
+for quite some time and offer somewhat stable APIs. We expose most of these module APIs directly through an async interface but refer
+you to the modules themselves for further documentation
+
+## Sharp
+Usage
+
+```typescript
+import { createSharpImage } from "@webhare/deps";
+const img = await createSharpImage(data);
+```
--- whtree/jssdk/deps/src/deps.ts
+++ whtree/jssdk/deps/src/deps.ts
@@ -1,13 +1,16 @@
 import type * as sharp from "sharp";
 
+////////////////////////////// SHARP //////////////////////////
 let sharppromise: Promise<typeof sharp> | undefined = undefined;
 
+/** Load Sharp  */
 export async function loadSharp(): Promise<typeof sharp> {
   if (!sharppromise)
     sharppromise = import("sharp");
   return await sharppromise;
 }
 
+/** Load an image (loading sharp as needed) */
 export async function createSharpImage(...args: Parameters<typeof sharp.default>): Promise<ReturnType<typeof sharp.default>> {
   const lib = await loadSharp();
   return lib.default(...args);
