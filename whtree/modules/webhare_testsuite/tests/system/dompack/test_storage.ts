d175523324c3062024154d8415f25f6eb3395498
Arnold Hendriks <arnold@webhare.nl>
2024-03-01 22:00:12 +0100

    merge tests

--- whtree/modules/webhare_testsuite/tests/system/dompack/test_storage.ts
+++ whtree/modules/webhare_testsuite/tests/system/dompack/test_storage.ts
@@ -1,6 +1,24 @@
 import * as test from "@webhare/test-frontend";
 import * as dompack from "@webhare/dompack";
 
+function runSharedTests() {
+  const testApi = test.getWin().__testApi;
+
+  test.eq(null, testApi.getLocal("testFwKey"));
+  test.eq(null, testApi.getSession("testFwKey"));
+  test.eq(null, testApi.getCookie("testFwCookie"));
+  test.assert(!testApi.listCookies().some(_ => _.name === "testFwCookie"));
+
+  testApi.setLocal("testFwKey", { x: 42 });
+  testApi.setSession("testFwKey", { x: 32 });
+  testApi.setCookie("testFwCookie", "x:22");
+  test.eq({ x: 42 }, testApi.getLocal("testFwKey"));
+  test.eq({ x: 32 }, testApi.getSession("testFwKey"));
+  test.eq("x:22", testApi.getCookie("testFwCookie"));
+  test.assert(testApi.listCookies().some(_ => _.name === "testFwCookie"));
+  testApi.deleteCookie("testFwCookie");
+  test.assert(!testApi.listCookies().some(_ => _.name === "testFwCookie"));
+}
 
 test.run(
   [
@@ -13,19 +31,7 @@ test.run(
       await test.load('/.webhare_testsuite/tests/pages/dompack/?testpage=empty'); //we need 'a' page, doesn't matter which
       const testApi = test.getWin().__testApi;
       test.assert(!testApi.isStorageIsolated());
-
-      test.eq(null, testApi.getLocal("testFwKey"));
-      test.eq(null, testApi.getSession("testFwKey"));
-      test.eq(null, testApi.getCookie("testFwCookie"));
-      test.assert(!testApi.listCookies().some(_ => _.name === "testFwCookie"));
-
-      testApi.setLocal("testFwKey", { x: 42 });
-      testApi.setSession("testFwKey", { x: 32 });
-      testApi.setCookie("testFwCookie", "x:22");
-      test.eq({ x: 42 }, testApi.getLocal("testFwKey"));
-      test.eq({ x: 32 }, testApi.getSession("testFwKey"));
-      test.eq("x:22", testApi.getCookie("testFwCookie"));
-      test.assert(testApi.listCookies().some(_ => _.name === "testFwCookie"));
+      runSharedTests();
     },
 
     "Test isolated storage",
@@ -33,20 +39,7 @@ test.run(
       await test.load('/.webhare_testsuite/tests/pages/dompack/?testpage=empty&isolatestorage=1');
       const testApi = test.getWin().__testApi;
       test.assert(testApi.isStorageIsolated());
-      test.eq(null, testApi.getLocal("testFwKey"));
-      test.eq(null, testApi.getSession("testFwKey"));
-      test.eq(null, testApi.getCookie("testFwCookie"));
-      test.assert(!testApi.listCookies().some(_ => _.name === "testFwCookie"));
-
-      testApi.setLocal("testFwKey", { x: 43 });
-      testApi.setSession("testFwKey", { x: 33 });
-      testApi.setCookie("testFwCookie", "x:23");
-      test.eq({ x: 43 }, testApi.getLocal("testFwKey"));
-      test.eq({ x: 33 }, testApi.getSession("testFwKey"));
-      test.eq("x:23", testApi.getCookie("testFwCookie"));
-      test.assert(testApi.listCookies().some(_ => _.name === "testFwCookie"));
-      testApi.deleteCookie("testFwCookie");
-      test.assert(!testApi.listCookies().some(_ => _.name === "testFwCookie"));
+      runSharedTests();
     },
 
     "Test non-isolated storage again",
@@ -55,9 +48,7 @@ test.run(
       const testApi = test.getWin().__testApi;
       test.eq({ x: 42 }, testApi.getLocal("testFwKey"));
       test.eq({ x: 32 }, testApi.getSession("testFwKey"));
-      test.eq("x:22", testApi.getCookie("testFwCookie"));
-      test.assert(testApi.listCookies().some(_ => _.name === "testFwCookie"));
-      testApi.deleteCookie("testFwCookie");
+      test.eq(null, testApi.getCookie("testFwCookie"));
       test.assert(!testApi.listCookies().some(_ => _.name === "testFwCookie"));
     }
   ]);
