6ec1740628287e6926d387f09c93f3d48678e008
Rob Hulswit <rob@webhare.nl>
2024-03-12 16:16:23 +0100

    Apply 2 suggestion(s) to 1 file(s)

--- whtree/modules/system/js/dompack/src/tree.ts
+++ whtree/modules/system/js/dompack/src/tree.ts
@@ -32,12 +32,16 @@ function generateInsertList(nodes: Array<string | Node>) {
 /** Test whether node is an Element, even if it's in a different iframe */
 export function isElement(node: unknown): node is Element {
   //TODO What is actually going on if defaultView is missing?
+  if (!node || typeof node !== "object")
+    return false
   const proto = (node as Element).ownerDocument.defaultView?.Element;
   return Boolean(proto && node instanceof proto);
 }
 /** Test whether node is a HTMLElement, even if it's in a different iframe */
 export function isHTMLElement(node: unknown): node is HTMLElement {
-  const proto = (node as Element).ownerDocument.defaultView?.HTMLElement;
+  if (!node || typeof node !== "object")
+    return false
+  const proto = (node as Element).ownerDocument.defaultView?.Element;
   return Boolean(proto && node instanceof proto);
 }
 
