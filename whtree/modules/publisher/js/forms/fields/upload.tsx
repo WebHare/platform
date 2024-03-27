f7484b163e7d8f25c32342883ff742f5184f54b2
Arnold Hendriks <arnold@webhare.nl>
2024-03-27 22:39:54 +0100

    fix typings

--- whtree/modules/publisher/js/forms/fields/upload.tsx
+++ whtree/modules/publisher/js/forms/fields/upload.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable */
-/// @ts-nocheck -- Bulk rename to enable TypeScript validation
-
 /* import ImgEditField from '@mod-publisher/js/forms/fields/imgedit';
 */
 import * as dompack from 'dompack';
@@ -8,9 +5,10 @@ import { getTid } from "@mod-tollium/js/gettid";
 import "../internal/form.lang.json";
 import FileEditBase from './fileeditbase';
 import './upload.css';
+import type { UploadedFile } from '@mod-system/js/compat/upload';
 
 
-function getLabelText(node) {
+function getLabelText(node: HTMLElement) {
   let label;
   if (node.id !== "") {
     const labelnode = document.querySelector(`label[for="${node.id}"]`);
@@ -25,8 +23,18 @@ function getLabelText(node) {
 
 
 export default class UploadField extends FileEditBase {
-  constructor(node, options?) {
-    super(node, options);
+  replacement;
+  private _filenameinput;
+  private _filenamefield;
+  private _deletebutton;
+  private _uploadbutton;
+  declare node: HTMLInputElement;
+
+  constructor(node: HTMLElement) { //we can't set this to HTMLInputElement, breaks too much existing code: `dompack.register(".wh-form__upload", node => new UploadField(node))`
+    if ((node as HTMLInputElement).type !== "file")
+      throw new Error(`An UploadField must be bound to a field of type file`);
+
+    super(node);
     if (!this.node)
       return; //init cancelled
 
@@ -34,7 +42,7 @@ export default class UploadField extends FileEditBase {
 
     this.node.addEventListener("click", e => this.selectFile(e)); //we still need to intercept clicks, even if we're hiding it
     this.node.addEventListener('dompack:takefocus', evt => this._takeFocus(evt));
-    this.replacement = <div class="wh-form__uploadfield" on={{ click: e => this.selectFile(e) }}>
+    this.replacement = <div class="wh-form__uploadfield" on={{ click: (e: MouseEvent) => this.selectFile(e) }}>
       {this._filenameinput =
         <span class="wh-form__uploadfieldinputholder">
           {this._filenamefield =
@@ -49,7 +57,7 @@ export default class UploadField extends FileEditBase {
               aria-label={getTid("publisher:site.forms.upload-remove")}
               tabindex="0"
               role="button"
-              on={{ click: evt => this._doDelete(evt), keypress: evt => this._checkForDelete(evt) }}
+              on={{ click: (evt: Event) => this._doDelete(evt), keypress: (evt: KeyboardEvent) => this._checkForDelete(evt) }}
             />
           }
         </span>
@@ -62,17 +70,17 @@ export default class UploadField extends FileEditBase {
     this.refresh();
     this._afterConstruction();
   }
-  _updateEnabledStatus(nowenabled) {
+  _updateEnabledStatus(nowenabled: boolean) {
     this.node.disabled = false; //we'll now manually handle disabled status
     this._uploadbutton.disabled = !nowenabled;
   }
-  _takeFocus(evt) {
+  _takeFocus(evt: Event) {
     evt.preventDefault();
     dompack.focus(this.replacement.querySelector("button"));
   }
 
   // check whether the delete button was activated through a keypress
-  _checkForDelete(evt) {
+  _checkForDelete(evt: KeyboardEvent) {
     // We only interested when the enter or space key was pressed
     if (evt.keyCode !== 13 && evt.keyCode !== 32)
       return;
@@ -80,7 +88,7 @@ export default class UploadField extends FileEditBase {
     this._doDelete(evt);
   }
 
-  _doDelete(evt) {
+  _doDelete(evt: Event) {
     dompack.stop(evt);
     if (!this._getEnabled())
       return;
@@ -98,9 +106,9 @@ export default class UploadField extends FileEditBase {
     dompack.toggleClass(this.replacement, "wh-form__uploadfield--hasfile", hasfile);
   }
   getFieldValueLink() {
-    return this.node.dataset.whFileurl;
+    return this.node.dataset.whFileurl || null;
   }
-  async handleUploadedFile(result) {
+  async handleUploadedFile(result: UploadedFile) {
     this.node.dataset.whFileurl = result.url;
     //this.filesize = result.size; - size is there if we need it, but not using it yet
     this.refresh();
