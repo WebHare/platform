c4c7846b3c60ce33bbf78883db164529cfcd2af0
Arnold Hendriks <arnold@webhare.nl>
2024-03-28 10:05:45 +0100

    multiupload

--- whtree/modules/webhare_testsuite/tests/wh/jscomponents/compat/testupload2.ts
+++ whtree/modules/webhare_testsuite/tests/wh/jscomponents/compat/testupload2.ts
@@ -1,9 +1,9 @@
 import * as test from '@webhare/test-frontend';
 
-async function runUploadTest(button: "#upload", files: Array<File | string>) {
+async function runUploadTest(button: "#upload" | "#uploadmultiple", files: Array<File | string>) {
   test.qR("#files").textContent = "";
   test.prepareUpload(files);
-  test.click("#upload");
+  test.click(button);
   return JSON.parse(await test.wait(() => test.qR("#files").textContent));
 }
 
@@ -17,6 +17,7 @@ async function testBasicUpload() {
     "hash": "A6xnQhbz4Vx2HuGl4lXwZ5U2I8iziLRFnhP5eNfIRvQ"
   }, await runUploadTest("#upload", [new File(['1234'], "file1.txt", { type: "text/plain" })]));
 
+
   //upload an image, quickly
   test.qR("#chunksize").value = "32768"; //ie about 5 chunks
   test.eqPartial({
@@ -25,6 +26,17 @@ async function testBasicUpload() {
     "mediaType": "image/jpeg",
     "hash": "JpFmbGS2jVY_UCJsLfkhye3ugnq_QC6SK-S-LpkvsGE"
   }, await runUploadTest("#upload", ['/tollium_todd.res/webhare_testsuite/tollium/portrait_8.jpg']));
+
+  //multiple upload
+  test.eqPartial([
+    {
+      "fileName": "file1.txt",
+      "hash": "A6xnQhbz4Vx2HuGl4lXwZ5U2I8iziLRFnhP5eNfIRvQ"
+    }, {
+      "fileName": "portrait_8.jpg",
+      "hash": "JpFmbGS2jVY_UCJsLfkhye3ugnq_QC6SK-S-LpkvsGE"
+    }
+  ], await runUploadTest("#uploadmultiple", [new File(['1234'], "file1.txt", { type: "text/plain" }), '/tollium_todd.res/webhare_testsuite/tollium/portrait_8.jpg']));
 
   //upload an image, slowly
   test.qR("#chunksize").value = "256";
--- whtree/modules/webhare_testsuite/web/tests/pages/upload/uploadtest.ts
+++ whtree/modules/webhare_testsuite/web/tests/pages/upload/uploadtest.ts
@@ -21,4 +21,23 @@ async function runUpload() { //upload button is clicked
   }
 }
 
+async function runUploadMultiple() { //upload button is clicked
+  const opts: UploadRequestOptions = {};
+  const chunkSize = parseInt(dompack.qR<HTMLInputElement>("#chunksize").value) || 0;
+
+  const uploader = await frontend.requestFiles(opts); //show dialog to user
+  if (uploader) { //not cancelled
+    //RPC call to ask server if its happy to receive the files. it will invoke a servside
+    const uploadinstructions = await test.invoke("@mod-webhare_testsuite/tests/wh/jscomponents/compat/testupload2-lib.ts#offerFiles", uploader.manifest, { chunkSize });
+
+    //Start the upload
+    const uploadedfiles = await uploader.upload(uploadinstructions);//, onProgress: ...);
+
+    //Do something with the upload to prove the server has it
+    const info = await Promise.all(uploadedfiles.map(file => test.invoke("@mod-webhare_testsuite/tests/wh/jscomponents/compat/testupload2-lib.ts#getFile", file.token)));
+    dompack.qR("#files").textContent = JSON.stringify(info, null, 2);
+  }
+}
+
 dompack.register("#upload", _ => _.addEventListener("click", runUpload));
+dompack.register("#uploadmultiple", _ => _.addEventListener("click", runUploadMultiple));
