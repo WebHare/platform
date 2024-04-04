0e3c40c75bfbe36a5be898d52de63c83e1dc0ad1
Arnold Hendriks <arnold@webhare.nl>
2024-04-04 23:53:48 +0200

    cleanup old uploads

--- whtree/modules/system/scripts/tasks/executemaintenance.ts
+++ whtree/modules/system/scripts/tasks/executemaintenance.ts
@@ -1,7 +1,10 @@
 import type { PlatformDB } from "@mod-system/js/internal/generated/whdb/platform";
 import { runAccountExpiration } from "@mod-system/js/internal/userrights/accountexpiration";
+import { toFSPath } from "@webhare/services";
+import { deleteRecursive } from "@webhare/system-tools";
 import { beginWork, commitWork, db } from "@webhare/whdb";
 import { listSchemas } from "@webhare/wrd";
+import { readdir } from "fs/promises";
 
 async function expireOldUsers() {
   let schemastofix = await listSchemas();
@@ -17,9 +20,24 @@ async function cleanupOldSessions() {
   await commitWork();
 }
 
+async function cleanupOldUploads() {
+  const basedir = toFSPath("storage::platform/uploads");
+  const currentuploads = (await readdir(basedir)).filter(_ => _ !== "CACHEDIR.TAG");
+  if (currentuploads.length === 0)
+    return; //nothing to do
+
+  const uploadsessions = new Set(
+    ...(await db<PlatformDB>().selectFrom("system.sessions").select(["sessionid"]).where("scope", "=", "platform:uploadsession").execute()).map(_ => _.sessionid));
+  for (const session of currentuploads)
+    if (!uploadsessions.has(session))
+      await deleteRecursive(`${basedir}/${session}`, { deleteSelf: true });
+}
+
 async function runMaintenance() {
-  await expireOldUsers();
+  //Things that may free up space always go first in case someone runs these maintenance scripts hoping to free up space fast
   await cleanupOldSessions();
+  await cleanupOldUploads();
+  await expireOldUsers();
 }
 
 runMaintenance();
