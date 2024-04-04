8bdaf2d20bf5f61e50d8563803875ba3e6b95c35
Arnold Hendriks <arnold@webhare.nl>
2024-04-04 13:12:27 +0200

    move cleanupsessions to TS

--- whtree/modules/system/scripts/tasks/executemaintenance.ts
+++ whtree/modules/system/scripts/tasks/executemaintenance.ts
@@ -1,4 +1,6 @@
+import type { PlatformDB } from "@mod-system/js/internal/generated/whdb/platform";
 import { runAccountExpiration } from "@mod-system/js/internal/userrights/accountexpiration";
+import { beginWork, commitWork, db } from "@webhare/whdb";
 import { listSchemas } from "@webhare/wrd";
 
 async function expireOldUsers() {
@@ -8,4 +10,16 @@ async function expireOldUsers() {
     await runAccountExpiration(schema.tag);
   }
 }
-expireOldUsers();
+
+async function cleanupOldSessions() {
+  await beginWork();
+  await db<PlatformDB>().deleteFrom("system.sessions").where("expires", "<", new Date).execute();
+  await commitWork();
+}
+
+async function runMaintenance() {
+  await expireOldUsers();
+  await cleanupOldSessions();
+}
+
+runMaintenance();
