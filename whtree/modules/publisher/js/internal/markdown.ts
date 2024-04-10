9fcfcc52d81f69bb14727cf29a0e0420ed0293b6
Arnold Hendriks <arnold@webhare.nl>
2024-04-10 14:49:57 +0200

    Markdown: shift headings so you can always use # at top level
    new: markdown headings will now be shifted one level down, eg # becomes H2, to better align with SEO conventions

--- whtree/modules/publisher/js/internal/markdown.ts
+++ whtree/modules/publisher/js/internal/markdown.ts
@@ -13,8 +13,18 @@ export async function renderMarkdown(request: SiteRequest): Promise<WebResponse>
   //FIXME prevent any HTML from leaking through!  maybe some in the future but we still need to Sanitize(Dompurify?) it and/or make it conform to the rtdtype ?
   const md = new MarkdownIt({ linkify: true });
   md.validateLink = (url: string) => true;
+
+  //We will shift headings one level down (TODO might want to make this configurable at some point)
   md.renderer.rules["heading_open"] = (tokens, idx, options, env, self) => {
-    return `<${tokens[0].tag} class="heading${tokens[0].tag.substring(1)}">`;
+    const level = parseInt(tokens[idx].tag[1]) + 1;
+    const tag = level > 6 ? "p" : `h${level}`;
+    const className = level > 6 ? "normal" : `heading${level}`;
+    return `<${tag} class="${className}">`;
+  };
+  md.renderer.rules["heading_close"] = (tokens, idx, options, env, self) => {
+    const level = parseInt(tokens[idx].tag[1]) + 1;
+    const tag = level > 6 ? "p" : `h${level}`;
+    return `</${tag}>`;
   };
   md.renderer.rules["paragraph_open"] = (tokens, idx, options, env, self) => {
     return `<p class="normal">`;
