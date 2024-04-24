105c3803993c2d9075787a05f593a0c0fe820aa4
Arnold Hendriks <arnold@webhare.nl>
2024-04-24 23:41:26 +0200

    verify refpoints work

--- whtree/modules/webhare_testsuite/tests/tollium/components/testimgedit-refpoint.ts
+++ whtree/modules/webhare_testsuite/tests/tollium/components/testimgedit-refpoint.ts
@@ -3,6 +3,7 @@
 
 import * as test from '@mod-tollium/js/testframework';
 import { prepareUpload } from '@webhare/test-frontend';
+import { getRelativeBounds } from '@webhare/dompack';
 
 test.registerTests(
   [
@@ -36,7 +37,39 @@ test.registerTests(
 
       const pointer = test.qS(".wh-refbox-pointer");
       test.assert(pointer, 'refbox pointer should be there');
-      test.clickTolliumButton("OK");
+      test.click('.wh-image-surface', { x: 308, y: "99%" }); //focus the 'YEAH'
 
+      const relpos = getRelativeBounds(test.qR(".wh-refbox-pointer"), test.qR(".wh-image-surface"));
+      //these might be a few pixels off, but not too much
+      test.assert(258 < relpos.top && relpos.top < 268, `relpos.top should be approximately 263, it is: ${relpos.top}`);
+      test.assert(295 < relpos.left && relpos.left < 305, `relpos.left should be approximately 300, it is: ${relpos.left}`);
+
+      //test whether the image shifted up. pretty white box approach, the first canvas shifts up
+      const imgcanvas = test.qS(".wh-image-surface > canvas");
+      test.assert(imgcanvas);
+      const relcanvaspos = getRelativeBounds(imgcanvas, test.qR(".wh-image-surface"));
+      //these might be a few pixels off, but not too much
+      test.assert(-46 < relcanvaspos.top && relcanvaspos.top < -36, `relcanvaspos.top should be approximately -41, it is: ${relcanvaspos.top}`);
+
+      test.clickTolliumButton("OK");
+      await test.wait("ui");
+      test.clickTolliumButton("Save");
+      await test.wait("ui");
+    },
+
+    "verify refpoint saved and restored",
+    async function () {
+      test.click(test.compByName("fragment1!editbutton"));
+      await test.wait("ui");
+
+      const refpoint = test.qSA("t-custom[data-name='imageeditor'] .wh-toolbar-button")
+        .filter(button => button.textContent.includes('Reference Point'))[0];
+      test.click(refpoint);
+      await test.wait("ui");
+
+      const relpos = getRelativeBounds(test.qR(".wh-refbox-pointer"), test.qR(".wh-image-surface"));
+      //these might be a few pixels off, but not too much
+      test.assert(258 < relpos.top && relpos.top < 268, `relpos.top should be approximately 263, it is: ${relpos.top}`);
+      test.assert(295 < relpos.left && relpos.left < 305, `relpos.left should be approximately 300, it is: ${relpos.left}`);
     }
   ]);
