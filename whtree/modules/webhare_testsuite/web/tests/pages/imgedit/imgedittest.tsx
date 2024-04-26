ca7110010fea6bf91a97e464078f73a286995afd
Arnold Hendriks <arnold@webhare.nl>
2024-04-26 10:22:45 +0200

    Add load button for manual tests

--- whtree/modules/webhare_testsuite/web/tests/pages/imgedit/imgedittest.tsx
+++ whtree/modules/webhare_testsuite/web/tests/pages/imgedit/imgedittest.tsx
@@ -1,11 +1,17 @@
 import * as dompack from "@webhare/dompack";
 import { ImageEditElement, type ImageEditMetadataEvent } from "@webhare/image-edit";
+import { fetchAsFile } from "@webhare/test-frontend";
 
 window.addEventListener("wh-image-edit-metadata", (event: ImageEditMetadataEvent) => {
   dompack.qR("#statusbar").textContent = JSON.stringify(event.detail);
 });
 
 customElements.define("wh-image-edit", ImageEditElement);
+
+dompack.register("#loadlandscape", node => node.addEventListener("click", async () => {
+  const img = await fetchAsFile('/tollium_todd.res/webhare_testsuite/tollium/landscape_4.jpg');
+  dompack.qR<ImageEditElement>("#imgedit").loadImage(img);
+}));
 
 dompack.register("#save", node => node.addEventListener("click", async () => {
   const saved = await dompack.qR<ImageEditElement>("wh-image-edit").saveImage();
