2176f5887e8fad26d54e6ca9101b7f73f9961999
Arnold Hendriks <arnold@webhare.nl>
2024-05-14 23:30:36 +0200

    Fix WEBHARE_DEBUG=runner

--- whtree/modules/platform/js/bootstrap/whnode.ts
+++ whtree/modules/platform/js/bootstrap/whnode.ts
@@ -2,13 +2,15 @@
    We replace the role of tsrun/index.ts
 
    Changes to us don't take effect immediately. Rerun wh finalize-webhare to rebuild this plugin and reset the compile cache
+
+   We support debug mode but only if WEBHARE_DEBUG explicitly contains 'runner' - we do not listen to globally managed debug flags
+   as those would require a lot of backend and/org bridge dependencies at a too early moment
 */
 
-import { debugFlags } from "@webhare/env/src/envbackend";
 import { installResolveHook } from "@webhare/tsrun/src/resolvehook";
 import "@webhare/tsrun/src/polyfills";
 
-const debug = Boolean(debugFlags.runner);
+const debug = process.env.WEBHARE_DEBUG?.split(",").includes("runner") ?? false;
 const cachePath = process.env.WEBHARE_TSBUILDCACHE;
 if (!cachePath) {
   console.error("WEBHARE_TSBUILDCACHE not set. Not invoked using `wh` ?");
