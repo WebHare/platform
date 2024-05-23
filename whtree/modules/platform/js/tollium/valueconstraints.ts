4fc1e28df589e3c19abe003e3325ad37ebb972f7
Arnold Hendriks <arnold@webhare.nl>
2024-05-23 11:45:38 +0200

    Add basic valueConstraints

---
+++ whtree/modules/platform/js/tollium/valueconstraints.ts
@@ -0,0 +1,27 @@
+import type { ValueConstraints } from "@mod-platform/generated/schema/siteprofile";
+
+//As long as we get away with exactly copying the definition in the futureit guarantees some consistency..
+export type { ValueConstraints };
+
+export function mergeConstraints(lhs: Readonly<ValueConstraints>, rhs: Readonly<ValueConstraints>): ValueConstraints;
+export function mergeConstraints(lhs: Readonly<ValueConstraints> | null, rhs: Readonly<ValueConstraints> | null): ValueConstraints | null;
+
+export function mergeConstraints(lhs: Readonly<ValueConstraints> | null, rhs: Readonly<ValueConstraints> | null): ValueConstraints | null {
+  if (lhs === null)
+    return rhs;
+  if (rhs === null)
+    return lhs;
+
+  //Now neither will be null
+  const mergedConstraints = { ...lhs, ...rhs };
+
+  //Any value in both constraints will have been set to the one in rhs. So now copy any lhs values where needed
+  if (lhs.minValue !== undefined && lhs.minValue > mergedConstraints.minValue!)
+    mergedConstraints.minValue = lhs.minValue;
+  if (lhs.maxValue !== undefined && lhs.maxValue < mergedConstraints.maxValue!)
+    mergedConstraints.maxValue = lhs.maxValue;
+  if (lhs.maxBytes !== undefined && lhs.maxBytes < mergedConstraints.maxBytes!)
+    mergedConstraints.maxBytes = lhs.maxBytes;
+
+  return mergedConstraints;
+}
---
+++ whtree/modules/webhare_testsuite/tests/tollium/api/test_constraints.ts
@@ -0,0 +1,25 @@
+import { mergeConstraints, type ValueConstraints } from "@mod-platform/js/tollium/valueconstraints";
+import * as test from "@webhare/test";
+
+function testConstraintMerge(expect: ValueConstraints | null, lhs: ValueConstraints | null, rhs: ValueConstraints | null) {
+  //order should not matter:
+  test.eq(expect, mergeConstraints(lhs, rhs));
+  test.eq(expect, mergeConstraints(rhs, lhs));
+  //merging any constraint with the expected value should simply return the expected value
+  test.eq(expect, mergeConstraints(expect, lhs));
+  test.eq(expect, mergeConstraints(expect, rhs));
+  test.eq(expect, mergeConstraints(lhs, expect));
+  test.eq(expect, mergeConstraints(rhs, expect));
+}
+
+function testValueConstraints() {
+  testConstraintMerge(null, null, null);
+  testConstraintMerge({ minValue: 2048 }, null, { minValue: 2048 });
+  testConstraintMerge({ minValue: 4096 }, { minValue: 4096 }, { minValue: 2048 });
+  testConstraintMerge({ maxValue: 2048 }, null, { maxValue: 2048 });
+  testConstraintMerge({ maxValue: 2048 }, { maxValue: 4096 }, { maxValue: 2048 });
+  testConstraintMerge({ maxBytes: 2048 }, null, { maxBytes: 2048 });
+  testConstraintMerge({ maxBytes: 2048 }, { maxBytes: 4096 }, { maxBytes: 2048 });
+}
+
+test.run([testValueConstraints]);
