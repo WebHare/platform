1c14c88a55ceed58d6b62442a7c1963fb09c83cb
Arnold Hendriks <arnold@webhare.nl>
2024-07-05 10:13:54 +0200

    expose RegisteredFieldBase from @webhare/forms

--- whtree/jssdk/forms/src/forms.ts
+++ whtree/jssdk/forms/src/forms.ts
@@ -1,6 +1,7 @@
 import FormBase from '@mod-publisher/js/forms/formbase';
 import RPCFormBase from '@mod-publisher/js/forms/rpc';
 import { verifyAddress, AddressValidationResult, AddressChecks, AddressValue } from './address';
+import type { RegisteredFieldBase } from './registeredfield';
 
-export { FormBase, RPCFormBase, verifyAddress };
+export { FormBase, RPCFormBase, verifyAddress, RegisteredFieldBase };
 export type { AddressValidationResult, AddressValue, AddressChecks };
