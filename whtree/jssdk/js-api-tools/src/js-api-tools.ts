339d27e8cd3d935021d38e5aa0c994863a19c6d5
Arnold Hendriks <arnold@webhare.nl>
2024-07-11 11:27:20 +0200

    Move RecursivePartial to js-api-tools

--- whtree/jssdk/js-api-tools/src/js-api-tools.ts
+++ whtree/jssdk/js-api-tools/src/js-api-tools.ts
@@ -2,7 +2,7 @@ export { checkPromiseErrorsHandled } from "./async";
 export { parseTrace, getStackTrace, prependStackTrace } from "./stacktracing";
 export { levenshteinDistance, getBestMatch, addBestMatch } from "./levenshtein";
 export type { StackTrace, StackTraceItem } from "./stacktracing";
-export type { RecursiveReadonly, PromisifyFunctionReturnType } from "./utility-types";
+export type { RecursiveReadonly, PromisifyFunctionReturnType, RecursivePartial } from "./utility-types";
 
 import type { RecursiveReadonly } from "./utility-types";
 /** @deprecated Switch to RecursiveReadonly in WH5.6 (matches TS Readonly casing) */
