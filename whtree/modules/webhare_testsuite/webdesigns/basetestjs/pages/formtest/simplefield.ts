bc347c3e8b3e35ddbdf99e70f30164b879381b76
Arnold Hendriks <arnold@webhare.nl>
2024-07-12 10:47:36 +0200

    Support simple field
    new: Support custom form fields only defining a witty

---
+++ whtree/modules/webhare_testsuite/webdesigns/basetestjs/pages/formtest/simplefield.ts
@@ -0,0 +1,25 @@
+import { JSFormElement } from '@webhare/forms';
+
+export type MySimpleFieldValue = { answer: number };
+
+export class MySimpleField extends JSFormElement<MySimpleFieldValue> {
+  didsetup = false;
+
+  connectedCallback() {
+    if (!this.didsetup) //attach event listeners only on first connect
+      this.querySelector("button")?.addEventListener("click", () => this.setAnswer());
+
+    this.didsetup = true;
+  }
+  get value() {
+    return {
+      answer: parseInt(this.querySelector(".answer")?.textContent || "") || 0
+    };
+  }
+  set value(val: { answer: number }) {
+    this.querySelector(".answer")!.textContent = String(val.answer);
+  }
+  setAnswer() {
+    this.value = { answer: this.value.answer + 14 };
+  }
+}
