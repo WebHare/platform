dc4a23a535f56116c56be9e9bc361b473f0ebbc0
Rob Hulswit <rob@webhare.nl>
2024-09-06 15:04:54 +0200

    Show dangling promises when closing a codecontext when debugflags retainers is active

--- whtree/jssdk/services/src/codecontexts.ts
+++ whtree/jssdk/services/src/codecontexts.ts
@@ -7,8 +7,9 @@ import { StackTraceItem, getCallStack } from "@mod-system/js/internal/util/stack
 import { debugFlags } from "@webhare/env";
 import { AsyncLocalStorage } from "async_hooks";
 import EventSource from "@mod-system/js/internal/eventsource";
-import { DebugFlags, setDebugFlagsOverrideCB } from "@webhare/env/src/envbackend";
+import { DebugFlags, registerDebugConfigChangedCallback, setDebugFlagsOverrideCB } from "@webhare/env/src/envbackend";
 import type { ConsoleLogItem } from "@webhare/env/src/concepts";
+import * as async_hooks from "node:async_hooks";
 import { registerAsNonReloadableLibrary } from "@mod-system/js/internal/hmrinternal";
 
 let contextcounter = 0;
@@ -67,6 +68,7 @@ export class CodeContext extends EventSource<CodeContextEvents> implements Async
   readonly storage = new Map<string | symbol, { resource: unknown; dispose?: (x: unknown) => void | Promise<void> }>();
   private closed = false;
   readonly consoleLog: ConsoleLogItem[] = [];
+  readonly allPromises = new Map<number, PromiseAdminData>;
   debugFlagsOverrides: DebugFlags[] = [{}];
 
   constructor(title: string, metadata: CodeContextMetadata = {}) {
@@ -141,6 +143,9 @@ export class CodeContext extends EventSource<CodeContextEvents> implements Async
     });
     this.storage.clear();
     this.closed = true;
+
+    if (debugFlags.retainers)
+      showDanglingPromises(this);
   }
 
   [Symbol.asyncDispose]() {
@@ -209,5 +214,150 @@ export function getActiveCodeContexts(): ActiveCodeContext[] {
   return retval;
 }
 
+
+const promiseAdminSymbol = Symbol("wh-retainers-admin");
+type Resource = object & { [promiseAdminSymbol]?: PromiseAdminData };
+type PromiseAdminData = {
+  promiseAsyncId: number;
+  triggerAsyncId: number;
+  trace: Error | undefined;
+  fulfilled: boolean;
+  attached: boolean;
+  parentAdminData: PromiseAdminData | undefined;
+};
+
+let hook: async_hooks.AsyncHook | undefined;
+registerDebugConfigChangedCallback(initHookIfNeeded);
+
+/** Promise this promise won't throw and that its async behaviours are safe (no DB queries etc) */
+export function markPromiseSafe(promise: Promise<unknown>) {
+  const ctxt = getCodeContext();
+  const asyncId = (promise as Resource)[promiseAdminSymbol]?.promiseAsyncId;
+  if (asyncId) {
+    const adminData = ctxt.allPromises.get(asyncId);
+    if (adminData) {
+      adminData.attached = true;
+      adminData.parentAdminData = undefined;
+      ctxt.allPromises.delete(asyncId);
+    }
+  }
+}
+
+let inHook = false;
+function initHookIfNeeded() {
+  if (debugFlags.retainers && !hook) {
+    hook = async_hooks.createHook({
+      init(asyncId, type, triggerAsyncId, resource: Resource) {
+        // We're using promise.then inside this function to detect promise fulfillment, so we need to avoid recursion
+        if (inHook)
+          return;
+        inHook = true;
+
+        // Get the promise admin data from the parent async resource
+        const parentAdminData = (async_hooks.executionAsyncResource() as Resource)[promiseAdminSymbol];
+        resource[promiseAdminSymbol] = parentAdminData;
+        if (type === "PROMISE") {
+          // New promise! Create a new admin data object and store it in the promise
+          //process.stdout.write(`Promise created: asyncId:${asyncId} triggerAsyncId:${triggerAsyncId} parentPromise:${parentAdminData?.promiseAsyncId}\n`);
+          const adminData = { promiseAsyncId: asyncId, triggerAsyncId, parentAdminData, trace: new Error, fulfilled: false, attached: false };
+          resource[promiseAdminSymbol] = adminData;
+
+          const ctxt = getCodeContext();
+
+          // When the promise is resolved or rejected, mark it as fulfilled (rejected promises that aren't caught are caught by the unhandled rejection handlers)
+          const promise = resource as Promise<unknown>;
+          const handler = () => {
+            adminData.fulfilled = true;
+            // Remove the parentAdminData. We lose the parent info here we could use to eliminate false positives, but it's probably not worth the memory cost
+            adminData.parentAdminData = undefined;
+            ctxt.allPromises.delete(asyncId);
+          };
+          promise.then(handler, handler);
+
+          // If the triggerAsyncId is a promise, assume this is a .then on the promise and mark the parent promise as attached
+          const triggerPromise = ctxt.allPromises.get(triggerAsyncId);
+          if (triggerPromise) {
+            triggerPromise.attached = true;
+            // Remove the parentAdminData. We lose the parent info here we could use to eliminate false positives, but it's probably not worth the memory cost
+            triggerPromise.parentAdminData = undefined;
+            ctxt.allPromises.delete(triggerAsyncId);
+          }
+          ctxt.allPromises.set(asyncId, adminData);
+        }
+        inHook = false;
+      },
+      before(asyncId) {
+        /* called for promises when they have been fulfilled. Needed to eliminate some false positives (you would guess that the promise.then
+           done in the init hook would be enough, but it's not) */
+        //process.stdout.write(`before: asyncId:${asyncId}\n`);
+        const ctxt = getCodeContext();
+        const adminData = ctxt.allPromises.get(asyncId);
+        if (adminData) {
+          adminData.attached = true;
+          // Remove the parentAdminData. We lose the parent info here we could use to eliminate false positives, but it's probably not worth the memory cost
+          adminData.parentAdminData = undefined;
+          ctxt.allPromises.delete(asyncId);
+        }
+      }
+    }).enable();
+  } else if (hook && !debugFlags.retainers) {
+    hook.disable();
+    hook = undefined;
+  }
+}
+
+function showDanglingPromises(context: CodeContext) {
+  if (context.allPromises.size) {
+    /* debug code to show all pending+unattached promises
+    function abbrevTrace(e: Error) {
+      const trace = cutPromiseInitHookTracePart(e.stack || "");
+      return trace?.split("\n").slice(1).map(s => s.replace(/^ *at( *)/, "").replace(/^([^ ]*).*\/([^)]*)(.*)/g, "$2 ($1)")).join(",") ?? "";
+    }
+
+    function getAsyncIds(promise: PromiseAdminData): number[] {
+      const ids = [];
+      for (let adminData: typeof promise | undefined = promise; adminData; adminData = adminData.parentAdminData)
+        ids.push(adminData.promiseAsyncId);
+      return ids;
+    }
+
+    console.table([...this.allPromises.values()].map(c => ({
+      ...c,
+      parentFulfilled: c.parentAdminData?.fulfilled,
+      parentAdminData: c.parentAdminData ? "set" : undefined,
+      triggerIds: getAsyncIds(c),
+      trace: abbrevTrace(c.trace!).substring(0, 60)
+    })));
+    */
+
+    // Show only the first promise of a parent asyncId (and check parentAdminData recursively shown asyncIds)
+    const shownParentAsyncIds = new Set<number>();
+    promiseLoop:
+    for (const adminData of context.allPromises.values()) {
+      if (!adminData.fulfilled && !adminData.attached && (adminData.parentAdminData?.fulfilled ?? true)) {
+        for (let testAdminData: typeof adminData | undefined = adminData; testAdminData; testAdminData = testAdminData.parentAdminData) {
+          if (shownParentAsyncIds.has(testAdminData.promiseAsyncId))
+            continue promiseLoop;
+        }
+
+        if (adminData.parentAdminData)
+          shownParentAsyncIds.add(adminData.parentAdminData.promiseAsyncId);
+        console.error(`Warning: promise not resolved at codeContext close, allocated:`, cutPromiseInitHookTracePart(adminData.trace?.stack));
+      }
+    }
+  }
+}
+
+function cutPromiseInitHookTracePart(trace: string | undefined) {
+  if (trace) {
+    const idx = trace.indexOf("promiseInitHook");
+    if (idx !== -1)
+      trace = "\n" + trace.substring(trace.indexOf("\n", idx) + 1);
+  }
+  return trace || "<unknown>";
+}
+
 // This library keeps important data in global variables, cannot reload
 registerAsNonReloadableLibrary(module);
+
+initHookIfNeeded();
