d2caf2cc6a71ee20341f06e3bc7dc68e5715e2b0
Rob Hulswit <rob@webhare.nl>
2024-09-06 14:54:30 +0200

    Store Error objects when using dbgflag retainers, using async hooks to get them for all resources

--- whtree/modules/system/js/internal/debug/retainers.js
+++ whtree/modules/system/js/internal/debug/retainers.js
@@ -1,44 +1,14 @@
 /** Experimental library that helps tracing the source of unallocated resources */
 
-function getStackTrace() {
-  return (new Error).stack.toString();
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires -- low-level module, no TS/module support here
+const async_hooks = require("node:async_hooks");
 
-class TracedPromise extends Promise {
-  constructor(...args) {
-    super(...args);
-    this.traceInfo = getStackTrace();
+const hook = async_hooks.createHook({
+  init(asyncId, type, triggerAsyncId, resource) {
+    // Getting a stack trace string is very expensive (source map lookups etc), so just get an
+    // error and let the debugger get the stack trace when needed
+    resource.traceInfo = new Error;
   }
-}
+});
 
-globalThis.Promise = TracedPromise;
-
-const saveresolve = globalThis.Promise.resolve.bind(globalThis.Promise);
-const savereject = globalThis.Promise.reject.bind(globalThis.Promise);
-
-globalThis.Promise.resolve = function tracedResolve(...args) {
-  const result = saveresolve(...args);
-  result.traceInfo = getStackTrace();
-  return result;
-}
-
-globalThis.Promise.reject = function tracedReject(...args) {
-  const result = savereject(...args);
-  result.traceInfo = getStackTrace();
-  return result;
-}
-
-const savetimeout = globalThis.setTimeout;
-const saveinterval = globalThis.setInterval;
-
-globalThis.setTimeout = function setTracedTimeout(...args) {
-  const result = savetimeout(...args);
-  result.traceInfo = getStackTrace();
-  return result;
-}
-globalThis.setInterval = function setTracedInterval(...args) {
-  const result = saveinterval(...args);
-  result.traceInfo = getStackTrace();
-  return result;
-}
-
+hook.enable();
