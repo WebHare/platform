23fe60418a25a84e4e249489a3d8c1ff07450e46
Arnold Hendriks <arnold@webhare.nl>
2024-09-16 22:38:25 +0200

    [backport] fix missing servicemanager.log cleanup
    (cherry picked from commit 6591bed73ad4a2218e5de7286a67256016d75145)
    
    Co-authored-by: Arnold Hendriks <arnold@webhare.nl>

--- whtree/modules/system/scripts/tasks/executemaintenance.ts
+++ whtree/modules/system/scripts/tasks/executemaintenance.ts
@@ -1,10 +1,11 @@
 import type { PlatformDB } from "@mod-system/js/internal/generated/whdb/platform";
 import { runAccountExpiration } from "@mod-system/js/internal/userrights/accountexpiration";
-import { toFSPath } from "@webhare/services";
+import { backendConfig, toFSPath } from "@webhare/services";
+import { convertWaitPeriodToDate } from "@webhare/std";
 import { deleteRecursive } from "@webhare/system-tools";
 import { beginWork, commitWork, db } from "@webhare/whdb";
 import { listSchemas } from "@webhare/wrd";
-import { readdir } from "fs/promises";
+import { unlink, readdir } from "fs/promises";
 
 async function expireOldUsers() {
   let schemastofix = await listSchemas();
@@ -34,11 +35,31 @@ async function cleanupOldUploads() {
       await deleteRecursive(`${basedir}/${session}`, { deleteSelf: true });
 }
 
+async function rotateLogs() {
+  //TODO log rotation should be configurable in system:config. 30 days was the original hardcoded value
+  //TODO manage all log files, take over from whmanager ?
+  const logdir = backendConfig.dataroot + "log";
+  const cutoff = convertWaitPeriodToDate("-P30D");
+  for (const log of await readdir(logdir)) {
+    if (!log.startsWith("servicemanager."))
+      continue;
+
+    const datenum = parseInt(log.split(".")[1]);
+    if (!datenum || datenum < 20230000)
+      continue; //invalid date?
+
+    const date = new Date(Math.floor(datenum / 10000), Math.floor((datenum % 10000) / 100) - 1, datenum % 100);
+    if (date.getTime() < cutoff.getTime())
+      await unlink(`${logdir}/${log}`);
+  }
+}
+
 async function runMaintenance() {
   //Things that may free up space always go first in case someone runs these maintenance scripts hoping to free up space fast
   await cleanupOldSessions();
   await cleanupOldUploads();
   await expireOldUsers();
+  await rotateLogs();
 }
 
 runMaintenance();
