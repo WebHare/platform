da86fca6d4dc4463679c9f880f9c2ae3ac1e7e79
Arnold Hendriks <arnold@webhare.nl>
2024-10-21 12:25:36 +0200

    Don't test internal stacktrace

--- whtree/modules/webhare_testsuite/tests/system/nodejs/test_stacktrace.ts
+++ whtree/modules/webhare_testsuite/tests/system/nodejs/test_stacktrace.ts
@@ -1,27 +1,18 @@
-import { getCallerLocation, getCallStack } from "@mod-system/js/internal/util/stacktrace";
+import { getCallStack, getCallerLocation } from "@mod-system/js/internal/util/stacktrace";
 import * as test from "@webhare/test";
 
+function testDeeperStack() {
+  //as we only visually inspect these stacktraces and don't rely on parsing them yet, just check the source demapping
+  const generat = getCallStack(0);
+  test.eq([
+    { filename: /test_stacktrace.ts$/, line: 6, col: 19, func: "testDeeperStack" },
+    { filename: /test_stacktrace.ts$/, line: 14, col: 3, func: "testStackTrace" },
+  ], generat.slice(0, 2));
+}
 
 function testStackTrace() {
-  const native = (new Error).stack;
-  const generat = getCallStack(0);
-
-  // Our code doesn't handle `Function.executeUserEntryPoint [as runMain]` correctly, returns executeUserEntryPoint
-  const corrected_native = native!.replace(/Function\.executeUserEntryPoint[^(]*/, "executeUserEntryPoint ").replace("TracingChannel.traceSync", "traceSync");
-
-  // correct generat[0] line number so it is the same as the position of 'new Error'
-  --generat[0].line;
-
-  test.eq(
-    corrected_native.split(' at ').map(x => x.trim()).slice(1),
-    generat.map((i, idx) => {
-      const loc = `${i.filename}:${i.line}:${i.col}`;
-      return i.func && i.func !== "unknown" ? `${i.func} (${loc})` : loc;
-    }));
-
-  // See if getCallerLocation(0) returns the correct caller function name
-  const expect_funcname = corrected_native.split(' at ')[1].split(' ')[0];
-  test.eq(expect_funcname, getCallerLocation(0).func);
+  testDeeperStack();
+  test.eq("testStackTrace", getCallerLocation(0).func);
 }
 
 test.run([testStackTrace]);
