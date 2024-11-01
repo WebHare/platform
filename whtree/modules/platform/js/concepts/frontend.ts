b34a7a10b8452cbdf083e48275b73a205631c884
Arnold Hendriks <arnold@webhare.nl>
2024-11-01 21:42:25 +0100

    sidecar prep: give dev module watchAssetPack an interface

--- whtree/modules/platform/js/concepts/frontend.ts
+++ whtree/modules/platform/js/concepts/frontend.ts
@@ -3,6 +3,18 @@
 
   this file holds stuff needed to run or build the frontend */
 
+export type DevModuleInterface = {
+  watchAssetPack?: (assetpackname: string) => void;
+};
+
+/** Interface provided by the dev module if devtools are present */
+declare global {
+  //TODO We kinda overlap with mod::tollium/js/internal/debuginterface.ts ?
+  interface Window {
+    whDev?: DevModuleInterface;
+  }
+}
+
 /** Get base url for assetpacks
  * @param assetpack - assetpack name, like "platform:frontend"
  * @returns The base URL for the assetpack, starting with and ending in a slash
