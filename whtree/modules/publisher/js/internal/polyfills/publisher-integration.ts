0e37c1b21e7008c8344f81c33dc50f73e71ee6e1
Arnold Hendriks <arnold@webhare.nl>
2024-11-01 21:41:16 +0100

    sidecar prep: fix publisher-integration

--- whtree/modules/publisher/js/internal/polyfills/publisher-integration.ts
+++ whtree/modules/publisher/js/internal/polyfills/publisher-integration.ts
@@ -1,22 +1,22 @@
-/* eslint-disable */
-/// @ts-nocheck -- Bulk rename to enable TypeScript validation
-
+//@ts-ignore -- Dynamically generated based on actual interface URLs, 'faked' by esbuild. TODO use wh-generated like namespaces instead just like whdb/wrd/...
 import publicconfig from "@storage-system/js/publicconfig.json";
+import { isHTMLElement } from "@webhare/dompack";
 
-function forwardPublisherNavigation(event) {
+function forwardPublisherNavigation(event: Event) {
   /* we watch for clicks.. as we only want to update the Publisher's selection for user triggered actions (otherwise we'd
      start navigation in response to JS or internal link autoredirects) */
+  if (!isHTMLElement(event.target))
+    return;
 
-  const navaction = event.target.closest('a[href]');
+  const navaction = event.target.closest<HTMLAnchorElement>('a[href]');
   if (!navaction)
     return;
 
   const desturl = navaction.href;
-  if (desturl.split('#')[0] !== location.href.split('#')[0]) //it's actual navigation...
-  {
+  if (desturl.split('#')[0] !== location.href.split('#')[0]) { //it's actual navigation...
     for (const origin of publicconfig.interfaces) {
       try {
-        window.top.postMessage({ type: "webhare-navigation", location: desturl }, origin);
+        window.top?.postMessage({ type: "webhare-navigation", location: desturl }, origin);
       } catch (ignore) {
         //ignore crossdomain errors
       }
@@ -24,6 +24,6 @@ function forwardPublisherNavigation(event) {
   }
 }
 
-if (window !== window.top) { //if we're in an iframe, we may be running in WebHare and need to forward navigation events
+if (typeof window !== "undefined" && window !== window.top) { //if we're in an iframe, we may be running in WebHare and need to forward navigation events
   window.addEventListener("click", forwardPublisherNavigation);
 }
