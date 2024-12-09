b7708afbf4586066529768e1c3af3d309732d93d
Arnold Hendriks <arnold@webhare.nl>
2024-12-10 00:15:52 +0100

    Improve click handling based on the actual w3c input spec

--- whtree/modules/webhare_testsuite/tests/system/testfw/test_frontendtests.tsx
+++ whtree/modules/webhare_testsuite/tests/system/testfw/test_frontendtests.tsx
@@ -8,14 +8,19 @@ test.run(
       //Test straight wait
       test.eq('H2', (await test.waitForElement('h2')).nodeName);
       //Indexed wait
-      await test.throws(/Multiple matches/, () => test.waitForElement('p'));
+      await test.throws(/Multiple matches/, () => test.waitForElement('#waitforelementtests p'));
       test.eq('p1', (await test.waitForElement(['p', 0])).id);
       test.eq('p2', (await test.waitForElement(['p', 1])).id);
 
-      const waitfor_p3 = test.waitForElement(['p', 2]);
+      const waitfor_p3 = test.waitForElement(['#waitforelementtests p', 2]);
 
       await test.sleep(5);
       test.qR("#p2").after(<p id="p3">p3</p>);
       test.eq('p3', (await waitfor_p3).id);
+
+      //Clicking a label should focus the associated element
+      test.assert(!test.hasFocus(test.qR("#textinput")));
+      test.click("#textinput_label");
+      test.assert(test.hasFocus(test.qR("#textinput")));
     }
   ]);
