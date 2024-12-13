5a4c70e906f52d55b79d42516c19ddf66e7c40b2
Arnold Hendriks <arnold@webhare.nl>
2024-12-13 13:23:23 +0100

    TS fix a test, add urlParams to test.load()

--- whtree/modules/webhare_testsuite/tests/publisher-webdesign/test-abtest-frontend.ts
+++ whtree/modules/webhare_testsuite/tests/publisher-webdesign/test-abtest-frontend.ts
@@ -1,23 +1,21 @@
-/// @ts-nocheck -- Bulk rename to enable TypeScript validation
-
-import * as test from '@mod-tollium/js/testframework';
+import * as test from '@webhare/test-frontend';
 
 let testinfo;
 const testemail = Math.floor(100000000 * Math.random()) + '-testformfile-online+jstest@beta.webhare.net';
 
-test.registerTests(
+test.run(
   [
     async function () {
       testinfo = await test.invoke('mod::webhare_testsuite/lib/internal/testsite.whlib#SetupABTest');
-      await test.load(testinfo.abtestlink + "?appending");
+      await test.load(testinfo.abtestlink, { urlParams: { appending: "" } });
 
       test.eq("myabtest", test.getDoc().documentElement.dataset.experimentId);
       test.eq("B", test.getDoc().documentElement.dataset.experimentVariant);
 
-      const dynamicpageparameters = JSON.parse(test.qS("#content").dataset.dynamicpageparameters);
+      const dynamicpageparameters = JSON.parse(test.qR("#content").dataset.dynamicpageparameters!);
       test.eq(testinfo.abtestlink, dynamicpageparameters.absolutebaseurl);
       test.eq("", dynamicpageparameters.subpath);
-      test.eq("?appending", dynamicpageparameters.append);
+      test.eq("?appending=", dynamicpageparameters.append);
     },
 
     'Submit a form',
@@ -26,7 +24,7 @@ test.registerTests(
       test.fill(test.qSA('input[type=email]')[0], testemail);
       test.click(test.qSA('[type=submit]')[0]);
       test.qSA('[type=submit]')[0].click(); //attempt double submission. click() avoids modality layers
-      await test.wait('ui');
+      await test.waitForUI();
 
       // The thankyou node is now filled
       const thankyou = test.qSA('h1').filter(node => node.textContent === "Thank you!");
@@ -34,7 +32,7 @@ test.registerTests(
       test.assert(test.canClick(thankyou[0]), "Thankyou node should NOW be visible");
       test.assert(!test.canClick(test.qSA('[type=submit]')[0]), "Submit button should not be available on the thankyou page");
 
-      test.assert(thankyou[0].closest('form').dataset.whFormResultguid);
+      test.assert(thankyou[0].closest('form')?.dataset.whFormResultguid);
     },
 
     'Process mail',
