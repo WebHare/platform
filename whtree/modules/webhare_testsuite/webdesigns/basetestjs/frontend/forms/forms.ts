566d26b56bb087ed433c7a53e78c812a5f0e15d9
Arnold Hendriks <arnold@webhare.nl>
2025-01-02 14:55:23 +0100

    Switch over basetestjs  to new boilerplate

--- whtree/modules/webhare_testsuite/webdesigns/basetestjs/frontend/forms/forms.ts
+++ whtree/modules/webhare_testsuite/webdesigns/basetestjs/frontend/forms/forms.ts
@@ -1,36 +1,33 @@
-import * as dompack from "dompack";
-import * as forms from '@mod-publisher/js/forms';
-import UploadField from '@mod-publisher/js/forms/fields/upload';
-import { SplitDateField, SplitTimeField } from '@mod-publisher/js/forms/fields/splitdatetime';
-import ImgEditField from '@mod-publisher/js/forms/fields/imgedit';
-import RTDField from '@mod-publisher/js/forms/fields/rtd';
+import * as dompack from "@webhare/dompack";
+import * as forms from "@webhare/forms";
+import "@webhare/forms/styling/functional.css";  //Implement functional form styling (eg for implementing visibility)
 import * as embedvideo from '@mod-publisher/js/forms/fields/rtd/embedvideo';
 
-//import * as googleRecaptcha from "@mod-publisher/js/captcha/google-recaptcha";
-
 //Load neutral styling (optional, but you'll need to supply your own styling for some of the fields below if you skip this)
-import '@mod-publisher/js/forms/themes/neutral';
+import "@webhare/forms/styling/neutral.scss";
 
 import './forms.scss';
-import { floatAsyncHandler } from "@mod-webhare_testsuite/js/testhelpers";
 
-//Enable forms and our builtin validation (starting with WebHare 4.23, validate:true will be the default)
-forms.setup({ validate: true });
+//Enable publisher forms (also registers the default RPC handlers)
+forms.setupForms();
 
-
-//Replaces upload fields with a nicer and edit-supporting version
+//Setup default file and image edit. Enable only if you've also enabled them in the site profiles (or use them in custom forms)
 if (location.href.includes('rtd=1') || location.href.includes('array=1'))
-  dompack.register(".wh-form__upload", node => new UploadField(node));
+  customElements.define("wh-fileedit", forms.FileEditElement);
+customElements.define("wh-imgedit", forms.ImgEditElement);
 
-//Replaces date fields with a split version
-dompack.register(".wh-form__date", node => new SplitDateField(node));
-dompack.register(".wh-form__time", node => new SplitTimeField(node));
+//The RTD Editor. Enable only if you'll be actually using it, even unloaded it currently adds ~20KB of overhead to your 'base' CSS and JS chunks, and quite a bit of compile time
+import { RTDEditElement } from "@webhare/forms-rtdedit";
+import { floatAsyncHandler } from "@mod-webhare_testsuite/js/testhelpers";
+customElements.define("wh-rtdedit", RTDEditElement);
 
-//Enable the imgedit and/or rtd fields:
-dompack.register(".wh-form__imgedit", node => new ImgEditField(node));
-dompack.register(".wh-form__rtd", node => new RTDField(node, {
-  onInsertVideo: location.href.includes('video=1') ? floatAsyncHandler(embedvideo.insertVideo) : undefined
-}));
+if (location.href.includes('video=1')) {  //TODO cleaner, use event handlers? RTD already has some action-eventhandler stuff though..
+  dompack.register<RTDEditElement>("wh-rtdedit", node => node.onInsertVideo = floatAsyncHandler(embedvideo.insertVideo));
+}
 
-//enable the line below AND the googleRecaptcha import if you want to use this recaptcha. you'll also need to enable it in the site profile
-//googleRecaptcha.setupGoogleRecaptcha();
+//Replaces date/time fields
+dompack.register(".wh-form__date", node => new forms.DateField(node));
+dompack.register(".wh-form__time", node => new forms.TimeField(node));
+
+//Enable to support google recaptcha you want to use this recaptcha. you'll also need to enable it in the site profile
+// forms.setupGoogleRecaptcha();
