6612e73700cffe294a3a7ecad9eae94bc7d96804
Arnold Hendriks <arnold@webhare.nl>
2025-01-10 12:13:18 +0100

    [backport] Turn imgcache into a separate service as it needs frequent restarting

---
+++ whtree/modules/webhare_testsuite/scripts/tests/load/unifiedcache.ts
@@ -0,0 +1,46 @@
+/* Load test the image cache */
+
+import * as test from "@webhare/test-backend";
+import { run } from "@webhare/cli";
+import { backendConfig } from "@webhare/services";
+import { rmSync } from "node:fs";
+
+run({
+  main: async function () {
+    //clean up img cache first
+    rmSync(backendConfig.dataroot + "caches/platform/uc", { recursive: true, force: true });
+
+    const testsitejs = await test.getTestSiteJS();
+    const snowbeagle = await testsitejs.openFile("photoalbum/snowbeagle.jpg");
+
+    const promises = [];
+    const totals = { numErrors: 0, numSuccess: 0 };
+
+    for (let size = 1000; size < 2000; ++size) {
+      //FIXME why can't I set baseurl ?
+      const webpBeagle = snowbeagle.data.toResized({ method: "scale", format: "image/webp", width: size, height: size });
+      const avifBeagle = snowbeagle.data.toResized({ method: "scale", format: "image/avif", width: size, height: size });
+
+      promises.push(fetch(new URL(webpBeagle.link, backendConfig.backendURL)).then(async (response) => {
+        if (!response.ok)
+          throw new Error(`HTTP error on the ${size}x${size} webp: ${response.status}`);
+
+        return response.arrayBuffer();
+      }).then(() => { ++totals.numSuccess; }, () => { ++totals.numErrors; }));
+
+      promises.push(fetch(new URL(avifBeagle.link, backendConfig.backendURL)).then(async (response) => {
+        if (!response.ok)
+          throw new Error(`HTTP error on the ${size}x${size} avif: ${response.status}`);
+
+        return response.arrayBuffer();
+      }).then(() => { ++totals.numSuccess; }, () => { ++totals.numErrors; }));
+    }
+
+    const start = Date.now();
+    const timer = setInterval(() => {
+      console.log(totals, `${Math.floor(totals.numSuccess / ((Date.now() - start) / 60000))} per minute`);
+    }, 5000);
+    await Promise.all(promises);
+    clearInterval(timer);
+  }
+});
