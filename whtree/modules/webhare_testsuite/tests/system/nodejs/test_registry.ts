8954b4210bd31a906bdc64ec5c6473f2e806f3d4
Arnold Hendriks <arnold@webhare.nl>
2025-02-04 11:57:28 +0100

    fix registry import

--- whtree/modules/webhare_testsuite/tests/system/nodejs/test_registry.ts
+++ whtree/modules/webhare_testsuite/tests/system/nodejs/test_registry.ts
@@ -1,4 +1,4 @@
-import { PlatformDB } from "@mod-platform/generated/whdb/platform";
+import type { PlatformDB } from "@mod-platform/generated/whdb/platform";
 import { loadlib } from "@webhare/harescript";
 import { readRegistryKey, writeRegistryKey, getRegistryKeyEventMasks, WebHareBlob, readRegistryNode } from "@webhare/services";
 import { deleteRegistryKey, deleteRegistryNode, readRegistryKeysByMask } from "@webhare/services/src/registry";
