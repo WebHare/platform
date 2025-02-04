d5fa1fbc9be8269e645e5488db61660d2038f2a4
Arnold Hendriks <arnold@webhare.nl>
2025-02-04 14:27:20 +0100

    Keep invokes/callJS inside a context to prevent work leaks from breaking the entire invoke service

--- whtree/modules/platform/js/nodeservices/calljs.ts
+++ whtree/modules/platform/js/nodeservices/calljs.ts
@@ -1,11 +1,13 @@
 /* The invoke service implements CallJS if invoked from native HareScript */
 
 import { BackendServiceConnection, loadJSFunction } from "@webhare/services";
+import { CodeContext } from "@webhare/services/src/codecontexts";
 
 class CallJSService extends BackendServiceConnection {
   async invoke(lib: string, name: string, args: unknown[]) {
+    await using context = new CodeContext("CallJSService", { lib, name });
     const func = await loadJSFunction<(...args: unknown[]) => unknown>(`${lib}#${name}`);
-    return await func(...args);
+    return await context.run(() => func(...args));
   }
 }
 
