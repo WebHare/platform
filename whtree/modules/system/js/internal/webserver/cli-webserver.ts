38426c16066e91e02cab0022fc2238203ea30ee1
Arnold Hendriks <arnold@webhare.nl>
2025-03-31 13:43:22 +0200

    Fix CLI webserver to workaround mac binding permission issues

--- whtree/modules/system/js/internal/webserver/cli-webserver.ts
+++ whtree/modules/system/js/internal/webserver/cli-webserver.ts
@@ -6,38 +6,41 @@
 import * as webserver from "./webserver";
 import type { Configuration } from "./webconfig";
 import { loadlib } from "@webhare/harescript";
-import { program } from 'commander';
+import { run } from "@webhare/cli";
 
-async function main() {
-  program
-    .name('cli-webserver')
-    .option('--rescue <portnumber>', 'Open only a rescue server at the specified port');
+run({
+  description: "WebHare Webserver",
+  options: {
+    "rescueport": { description: "Open only a rescue server at the specified port" },
+  },
+  async main({ opts }) {
+    const config = await loadlib("mod::system/lib/internal/webserver/config.whlib").DownloadWebserverConfig() as Configuration;
 
-  program.parse();
-  const rescueport = program.opts().rescue as string | null;
-  const config = await loadlib("mod::system/lib/internal/webserver/config.whlib").DownloadWebserverConfig() as Configuration;
+    // FIXME Remove or cleanup this dev workaround. To be able to listen to 80/443 ports on mac, we drop the IP binding from 127.0.0.1
+    if (process.platform === "darwin")
+      for (const port of config.ports)
+        if (port.ip === "127.0.0.1" && (port.port === 80 || port.port === 443))
+          port.ip = "";
 
-  //Remove the HS trusted port from our bindlist - that one needs to be held by the HS webserver
-  const trustedportidx = config.ports.findIndex(_ => _.id === -6 /*whwebserverconfig_hstrustedportid*/);
-  if (trustedportidx >= 0)
-    config.ports.splice(trustedportidx, 1);
+    //Remove the HS trusted port from our bindlist - that one needs to be held by the HS webserver
+    const trustedportidx = config.ports.findIndex(_ => _.id === -6 /*whwebserverconfig_hstrustedportid*/);
+    if (trustedportidx >= 0)
+      config.ports.splice(trustedportidx, 1);
 
+    if (opts.rescueport) {
+      const parts = opts.rescueport.match(/^(.*:)?(\d+)$/);
+      if (!parts)
+        throw new Error(`Invalid --rescue specification: ${opts.rescueport}`);
+      const portnumber = parseInt(parts[2]!);
+      if (portnumber < 1 || portnumber > 65535)
+        throw new Error(`Invalid listening port ${portnumber}`);
 
-  if (rescueport) {
-    const parts = rescueport.match(/^(.*:)?(\d+)$/);
-    if (!parts)
-      throw new Error(`Invalid --rescue specification: ${rescueport}`);
-    const portnumber = parseInt(parts[2]!);
-    if (portnumber < 1 || portnumber > 65535)
-      throw new Error(`Invalid listening port ${portnumber}`);
+      config.ports = config.ports.filter(_ => _.id === -4); //keeps only the original 13679 rescueport
+      config.ports[0].port = portnumber;
+      config.ports[0].ip = parts[1] || "127.0.0.1";
+      console.log(`Opening rescue interface on http://${config.ports[0].ip}:${config.ports[0].port}/`);
+    }
 
-    config.ports = config.ports.filter(_ => _.id === -4); //keeps only the original 13679 rescueport
-    config.ports[0].port = portnumber;
-    config.ports[0].ip = parts[1] || "127.0.0.1";
-    console.log(`Opening rescue interface on http://${config.ports[0].ip}:${config.ports[0].port}/`);
+    await webserver.launch(config);
   }
-
-  await webserver.launch(config);
-}
-
-void main();
+});
