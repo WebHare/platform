928d6670cc8fb714b095f67e9286c629ff02aa32
Arnold Hendriks <arnold@webhare.nl>
2025-04-08 13:15:53 +0200

    Replace wh:ts/public-config with public config file

--- whtree/modules/system/js/internal/generation/gen_misc_ts.ts
+++ whtree/modules/system/js/internal/generation/gen_misc_ts.ts
@@ -1,22 +1,35 @@
-import { generatorBanner, type FileToUpdate } from "./shared";
+import type { FileToUpdate } from "./shared";
 import { listWebServers } from "@mod-platform/js/configure/webservers";
 
+export type CMSConfig = {
+  interfaceServers: string[];
+};
 
-export async function generatePublicConfig(): Promise<string> {
-  const interfaceServers = await listWebServers();
-  return `${generatorBanner}
-
-export const interfaceServers = ${JSON.stringify(interfaceServers.filter(s => s.isInterface).map(s => s.baseURL))};
-`;
+export async function generateCMSConfig(): Promise<string> {
+  const config: CMSConfig = {
+    interfaceServers: (await listWebServers()).filter(s => s.isInterface).map(s => s.baseURL)
+  };
+  return JSON.stringify(config) + '\n';
 }
 
 export async function listMiscTS(mods: string[]): Promise<FileToUpdate[]> {
+  return [];
+  // { we currently have no 'misc TS' but they're likely to reappear? keeping listMiscTS for now
+  //   path: `config/public-config.json`,
+  //   module: "dummy-installed",
+  //   type: "ts",
+  //   generator: (/*context: GenerateContext*/) => generatePublicConfig()
+  // }
+}
+
+
+export async function listPublicConfig(): Promise<FileToUpdate[]> {
   return [
     { //FIXME unlike public-config, ensure we regenerate whenever an interface webserver changes
-      path: `ts/public-config.ts`, //included by all non builtin assetpacks
+      path: `public/cms.json`, //public publisher/cms configuration - hosted on https://my.webhare.dev/.wh/ea/config/cms.json with a 15 second cache policy
       module: "dummy-installed",
-      type: "ts",
-      generator: (/*context: GenerateContext*/) => generatePublicConfig()
+      type: "public",
+      generator: (/*context: GenerateContext*/) => generateCMSConfig()
     }
   ];
 }
