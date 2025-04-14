0cf2053fb626ddcdd067378c50725792ef4a48bd
Arnold Hendriks <arnold@webhare.nl>
2025-04-14 14:47:26 +0200

    backport promise support for addDocEventListener

--- whtree/jssdk/dompack/dompack.ts
+++ whtree/jssdk/dompack/dompack.ts
@@ -5,8 +5,8 @@ declare module "@webhare/dompack" {
 /* This is our public api */
 export { flagUIBusy, setupBusyModal } from './impl/busy';
 export type { UIBusyLock, BusyModalEvent } from './impl/busy';
-export { dispatchCustomEvent, dispatchDomEvent, fireModifiedEvents, changeValue, stop, addDocEventListener, EventListenerSet } from '../../modules/system/js/dompack/src/events';
-export type { AddDocEventListenerOptions, DocEvent, DomEventOptions } from '../../modules/system/js/dompack/src/events';
+export { dispatchCustomEvent, dispatchDomEvent, fireModifiedEvents, changeValue, stop, addDocEventListener } from '../../modules/system/js/dompack/src/events';
+export type { DocEvent, DomEventOptions } from '../../modules/system/js/dompack/src/events';
 export { qS, qR, qSA, isFormControl, isElement, isHTMLElement, getRelativeBounds } from './impl/tree';
 export type { FormControlElement, Rect } from './impl/tree';
 export { isDomReady, onDomReady } from '../../modules/system/js/dompack/src/tree';
