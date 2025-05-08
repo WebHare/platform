8e209a34cb42539b035e667dd2a381a7dfdc3156
Arnold Hendriks <arnold@webhare.nl>
2025-05-08 10:25:32 +0200

    fixes to twohare test scripts

--- whtree/modules/webhare_testsuite/scripts/whcommands/twoharetests.sh
+++ whtree/modules/webhare_testsuite/scripts/whcommands/twoharetests.sh
@@ -4,7 +4,7 @@
 
 die()
 {
-  echo $1
+  echo "$1"
   exit 1
 }
 die_help()
@@ -30,25 +30,34 @@ HERE
   exit 1
 }
 
+OUTPUTDIR="/tmp/output"
+
 if [ -n "$TESTENV_CONTAINER1" ]; then
+  OUTPUTDIR="/output"
   TH_EXEC1="docker exec $TESTENV_CONTAINER1 wh"
-  TH_IP1=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $TESTENV_CONTAINER1)
+  TH_IP1=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$TESTENV_CONTAINER1")
   TH_WEBINTERFACE1="http://$TH_IP1"
 fi
 if [ -n "$TESTENV_CONTAINER2" ]; then
   TH_EXEC2="docker exec $TESTENV_CONTAINER2 wh"
-  TH_IP2=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $TESTENV_CONTAINER2)
+  TH_IP2=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$TESTENV_CONTAINER2")
   TH_WEBINTERFACE2="http://$TH_IP2"
 fi
 
-[ -n "$TH_EXEC1" -a -n "$TH_WEBINTERFACE1" ] || die_help "missing config for container 1"
-[ -n "$TH_EXEC2" -a -n "$TH_WEBINTERFACE2" ] || die_help "missing config for container 2"
+if [ -n "$TH_EXEC1" ] && [ -z "$TH_WEBINTERFACE1" ]; then
+  die_help "missing config for container 1"
+fi
+if [ -n "$TH_EXEC2" ] && [ -z "$TH_WEBINTERFACE2" ]; then
+  die_help "missing config for container 2"
+fi
 
 # Make sure the testserver knows it's supposed to be listening here (testfw configures in virtualhosting and assumes you want 127.0.0.1, and TH_WEBINTERFACE2 will often be 172.0.2.2)
-$TH_EXEC2 webserver addbackend $TH_WEBINTERFACE2
+$TH_EXEC2 webserver addbackend "$TH_WEBINTERFACE2"
 
 #$TH_EXEC1 run mod::webhare_testsuite/tests/system/twohare/prepare1.whscr
-$TH_EXEC1 registry set webhare_testsuite.tests.secondhareinterface $TH_WEBINTERFACE2 &&
-  $TH_EXEC2 run mod::webhare_testsuite/tests/system/twohare/prepare-server2.whscr &&
-  $TH_EXEC1 runtest --outputdir /output system.twohare.test_peerserver ||
+# debugging may require adding --showbrowser --keepopen to wh runtest
+if ! $TH_EXEC1 registry set webhare_testsuite.tests.secondhareinterface "$TH_WEBINTERFACE2" ||
+   ! $TH_EXEC2 run mod::webhare_testsuite/tests/system/twohare/prepare-server2.whscr ||
+   ! $TH_EXEC1 runtest --outputdir "$OUTPUTDIR" system.twohare.test_peerserver;then
   die "tests failed"
+fi
