d2d76d77cdfcdd9e4988e30249ef4b1f82d72acb
Arnold Hendriks <arnold@webhare.nl>
2025-05-08 12:51:59 +0200

    Resolve stack for puppeteer console logs

--- whtree/modules/platform/js/testing/foregroundrunner.ts
+++ whtree/modules/platform/js/testing/foregroundrunner.ts
@@ -5,7 +5,9 @@ import { broadcast, subscribeToEventStream } from "@webhare/services";
 import { generateRandomId, sleep } from "@webhare/std";
 import { storeDiskFile } from "@webhare/system-tools";
 import type { KeyboardModifierOptions } from "dompack/testframework/keyboard";
-import type { KeyInput } from "puppeteer";
+import { originalPositionFor, TraceMap } from "@jridgewell/trace-mapping";
+import { readFileSync } from "node:fs";
+import { getBundleOutputPath } from "../assetpacks/support";
 
 let puppeteer: Puppeteer.Browser | null = null;
 
@@ -56,17 +58,59 @@ type TestDevtoolsEvent = {
   };
 };
 
+type LogEntry = {
+  when: Date;
+  msg: string;
+  trace?: Puppeteer.ConsoleMessageLocation[];
+};
+
 let debug = false;
 let keepopen = false;
+let alwayslog = false;
 
-async function reportFailure(testinfo: Test, message: string, log: string[], page: Puppeteer.Page | null) {
+async function remapLocation(loc: Puppeteer.ConsoleMessageLocation): Promise<Puppeteer.ConsoleMessageLocation> {
+  if (!(loc.lineNumber && loc.columnNumber && loc.url))
+    return loc;
+
+  const packagename = loc.url?.split("/.wh/ea/ap/")[1];
+  if (!packagename)
+    return loc;
+
+  const bundlename = packagename.split("/")[0].replace('.', ':');
+  const filename = packagename.split("/").slice(1).join("/");
+  const mapFile = getBundleOutputPath(bundlename) + filename + ".map";
+
+  let source = '';
+  //TODO cache the parsed sources
+  try {
+    source = readFileSync(mapFile, 'utf8');
+  } catch {
+  }
+
+  if (!source)
+    return loc;
+
+  const tracer = new TraceMap(source);
+  const traced = originalPositionFor(tracer, { line: loc.lineNumber, column: loc.columnNumber });
+  return traced.line && traced.source ? { lineNumber: traced.line, columnNumber: traced.column, url: traced.source } : loc;
+}
+
+async function reportFailure(testinfo: Test, message: string | null, log: LogEntry[], page: Puppeteer.Page | null) {
   const outbase = `/tmp/jstests/${testinfo.testname}.${new Date().toISOString()}`;
-  const logfile = `${outbase}.log`;
+  const logfile = `${outbase}.txt`;
+  const jsonlogfile = `${outbase}.json`;
   const screenshotfile = `${outbase}.png`;
 
-  console.log(`Test ${testinfo.testname} failed: ${message}`);
+  console.log(`Test ${testinfo.testname} ${message ? `failed: ${message}` : "completed successfully"}`);
   console.log(`Writing log to ${logfile}`);
-  await storeDiskFile(logfile, log.join("\n"), { mkdir: true });
+
+  for (const line of log)
+    if (line.trace)
+      for (const [idx, item] of line.trace.entries())
+        line.trace[idx] = await remapLocation(item);
+
+  await storeDiskFile(logfile, log.map(_ => `[${_.when.toISOString()}] ${_.msg}`).join("\n") + "\n", { mkdir: true });
+  await storeDiskFile(jsonlogfile, log.map(_ => JSON.stringify(_)).join("\n") + "\n", { mkdir: true });
 
   if (page) {
     console.log(`Writing screenshot to ${screenshotfile}`);
@@ -77,11 +121,13 @@ async function reportFailure(testinfo: Test, message: string, log: string[], pag
 export async function init(options: {
   debug: boolean;
   keepopen: boolean;
+  alwayslog: boolean;
   showbrowser: boolean;
 }) {
 
   debug = Boolean(options.debug);
   keepopen = Boolean(options.keepopen);
+  alwayslog = Boolean(options.alwayslog);
   puppeteer = await launchPuppeteer({
     headless: !(options.showbrowser || options.keepopen),
     defaultViewport: { height: 1024, width: 1280, deviceScaleFactor: 1 }
@@ -97,19 +143,20 @@ export async function runTest(test: Test) {
   // Begin listening for reports before setting the URL
   const events = subscribeToEventStream(["system:jstest.report." + reportid, "system:jstest.devtools." + reportid]);
   const timeout = sleep(test.timeout).then(() => "timeout");
-  const log: string[] = [];
+  const log: LogEntry[] = [];
   let page;
 
-  function addLogLine(text: string) {
-    text = `[${new Date().toISOString()}] ${text}`;
+  function addLogLine(msg: string, trace?: Puppeteer.ConsoleMessageLocation[]) {
+    const when = new Date();
     if (debug)
-      console.log(text);
-    log.push(text);
+      console.log(`[${when.toISOString()}] ${msg}`);
+
+    log.push({ when, msg, trace: structuredClone(trace) }); //copy trace just in case, we were receiving incorrect coordinates which appear to belong to later messages - is its being updated?
   }
 
   try {
     page = await puppeteer!.newPage();
-    page.on('console', message => addLogLine(`${message.type().substring(0, 3).toUpperCase()} ${message.text()}`));
+    page.on('console', message => addLogLine(`${message.type().substring(0, 3).toUpperCase()} ${message.text()}`, message.stackTrace()));
     page.on('pageerror', message => addLogLine(`PageError: ${message}`));
     // perhaps add these behind a debug flag? they're very noise especially for loading (data:) images
     // page.on('request', req => addLogLine(`Request: ${req.url()}`));
@@ -132,14 +179,14 @@ export async function runTest(test: Test) {
         const keymap = { shiftkey: "Shift", ctrlkey: "Control", altkey: "Alt", metakey: "Meta" };
         for (const [flag, name] of Object.entries(keymap))
           if (nextEvent.value.data.options?.[flag as keyof KeyboardModifierOptions])
-            await page.keyboard.down(name as KeyInput);
+            await page.keyboard.down(name as Puppeteer.KeyInput);
 
         for (const key of nextEvent.value.data.keys)
-          await page.keyboard.press(key as KeyInput);
+          await page.keyboard.press(key as Puppeteer.KeyInput);
 
         for (const [flag, name] of Object.entries(keymap).reverse())
           if (nextEvent.value.data.options?.[flag as keyof KeyboardModifierOptions])
-            await page.keyboard.up(name as KeyInput);
+            await page.keyboard.up(name as Puppeteer.KeyInput);
 
         broadcast("system:jstest.devresponse." + reportid);
         continue;
@@ -156,6 +203,8 @@ export async function runTest(test: Test) {
       return { status: "fail" };
     }
 
+    if (alwayslog)
+      await reportFailure(test, null, log, page);
     await page.close();
     page = null;
     return { status: "ok" };
