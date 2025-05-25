f9c7ce3b6655696d3656eb7bbeeda209c2bb6ea8
Arnold Hendriks <arnold@webhare.nl>
2025-05-25 23:00:34 +0200

    Cleanup testredirect without behavioral changes

--- whtree/modules/webhare_testsuite/tests/publisher/forms/testredirect.ts
+++ whtree/modules/webhare_testsuite/tests/publisher/forms/testredirect.ts
@@ -1,47 +1,25 @@
-import * as test from '@mod-system/js/wh/testframework';
+import * as test from '@webhare/test-frontend';
 
 test.runTests(
   [
-    async () => {
+    "test redirect",
+    async function () {
       await test.invoke('mod::webhare_testsuite/lib/internal/testsite.whlib#SnoozeRateLimits');
       await test.load(test.getTestSiteRoot() + 'testpages/formtest/?redirect=1&');
+      test.click('button[type=submit]');
+      await test.waitForLoad();
+      test.eq("about:blank", test.getWin().location.href);
     },
 
-    {
-      test: () => {
-        test.click('button[type=submit]');
-      },
-      waits: ['pageload']
-    },
-
-    {
-      test: () => {
-        test.eq("about:blank", test.getWin().location.href);
-      }
-    },
-
-    async () => {
+    "test with delay",
+    async function () {
       await test.invoke('mod::webhare_testsuite/lib/internal/testsite.whlib#SnoozeRateLimits');
       await test.load(test.getTestSiteRoot() + 'testpages/formtest/?redirectdelay=1&');
-    },
+      test.click('button[type=submit]');
+      await test.waitForUI();
 
-    {
-      test: () => {
-        test.click('button[type=submit]');
-      },
-      waits: ['ui']
-    },
-
-    {
-      test: () => {
-        test.assert(test.qS(".wh-form__page--visible [data-wh-form-group-for='outtro']"), "outtro text should be displayed before redirecting");
-      },
-      waits: ['pageload']
-    },
-
-    {
-      test: () => {
-        test.eq("about:blank", test.getWin().location.href);
-      }
+      test.assert(test.qS(".wh-form__page--visible [data-wh-form-group-for='outtro']"), "outtro text should be displayed before redirecting");
+      await test.waitForLoad();
+      test.eq("about:blank", test.getWin().location.href);
     }
   ]);
