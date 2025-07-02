714b5c3620e530159a42c276473a5b231350d979
Rob Hulswit <rob@webhare.nl>
2025-07-02 14:22:20 +0200

    Reinstate old wrd types and schema modules, with deprecated exports needed for compat

---
+++ whtree/modules/wrd/js/internal/schema.ts
@@ -0,0 +1,8 @@
+import type { WRDSingleQueryBuilder } from "@webhare/wrd/src/schema";
+
+// Temporary exports for WH 5.7 compat
+
+export type {
+  /** @deprecated Try to avoid for now, if really necessary use \@webhare/wrd/src/schema instead */
+  WRDSingleQueryBuilder
+};
