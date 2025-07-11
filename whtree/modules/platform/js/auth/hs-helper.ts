c0912ff335ac9d5454af108e5ea39309500bdc5a
Arnold Hendriks <arnold@webhare.nl>
2025-07-11 13:41:46 +0200

    [backport] Work around JWT X5T signature issues
    Just use the JS implementation we already have, this might have been an openssl issue? The PEM we were transferring was okay..
    
    # Conflicts:
    #	whtree/modules/wrd/web/endpoints/oidc.shtml

---
+++ whtree/modules/platform/js/auth/hs-helper.ts
@@ -0,0 +1,18 @@
+import { createPublicKey, type JsonWebKey } from "node:crypto";
+import jwt, { type VerifyOptions } from "jsonwebtoken";
+
+export async function verifyJWT(keys: string, token: string, clockTimestamp: Date, verifyoptions?: VerifyOptions) {
+  verifyoptions = { ...verifyoptions, clockTimestamp: clockTimestamp.getTime() / 1000 };
+  const decoded = jwt.decode(token, { complete: true });
+  const key = (JSON.parse(keys).keys as Array<JsonWebKey & { kid: string }>).find(k => k.kid === decoded?.header.kid);
+  if (!key)
+    return null;
+
+  const jwk = createPublicKey({ key: key, format: 'jwk' });
+  const data = jwt.verify(token, jwk, verifyoptions);
+
+  if (typeof data !== "object")
+    return null;
+
+  return data;
+}
