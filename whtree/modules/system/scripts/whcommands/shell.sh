92af0d7500fcee0090994f1b5369548e86a50cfe
Arnold Hendriks <arnold@webhare.nl>
2025-08-01 13:44:22 +0200

    Fix hardcoded path triggered by 'wh shell'

--- whtree/modules/system/scripts/whcommands/shell.sh
+++ whtree/modules/system/scripts/whcommands/shell.sh
@@ -1,8 +1,9 @@
+# shellcheck shell=bash
 # short: Launch a shell (similar to what Docker would do)
 
 if [ -n "$WEBHARE_CHECKEDOUT_TO" ]; then
   export HOME=$WEBHARE_DATAROOT/root
 fi
 
-mkdir -p $WEBHARE_DATAROOT/root
-exec bash --rcfile $WEBHARE_CHECKEDOUT_TO/addons/docker-build/dropins/etc/bash.bashrc -O histappend -O cmdhist
+mkdir -p "$WEBHARE_DATAROOT/root"
+exec bash --rcfile "$WEBHARE_CHECKEDOUT_TO/addons/docker-build/dropins/etc/bash.bashrc" -O histappend -O cmdhist
