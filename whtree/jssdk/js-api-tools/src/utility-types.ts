bb2bbddfa6b7fbe3ca24e140190b7bf3da083620
Rob Hulswit <rob@webhare.nl>
2025-09-24 09:22:02 +0200

    Move DisallowExtraPropsRecursive to js-api-tools

--- whtree/jssdk/js-api-tools/src/utility-types.ts
+++ whtree/jssdk/js-api-tools/src/utility-types.ts
@@ -14,3 +14,18 @@ export type RecursivePartial<T> = T extends Array<infer U> ? Array<RecursivePart
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- using any is needed for this type definition
 export type PromisifyFunctionReturnType<T extends (...a: any) => any> = (...a: Parameters<T>) => ReturnType<T> extends (1 & ReturnType<T>) ? Promise<any> : ReturnType<T> extends Promise<any> ? ReturnType<T> : Promise<ReturnType<T>>;
 // The above definition use `extends (1 & ReturnType<T>)` to detect 'any'
+
+/** Given a type Contract and a type Actual that extends Contract, returns Contract with
+ * properties that are added in Actual with type never. If used in a function signature like
+ * this: `<Actual extends Contact>(param: Actual & DisallowExtraPropsRecursive<Actual, Contract>`,
+ * this has the effect that extra properties in Actual will be disallowed compile-time.
+ */
+export type DisallowExtraPropsRecursive<Actual extends Contract, Contract> = Contract extends unknown[]
+  ? (Actual extends unknown[]
+    ? Array<DisallowExtraPropsRecursive<Actual[number], Contract[number]>>
+    : Contract)
+  : (Contract extends object
+    ? (Actual extends Contract
+      ? { [K in keyof Contract]: K extends keyof Actual ? DisallowExtraPropsRecursive<Actual[K], Contract[K]> : Contract[K] } & { [K in Exclude<keyof Actual, keyof Contract>]: never }
+      : Contract)
+    : Contract);
