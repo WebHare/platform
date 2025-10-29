ac64895d763bb9f279711027d3476cfd1efb4dc0
Rob Hulswit <rob@webhare.nl>
2025-10-29 13:50:40 +0100

    Auto downgrade npm from 11.6.[12] to 11.6.0 in docker

--- whtree/modules/platform/scripts/bootstrap/finalize-webhare.sh
+++ whtree/modules/platform/scripts/bootstrap/finalize-webhare.sh
@@ -38,8 +38,14 @@ logWithTime "Finalizing WebHare in $WEBHARE_DATAROOT"
 getwebhareversion
 
 if [[ "$(npm -v)" =~ ^11.6.[12]$ ]]; then
-  echo "Buggy version of npm installed! Buggy versions are 11.6.1 and 11.6.2, current version: $(npm -v). Please downgrade to 11.6.0 (npm -g i npm@11.6.0)"
-  die "Buggy npm version installed"
+  echo "Buggy version of npm installed! Buggy versions are 11.6.1 and 11.6.2, current version: $(npm -v)"
+  if [ -z "$WEBHARE_IN_DOCKER" ]; then
+    echo "Please downgrade to 11.6.0 (npm -g i npm@11.6.0)"
+    die "Buggy npm version installed"
+  else
+    echo "Downgrading NPM to 11.6.0 (npm -g i npm@11.6.0)"
+    npm -g i npm@11.6.0
+  fi
 fi
 
 # Install node_modules
