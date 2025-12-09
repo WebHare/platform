4eafe1c7e692d5044d610b43aef672afa32cba7f
Kris Breuker <kris@webhare.nl>
2025-12-09 17:25:53 +0100

    Prevent scheduling multiple renewal tasks

--- whtree/modules/system/scripts/tasks/renewcertbot.ts
+++ whtree/modules/system/scripts/tasks/renewcertbot.ts
@@ -1,6 +1,6 @@
 import { listStoredKeyPairs } from "@mod-platform/js/webserver/keymgmt";
 import { run } from "@webhare/cli";
-import { scheduleTask } from "@webhare/services";
+import { describeTask, listTasks, scheduleTask } from "@webhare/services";
 import { toSnakeCase } from "@webhare/std";
 import { beginWork, commitWork } from "@webhare/whdb";
 
@@ -18,16 +18,28 @@ run({
       if (!cert.name.startsWith("certbot-"))
         continue;
 
+      await beginWork();
+
+      // Don't schedule if a task for this certificate is already running/pending
+      let alreadyScheduled = false;
+      for (const task of await listTasks("platform:requestcertificate", { onlyPending: true })) {
+        const data = await describeTask(task.id);
+        if (data?.data && typeof data.data === "object" && "certificate_id" in data.data && data.data.certificate_id === cert.id) {
+          if (options.debug)
+            console.log(`Request task ${task.id} already scheduled for '${cert.name}'`);
+          alreadyScheduled = true;
+        }
+      }
+
       if (options.debug)
         console.log(`Requesting for '${cert.name}'`);
-
-      await beginWork();
-      await scheduleTask("platform:requestcertificate", toSnakeCase({
-        certificateId: cert.id,
-        isRenewal: !options.force,
-        staging: options.staging,
-        debug: options.debug,
-      }));
+      if (!alreadyScheduled)
+        await scheduleTask("platform:requestcertificate", toSnakeCase({
+          certificateId: cert.id,
+          isRenewal: !options.force,
+          staging: options.staging,
+          debug: options.debug,
+        }));
       await commitWork();
     }
   }
