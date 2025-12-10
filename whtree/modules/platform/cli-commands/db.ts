6905adcaf9aea173360f5f8c77725582afa4c075
Rob Hulswit <rob@webhare.nl>
2025-12-10 10:41:20 +0100

    Add wh db showusage

--- whtree/modules/platform/cli-commands/db.ts
+++ whtree/modules/platform/cli-commands/db.ts
@@ -1,10 +1,12 @@
 import { readPlatformConf } from "@mod-platform/js/configure/axioms";
-import { intOption, run } from "@webhare/cli";
+import { enumOption, intOption, run } from "@webhare/cli";
 import { toFSPath } from "@webhare/services";
 import { pick } from "@webhare/std";
-import { runInWork } from "@webhare/whdb";
+import { db, runInWork, sql } from "@webhare/whdb";
+import { getSchemas, getTables } from "@webhare/whdb/src/introspection";
 import { cancelBackend, getCurrentPGVersion, getDatabaseMonitorInfo, getDatabaseSequences, restartAllSequences } from "@webhare/whdb/src/management";
 import { spawnSync } from "child_process";
+import type { SelectQueryBuilder } from "kysely";
 
 run({
   description: "Database management commands",
@@ -117,6 +119,131 @@ run({
         } else {
           console.error(opts.dryRun ? "Upgrade failed" : "Upgrade failed - you may need to restart WebHare to exit read-only mode! (wh service relaunch)");
           process.exit(result.status || 1);
+        }
+      }
+    },
+    showusage: {
+      description: "Show database usage and size per table (deduplicates blobs only within tables)",
+      options: {
+        format: {
+          type: enumOption(["table", "json"]),
+          description: "Output format",
+          default: "table",
+        }
+      },
+      async main({ opts }) {
+        const results: {
+          tableName: string;
+          totalsize: number;
+          tablesize: number;
+          indexessize: number;
+          liverowestimate: number;
+          blobSize: number;
+          dedupBlobSize: number;
+          blobCount: number;
+        }[] = [];
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const blobType = await db<any>()
+          .selectFrom("pg_catalog.pg_type as t")
+          .innerJoin("pg_catalog.pg_namespace as n", "t.typnamespace", "n.oid")
+          .innerJoin("pg_catalog.pg_proc as p", "t.typinput", "p.oid")
+          .select(["t.oid", "t.typname"])
+          .where("n.nspname", "=", "webhare_internal")
+          .where("t.typname", "=", "webhare_blob")
+          .where("p.proname", "=", "record_in")
+          .executeTakeFirst();
+
+
+        for (const schema of await getSchemas()) {
+          for (const table of await getTables(schema.schemaName)) {
+            const size = await db()
+              .selectNoFrom([
+                sql<number>`pg_total_relation_size(${table.oid})`.as("totalsize"),
+                sql<number>`pg_relation_size(${table.oid})`.as("tablesize"),
+                sql<number>`pg_indexes_size(${table.oid})`.as("indexessize"),
+                sql<number>`pg_stat_get_live_tuples(${table.oid})`.as("liverowestimate"),
+              ])
+              .executeTakeFirst();
+
+            if (size) {
+              let blobSize = 0, dedupBlobSize = 0, blobCount = 0;
+              if (blobType) {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                const attrs = await db<any>()
+                  .selectFrom("pg_attribute")
+                  .select(["attname", "atttypid"])
+                  .where("attrelid", "=", table.oid)
+                  .where("attnum", ">", 0)
+                  .where("attisdropped", "=", false)
+                  .where("atttypid", "=", blobType.oid)
+                  .execute();
+
+                type AttrQuery = SelectQueryBuilder<{ t: unknown }, "t", {
+                  data: { id: string; size: number };
+                }>;
+
+                const attrUnionQuery = attrs.reduce((dbQuery: AttrQuery | null, attr) => {
+                  const attrQuery = db()
+                    .selectFrom(sql.table(schema.schemaName + "." + table.tableName).as("t"))
+                    .select(sql.ref<{ id: string; size: number }>(attr.attname).as("data"))
+                    .where(sql.ref(attr.attname), `is not`, null);
+                  return dbQuery ? dbQuery.unionAll(attrQuery) : attrQuery;
+                }, null);
+
+                if (attrUnionQuery) {
+                  // Deduplicated blobs by id
+                  const allBlobsQuery = db()
+                    .selectFrom(attrUnionQuery.as("allblobs"))
+                    .select([
+                      sql<number>`SUM((data).size)`.as("size"),
+                      sql<number>`MIN((data).size)`.as("dedupsize"),
+                      sql<number>`COUNT(*)`.as("count"),
+                    ])
+                    .groupBy(sql`(data).id`);
+
+                  // And get the sums
+                  const blobSizeRec = await db()
+                    .selectFrom(allBlobsQuery.as("sub"))
+                    .select([
+                      sql<number>`SUM(size)::int8`.as("totalsize"),
+                      sql<number>`SUM(dedupsize)::int8`.as("dedupsize"),
+                      sql<number>`COUNT(*)`.as("blobcount"),
+                    ])
+                    .executeTakeFirst();
+
+                  if (blobSizeRec) {
+                    size.totalsize += blobSizeRec.dedupsize || 0;
+                    blobSize += blobSizeRec.totalsize || 0;
+                    dedupBlobSize += blobSizeRec.dedupsize || 0;
+                    blobCount += blobSizeRec.blobcount || 0;
+                  }
+                }
+              }
+
+              results.push({
+                tableName: `${schema.schemaName}.${table.tableName}`,
+                ...size,
+                blobSize,
+                dedupBlobSize,
+                blobCount,
+              });
+            }
+          }
+        }
+        results.sort((a, b) => b.totalsize - a.totalsize);
+        if (opts.format === "json")
+          console.log(JSON.stringify(results, null, 2));
+        else {
+          console.table(results.map(r => ({
+            tableName: r.tableName,
+            totalSizeMB: (r.totalsize / (1024 * 1024)).toFixed(3) + " MB",
+            tableSizeMB: (r.tablesize / (1024 * 1024)).toFixed(3) + " MB",
+            indexesSizeMB: (r.indexessize / (1024 * 1024)).toFixed(3) + " MB",
+            blobSizeMB: (r.blobSize / (1024 * 1024)).toFixed(3) + " MB",
+            dedupBlobSizeMB: (r.dedupBlobSize / (1024 * 1024)).toFixed(3) + " MB",
+            liveRowEstimate: r.liverowestimate
+          })));
         }
       }
     }
