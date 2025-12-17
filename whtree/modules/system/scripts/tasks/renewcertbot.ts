659e02ef13286203c74f801595b542f5c682e339
Arnold Hendriks <arnold@webhare.nl>
2025-12-17 09:37:59 +0100

    Add --dry-run, don't say "requesting" for tasks we're not creating due to alreadsyScheduled

--- whtree/modules/system/scripts/tasks/renewcertbot.ts
+++ whtree/modules/system/scripts/tasks/renewcertbot.ts
@@ -8,39 +8,40 @@ run({
   flags: {
     staging: { default: false, description: "use the staging server, if available for the certificate provider" },
     force: { default: false, description: "force a certificate update, even if it's not yet up for renewal" },
-    debug: { default: false },
+    debug: "Debug output",
+    "dry-run": "Perform a dry run without scheduling tasks",
   },
   main: async ({ opts: options }) => {
+    const debug = options.debug || options.dryRun;
     const allCerts = await listStoredKeyPairs();
+    await beginWork();
 
     for (const cert of allCerts) {
       // In scope?
       if (!cert.name.startsWith("certbot-"))
         continue;
 
-      await beginWork();
 
       // Don't schedule if a task for this certificate is already running/pending
-      let alreadyScheduled = false;
       for (const task of await listTasks("platform:requestcertificate", { onlyPending: true })) {
         const data = await describeTask(task.id);
         if (data?.data && typeof data.data === "object" && "certificate_id" in data.data && data.data.certificate_id === cert.id) {
-          if (options.debug)
+          if (debug)
             console.log(`Request task ${task.id} already scheduled for '${cert.name}'`);
-          alreadyScheduled = true;
+          continue;
         }
       }
 
-      if (options.debug)
+      if (debug)
         console.log(`Requesting for '${cert.name}'`);
-      if (!alreadyScheduled)
-        await scheduleTask("platform:requestcertificate", toSnakeCase({
-          certificateId: cert.id,
-          isRenewal: !options.force,
-          staging: options.staging,
-          debug: options.debug,
-        }));
-      await commitWork();
+
+      await scheduleTask("platform:requestcertificate", toSnakeCase({
+        certificateId: cert.id,
+        isRenewal: !options.force,
+        staging: options.staging,
+        debug: options.debug,
+      }));
     }
+    await commitWork();
   }
 });
