b83771d17a143da5f5e3548d8ef968c81a8422ee
Arnold Hendriks <arnold@webhare.nl>
2026-02-03 14:47:14 +0100

    Implement wrd import

--- whtree/modules/webhare_testsuite/tests/wrd/nodejs/test_wrd_cli.ts
+++ whtree/modules/webhare_testsuite/tests/wrd/nodejs/test_wrd_cli.ts
@@ -1,12 +1,13 @@
 import { WRDSchema } from "@webhare/wrd";
 import * as test from "@mod-webhare_testsuite/js/wts-backend";
 import { createWRDTestSchema, testSchemaTag, type CustomExtensions } from "@mod-webhare_testsuite/js/wrd/testhelpers";
-import type { Combine } from "@webhare/wrd/src/types";
+import type { Combine, WRDInsertable } from "@webhare/wrd/src/types";
 import type { WRD_TestschemaSchemaType } from "@mod-platform/generated/wrd/webhare";
 import { buildRTD, ResourceDescriptor } from "@webhare/services";
 import { beginWork, commitWork } from "@webhare/whdb";
 import { spawnSync } from "node:child_process";
 import type { RTDExport } from "@webhare/services/src/richdocument";
+import { omit } from "@webhare/std";
 
 
 async function testWRDCli() { //  tests
@@ -28,10 +29,15 @@ async function testWRDCli() { //  tests
 
   await commitWork();
 
+  const clonableAttributes = (await wrdschema.getType("wrdPerson").listAttributes()).
+    filter(attr => !["wrdId", "wrdGuid", "wrdType", "wrdTitle", "wrdFullName", "wrdContactEmail"].includes(attr.tag)).
+    map(_ => _.tag) as Array<keyof WRDInsertable<TestSchemaType["wrdPerson"]>>;
+  const original = await wrdschema.getFields("wrdPerson", newPerson, clonableAttributes);
+
   for (const resourceMode of ["fetch", "base64"] as const) {
     const options = resourceMode === "fetch" ? [] : ["--resources", "base64"];
 
-    const exportResult = spawnSync("wh", ["wrd", "export", ...options, String(newPerson)], { shell: false, encoding: "utf-8", stdio: ['inherit', 'pipe', 'inherit'] });
+    const exportResult = spawnSync("wh", ["wrd", "export", "--json", ...options, String(newPerson)], { shell: false, encoding: "utf-8", stdio: ['inherit', 'pipe', 'inherit'] });
     test.eq(0, exportResult.status, "wrd export command should succeed");
     const exportData = JSON.parse(exportResult.stdout);
     const richieFirstParagraph = (exportData.richie as RTDExport)[0];
@@ -45,6 +51,14 @@ async function testWRDCli() { //  tests
       test.assert(!("base64" in imageNode.image.data));
     }
 
+    const insertPerson = omit(exportData, ["wrdId", "wrdGuid"]);
+    insertPerson.wrdContactEmail = `import-${resourceMode}@beta.webhare.net`;
+    const insertResult = spawnSync("wh", ["wrd", "insert", "--json", testSchemaTag, "wrdPerson", "-"], { shell: false, encoding: "utf-8", input: JSON.stringify(insertPerson), stdio: ['pipe', 'pipe', 'inherit'] });
+    test.eq(0, insertResult.status, "wrd insert command should succeed");
+    const insertOutput = JSON.parse(insertResult.stdout);
+
+    const inserted = await wrdschema.getFields("wrdPerson", insertOutput.wrdId, clonableAttributes);
+    test.eq(original, inserted);
   }
 }
 
