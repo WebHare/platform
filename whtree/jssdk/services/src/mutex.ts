9ca2d3697bd7b82724b1c2f56aef56aff2129012
Arnold Hendriks <arnold@webhare.nl>
2026-02-04 12:09:54 +0100

    new: add wh bridge get-locks
    # Conflicts:
    #	whtree/modules/system/scripts/whcommands/bridge.ts

--- whtree/jssdk/services/src/mutex.ts
+++ whtree/jssdk/services/src/mutex.ts
@@ -36,7 +36,34 @@ interface UnlockResponse {
   status: string;
 }
 
-export type MutexManagerLinkType = IPCLinkType<InitTask | LockTask | UnlockTask, InitResponse | LockResponse | UnlockResponse>;
+interface StatusTask {
+  task: "status";
+}
+
+interface StatusResponse {
+  status: "ok";
+  links: Array<{
+    id: number;
+    clientname: string;
+    groupid: string;
+    connectedsince: Date;
+    waiting: boolean;
+    waituntil: Date;
+    waitfor: string;
+    waittrace: unknown[];
+  }>;
+  mutexes: Array<{
+    mutexname: string;
+    taken: boolean;
+    takenby: number;
+    waiters: number;
+    takentrace: unknown[];
+  }>;
+  log: unknown[];
+  logtraces: boolean;
+}
+
+export type MutexManagerLinkType = IPCLinkType<InitTask | LockTask | UnlockTask | StatusTask, InitResponse | LockResponse | UnlockResponse | StatusResponse>;
 export type MutexManagerLink = MutexManagerLinkType["ConnectEndPoint"];
 
 class Mutex {
--- whtree/modules/platform/js/bridge/tools.ts
+++ whtree/modules/platform/js/bridge/tools.ts
@@ -1,5 +1,6 @@
 import bridge from "@mod-system/js/internal/whmanager/bridge";
 import { DebugMgrClientLinkRequestType, type DebugMgrClientLink } from "@mod-system/js/internal/whmanager/debug";
+import type { MutexManagerLinkType } from "@webhare/services/src/mutex";
 import { throwError } from "@webhare/std";
 
 export async function getInspectorURL(process: string): Promise<string> {
@@ -10,4 +11,12 @@ export async function getInspectorURL(process: string): Promise<string> {
     processid: process
   });
   return inspectorinfo?.url || throwError("Could not get an inspector URL");
+}
+
+export async function listLocks() {
+  using link = bridge.connect<MutexManagerLinkType>("system:mutexmanager", { global: true });
+  await link.activate();
+
+  const statusrequest = await link.doRequest({ task: "status" });
+  return statusrequest;
 }
