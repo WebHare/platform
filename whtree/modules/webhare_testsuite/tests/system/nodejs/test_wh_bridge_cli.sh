b6f8b7a4d9e83aecdf0efbf758527eb5fcfeadcb
Arnold Hendriks <arnold@webhare.nl>
2026-02-05 13:58:19 +0100

    update bridge cli test

--- whtree/modules/webhare_testsuite/tests/system/nodejs/test_wh_bridge_cli.sh
+++ whtree/modules/webhare_testsuite/tests/system/nodejs/test_wh_bridge_cli.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# TODO port to JS (See the various other cli tests), rename to reflect wh bridge->debug
 # wh runtest system.nodejs.test_wh_bridge_cli
 source "${WEBHARE_DIR}/lib/wh-functions.sh"
 
-testEq "1" "$(wh bridge connections --json|jq -r 'map(select (.name|endswith("whcommands/bridge.ts"))) | length')"
+testEq "1" "$(wh debug list-processes --json |jq -r 'map(select (.name|endswith("cli-commands/debug.ts"))) | length')"
 exit 0
