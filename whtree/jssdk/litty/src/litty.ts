7d28c002f37dda68de20eb2ae02fe9100ca7e4d9
Arnold Hendriks <arnold@webhare.nl>
2026-02-06 20:53:36 +0100

    new: litty - witty-inspired template literals

---
+++ whtree/jssdk/litty/src/litty.ts
@@ -0,0 +1,48 @@
+// This gets TypeScript to refer to us by our @webhare/... name in auto imports:
+declare module "@webhare/litty" {
+}
+
+import { encodeString } from "@webhare/std";
+
+type LittyValue = string | number | Litty | Litty[];
+
+export type Litty = {
+  strings: string[];
+  values: LittyValue[];
+};
+
+/** Build a Litty template literal, automatically encoding inserted strings
+ * #example litty`<p>${content}</p>`
+ */
+export function litty(strings: TemplateStringsArray, ...values: LittyValue[]): Litty {
+  return { strings: [...strings], values };
+}
+
+/** Wrap a string as raw content which shouldn't be encoded
+ * #example litty`<p>${content}</p>`
+ */
+export function rawLitty(str: string): Litty {
+  return { strings: [str], values: [] };
+}
+
+/** Convert a litty template to a string */
+export async function littyToString(lit: Litty): Promise<string> {
+  //We're async to have room to support async template parts in the future
+  let result = "";
+  for (let i = 0; i < lit.strings.length; i++) {
+    result += lit.strings[i];
+    if (i < lit.values.length) {
+      const item = lit.values[i];
+      if (typeof item === "number") {
+        result += item.toString();
+      } else if (typeof item === "string") {
+        result += encodeString(item, 'html');
+      } else if (Array.isArray(item)) {
+        result += (await Array.fromAsync(item.map(littyToString))).join("");
+      } else {
+        result += await littyToString(item);
+      }
+    }
+  }
+  return result;
+}
---
+++ whtree/modules/webhare_testsuite/tests/system/nodejs/test_litty.ts
@@ -0,0 +1,15 @@
+import { litty, littyToString } from "@webhare/litty";
+import * as test from "@webhare/test";
+
+async function testLitty() {
+  const templ = litty`<div>${"<test>"}</div>`;
+  test.eq(`<div>&lt;test&gt;</div>`, await littyToString(templ));
+
+  const templ2 = litty`<div>${templ}</div>`;
+  test.eq(`<div><div>&lt;test&gt;</div></div>`, await littyToString(templ2));
+
+  const templ3 = litty`<div>${[templ, templ2]}</div>`;
+  test.eq(`<div><div>&lt;test&gt;</div><div><div>&lt;test&gt;</div></div></div>`, await littyToString(templ3));
+}
+
+test.run([testLitty]);
