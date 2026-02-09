f8d051410862e79ad5b78154522dc259872b08f7
Arnold Hendriks <arnold@webhare.nl>
2026-02-09 16:57:55 +0100

    cleanup photoalbums
    deprecated: photoalbum folders are deprecated, special handling (such as resizing images) has have been removed

--- whtree/modules/webhare_testsuite/tests/publisher-webdesign/test_gallery.ts
+++ whtree/modules/webhare_testsuite/tests/publisher-webdesign/test_gallery.ts
@@ -3,6 +3,7 @@ import * as test from '@webhare/test-frontend';
 test.runTests(
   [
     async function () {
+      //we test whether the photoalbum rendered, nothing else
       await test.waitForPublishCompletion(test.getTestSiteRoot() + 'photoalbum/');
       await test.load(test.getTestSiteRoot() + 'photoalbum/');
 
@@ -13,49 +14,5 @@ test.runTests(
       test.eq(150, images[0].querySelector("img")?.height);
       test.eq(113, images[1].querySelector("img")?.width);
       test.eq(150, images[1].querySelector("img")?.height);
-
-      test.click(test.qSA('.wh-gallery__image')[1]);
-
-      let modalcontainer = test.qR(".wh-gallery-modal");
-
-      await test.wait(() => test.qSA(".wh-gallery-modal__image--selected").length === 1);
-      let currentimage = test.qR(".wh-gallery-modal__image--selected");
-      test.assert(test.canClick(currentimage));
-
-      test.assert(!modalcontainer.classList.contains("wh-gallery-modal--firstslide"));
-      test.assert(!modalcontainer.classList.contains("wh-gallery-modal--lastslide"));
-      //unfortunately the current positioning method can't actually ensure we round to the proper coordinates due to the padding-top trick
-      test.eq(450, test.getRoundedBoundingClientRect(currentimage).width);
-      test.eq(600, test.getRoundedBoundingClientRect(currentimage).height);
-
-      await test.pressKey('ArrowRight');
-
-      await test.wait(() => test.qSA(".wh-gallery-modal__image--selected").length === 1);
-      currentimage = test.qR(".wh-gallery-modal__image--selected");
-      test.assert(test.canClick(currentimage));
-      test.assert(!modalcontainer.classList.contains("wh-gallery-modal--firstslide"));
-      test.eq(428, test.getRoundedBoundingClientRect(currentimage).width);
-      test.eq(284, test.getRoundedBoundingClientRect(currentimage).height);
-
-      await test.pressKey('Escape');
-      await test.wait(() => !test.qS(".wh-gallery-modal"));
-      test.assert(images[2].contains(test.getDoc().activeElement));
-
-      test.click(images[2]);
-      modalcontainer = await test.waitForElement(".wh-gallery-modal");
-
-      await test.pressKey('ArrowLeft');
-      await test.pressKey('ArrowLeft');
-
-      await test.wait(() => test.qSA(".wh-gallery-modal__image--selected").length === 1);
-      currentimage = test.qR(".wh-gallery-modal__image--selected");
-      test.eq(600, test.getRoundedBoundingClientRect(currentimage).width);
-      test.eq(450, test.getRoundedBoundingClientRect(currentimage).height);
-      test.assert(modalcontainer.classList.contains("wh-gallery-modal--firstslide"));
-      test.assert(!modalcontainer.classList.contains("wh-gallery-modal--lastslide"));
-
-      for (let i = 0; i < 6; i++)
-        await test.pressKey('ArrowRight');
-      test.assert(modalcontainer.classList.contains("wh-gallery-modal--lastslide"));
     }
   ]);
