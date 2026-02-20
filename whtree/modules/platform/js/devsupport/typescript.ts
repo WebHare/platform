137e93bc202843c11a5eba59767e48febdaabff3
Rob Hulswit <rob@webhare.nl>
2026-02-20 10:46:10 +0100

    Get incremental compiling to work in typescript check, add check for circular loadlibs

--- whtree/modules/platform/js/devsupport/typescript.ts
+++ whtree/modules/platform/js/devsupport/typescript.ts
@@ -43,6 +43,134 @@ function mapDiagnostics(basedir: string, diagnostics: ts.Diagnostic[]): Validati
   return issues;
 }
 
+function getCircularImports(compiler: ts.CompilerHost, tsOptions: ts.CompilerOptions, program: ts.Program) {
+  const analysisResult: {
+    file: string;
+    cycle: string[];
+  }[] = [];
+
+  /// References of a file
+  type Ref = { name: string; from: Set<Ref>; to: Set<Ref> };
+
+  /// List of all files
+  const refs = new Map<string, Ref>;
+
+  /// Get/create a ref for a file name
+  function getRef(name: string): Ref {
+    let ref = refs.get(name);
+    if (!ref) {
+      ref = { name: name, from: new Set, to: new Set };
+      refs.set(name, ref);
+    }
+    return ref;
+  }
+
+  for (const sourcefile of program.getSourceFiles()) {
+    // Ignore all node_modules files
+    if (sourcefile.fileName.indexOf("node_modules") !== -1)
+      continue;
+
+    const fromref = getRef(sourcefile.fileName);
+
+    // Walk over all 'important' nodes (skips over stuff like naked semicolons)
+    ts.forEachChild(sourcefile, node => {
+      // ignore type-only imports/exports
+      if (ts.isImportDeclaration(node) && node.importClause?.isTypeOnly)
+        return;
+      if (ts.isExportDeclaration(node) && node.isTypeOnly)
+        return;
+
+      if (ts.isImportDeclaration(node) || ts.isExportDeclaration(node)) {
+        if (node.moduleSpecifier && ts.isStringLiteral(node.moduleSpecifier)) {
+          const resolved = ts.resolveModuleName(node.moduleSpecifier.text, sourcefile.fileName, tsOptions, compiler);
+
+          if (resolved.resolvedModule) {
+            // ignore loads of node_modules
+            if (resolved.resolvedModule.resolvedFileName.indexOf("node_modules") !== -1)
+              return;
+            const toref = getRef(resolved.resolvedModule.resolvedFileName);
+            fromref.to.add(toref);
+            toref.from.add(fromref);
+          }
+        }
+      }
+    });
+  }
+
+  // Remove all files that aren't references or don't have references themselves
+  function removeLeaves() {
+    for (; ;) {
+      let anychanged = false;
+      for (const v of refs.values()) {
+        if (!v.to.size) {
+          anychanged = true;
+          refs.delete(v.name);
+          for (const f of v.from.values())
+            f.to.delete(v);
+        }
+        if (!v.from.size) {
+          anychanged = true;
+          refs.delete(v.name);
+          for (const f of v.to.values())
+            f.from.delete(v);
+        }
+      }
+      if (!anychanged)
+        break;
+    }
+  }
+
+  // Removes a single reference
+  function removeEdge(from: Ref, to: Ref) {
+    if (!from.to.has(to))
+      throw new Error(`edge does not exist`);
+    to.from.delete(from);
+    from.to.delete(to);
+  }
+
+  // Finds the shortest reference involving a file
+  function findShortestCycle(root: Ref) {
+    // breadth-first search, keeping how we got to a file
+    const directions = new Map<Ref, Ref>;
+    const todo = [root];
+    for (const elt of todo) {
+      for (const c of elt.to) {
+        //console.log(elt.name, "->", c.name);
+        if (c === root) {
+          const result: Ref[] = [];
+          for (let iter: Ref | undefined = elt; iter; iter = directions.get(iter))
+            result.unshift(iter);
+          return result;
+
+          // shortest cycle
+        } else if (!directions.get(c)) {
+          directions.set(c, elt);
+          todo.push(c);
+        }
+      }
+    }
+    return [];
+  }
+
+  removeLeaves();
+
+  while (refs.size) {
+    for (const todo of refs.values()) {
+      const cycle = findShortestCycle(todo);
+      if (!cycle.length)
+        continue;
+
+      analysisResult.push({ file: todo.name, cycle: cycle.map(r => r.name) });
+
+      removeEdge(cycle[0], cycle[1] ?? cycle[0]);
+      removeLeaves();
+      break;
+    }
+
+  }
+  return analysisResult;
+}
+
 export async function checkUsingTSC(modulename: string, options?: { files: string[] }): Promise<ValidationMessageWithType[]> {
   const isPlatform = modulename === "jssdk" || whconstant_builtinmodules.includes(modulename);
   const projectRoot = isPlatform ? backendConfig.installationRoot : backendConfig.module[modulename].root;
@@ -53,11 +181,26 @@ export async function checkUsingTSC(modulename: string, options?: { files: strin
   const tsbuildinfodir = backendConfig.dataRoot + "caches/platform/typescript";
   await mkdir(tsbuildinfodir, { recursive: true });
 
-  const { program, diagnostics } = await prepTSHost(projectFile, { setFiles: options?.files, ignoreErrors: true, tsBuildInfoFile: path.join(tsbuildinfodir, modulename + ".tsbuildinfo") });
-  if (diagnostics.length)
-    return mapDiagnostics(projectRoot, diagnostics);
+  let { host, tsOptions, program, diagnostics, builderProgram } = await prepTSHost(projectFile, { setFiles: options?.files, ignoreErrors: true, tsBuildInfoFile: path.join(tsbuildinfodir, modulename + ".tsbuildinfo") });
 
-  const emitResult = program.emit();
-  const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
-  return mapDiagnostics(projectRoot, allDiagnostics);
+  const emitResult = builderProgram.emit();
+  diagnostics = diagnostics.concat(emitResult.diagnostics);
+  const circularImports = getCircularImports(host, tsOptions, program);
+
+  return [
+    ...mapDiagnostics(projectRoot, diagnostics),
+    ...circularImports.map(cycle => {
+      // Report only cycles that involve the checked module
+      if (!cycle.cycle.some(c => c.startsWith(projectRoot)))
+        return null;
+      return {
+        type: "hint" as const,
+        resourcename: toResourcePath(cycle.file, { keepUnmatched: true }),
+        line: 0,
+        col: 0,
+        message: `Circular import detected: ${cycle.cycle.map(file => toResourcePath(file, { keepUnmatched: true })).join(" => ")}`,
+        source: "tsc-circular-import"
+      };
+    })
+  ].filter(_ => _ !== null).filter(_ => !_.message.startsWith("No inputs were found"));
 }
