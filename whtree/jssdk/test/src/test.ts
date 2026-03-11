cf5d50427e7cd4f4a04b35a16835fda75e533bba
Arnold Hendriks <arnold@webhare.nl>
2026-03-11 14:48:12 +0100

    new: Add extractSchemaOrgData to testframework
    this also required making the internal elements() more flexible to understand both browser and xmldom/xmldom DOMs

--- whtree/jssdk/test/src/test.ts
+++ whtree/jssdk/test/src/test.ts
@@ -4,7 +4,7 @@ import { runTests } from './testrunner';
 declare module "@webhare/test" {
 }
 
-export { triggerGarbageCollection, getActiveGenerators } from './testsupport';
+export { triggerGarbageCollection, getActiveGenerators, extractSchemaOrgData } from './testsupport';
 export { runTests } from './testrunner';
 
 export {
