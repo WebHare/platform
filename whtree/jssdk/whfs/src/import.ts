a469052120fa312facf08a6bc23834a982bedf64
Arnold Hendriks <arnold@webhare.nl>
2026-03-15 22:08:14 +0100

    Refactor import, we need a prepared map of what we'll import to do future id allocation

--- whtree/jssdk/whfs/src/import.ts
+++ whtree/jssdk/whfs/src/import.ts
@@ -1,5 +1,5 @@
 import YAML from 'yaml';
-import { compareProperties, toCLocaleLowercase } from "@webhare/std";
+import { appendToArray, compareProperties, toCLocaleLowercase } from "@webhare/std";
 import { listDirectory } from "@webhare/system-tools";
 import type { UnpackArchiveResult } from "@webhare/zip";
 import { stat } from "fs/promises";
@@ -9,6 +9,7 @@ import { ResourceDescriptor } from "@webhare/services";
 import { openAsBlob } from "fs";
 import { getType } from './describe';
 import { whfsType, type ExportedInstance } from '@webhare/whfs/src/contenttypes';
+import type { CSPContentType } from './siteprofiles';
 
 export interface ImportWHFSOptions {
 }
@@ -25,6 +26,13 @@ type ImportItem = {
   name: string;
   subPath: string;
   blob: () => Promise<Blob>;
+};
+
+type CombinedImportItem = {
+  name: string;
+  subPath: string;
+  blob?: () => Promise<Blob>;
+  metadata?: () => Promise<Blob>;
 };
 
 export type ImportedVirtualMetaData = {
@@ -91,7 +99,7 @@ class ImportSession {
   items;
   folderMap;
 
-  constructor(items: ImportItem[], public targetFolder: WHFSFolder) {
+  constructor(items: CombinedImportItem[], public targetFolder: WHFSFolder) {
     items.sort(compareProperties(["subPath"])); //ensure parent folders come before their children
     this.items = new Map(items.map(item => [toCLocaleLowercase(item.subPath), item]));
     this.folderMap = new Map<string, WHFSFolder>([["", targetFolder]]); //maps source subpaths to their corresponding WHFSFolder in the target (starting with the root)
@@ -117,26 +125,31 @@ class ImportSession {
     return currentFolder;
   }
 
-  async importByMetadata(item: ImportItem) {
+  async import(item: CombinedImportItem) {
     const storeFolder = await this.ensureFolder(dirname(item.subPath));
 
     //TODO import data if type hasData and available
-    const meta = YAML.parse(await (await item.blob()).text()) as {
-      type?: string;
-      instances?: ExportedInstance[];
-    };
-    if (!meta.type) {
-      this.result.messages.push({ subPath: item.subPath, type: "error", message: `Missing type` });
-      return;
+    let meta, typeinfo: CSPContentType;
+    if (item.metadata) {
+      meta = YAML.parse(await (await item.metadata()).text()) as {
+        type?: string;
+        instances?: ExportedInstance[];
+      };
+      if (!meta.type) {
+        this.result.messages.push({ subPath: item.subPath, type: "error", message: `Missing type` });
+        return;
+      }
+
+      typeinfo = getType(meta.type)!; //we'll check for undefined:
+      if (!typeinfo) {
+        this.result.messages.push({ subPath: item.subPath, type: "error", message: `Unknown type '${meta.type}'` });
+        return;
+      }
+    } else { //no metadata, ie a raw zip import
+      typeinfo = getType("platform:filetypes.unknown")!; //FIXME type detect
     }
 
-    const typeinfo = getType(meta.type);
-    if (!typeinfo) {
-      this.result.messages.push({ subPath: item.subPath, type: "error", message: `Unknown type '${meta.type}'` });
-      return;
-    }
-
-    const objectData = meta.instances?.find(instance => instance.whfsType === "platform:virtual.objectdata")?.data;
+    const objectData = meta?.instances?.find(instance => instance.whfsType === "platform:virtual.objectdata")?.data;
     let baseMetaData: CreateFileMetadata & CreateFolderMetadata = {};
     if (objectData) {
       const resolveResult = await resolveVirtualMetaData(null, objectData);
@@ -145,35 +158,55 @@ class ImportSession {
         baseMetaData = resolveResult.data;
     }
 
-    const newName = item.name.replace(/\.whfs\.yml$/i, '');
-    const newSubPath = item.subPath.replace(/\.whfs\.yml$/i, '');
-    const exists = await storeFolder.openFileOrFolder(newName, { allowMissing: true });
+    const exists = await storeFolder.openFileOrFolder(item.name, { allowMissing: true });
     if (exists) {
       //TODO implement overwrite modes
       this.result.messages.push({ subPath: item.subPath, type: "error", message: `Cannot import '${item.name}' at '${storeFolder.whfsPath}' - a ${exists.isFolder ? "folder" : "file"} with that name already exists` });
       return;
     }
 
-    const dataFile = this.items.get(toCLocaleLowercase(newSubPath));
-    if (dataFile) {
-      baseMetaData.data = await ResourceDescriptor.fromBlob(await dataFile.blob());
-      this.items.delete(toCLocaleLowercase(newSubPath)); //mark as processed
+    if (item.blob) {
+      baseMetaData.data = await ResourceDescriptor.fromBlob(await item.blob());
     }
 
-    const newObj = await storeFolder[typeinfo.foldertype ? "createFolder" : "createFile"](newName, {
-      type: meta.type,
+    const newObj = await storeFolder[typeinfo.foldertype ? "createFolder" : "createFile"](item.name, {
+      type: typeinfo?.scopedtype,
       ...baseMetaData
     });
     if (typeinfo.foldertype)
-      this.folderMap.set("/" + toCLocaleLowercase(newSubPath), newObj as WHFSFolder);
+      this.folderMap.set("/" + toCLocaleLowercase(item.subPath), newObj as WHFSFolder);
 
-    for (const instance of meta.instances || []) {
+    for (const instance of meta?.instances || []) {
       if (instance.whfsType === "platform:virtual.objectdata")
         continue;
       const typeHandler = whfsType(instance.whfsType);
       await typeHandler.set(newObj.id, instance.data as object || {});
     }
   }
+}
+
+/** Reduce items with separate data to a list of unique entries with their metadata */
+function collapseMetadata(items: ImportItem[]): CombinedImportItem[] {
+  const itemMap = new Map<string, ImportItem>(items.map(item => [toCLocaleLowercase(item.subPath), item]));
+  const outItems: CombinedImportItem[] = [];
+
+  //first add all items that have metadta
+  for (const [subpath, item] of itemMap) {
+    if (subpath.endsWith(".whfs.yml")) {
+      const actualData = itemMap.get(subpath.slice(0, -9)); //remove .whfs.yml from filename
+      if (actualData)
+        itemMap.delete(subpath.slice(0, -9)); //mark data as processed
+      outItems.push({
+        name: item.name.replace(/\.whfs\.yml$/i, ''),
+        subPath: item.subPath.replace(/\.whfs\.yml$/i, ''),
+        metadata: item.blob,
+        blob: actualData?.blob
+      });
+      itemMap.delete(subpath); //mark metadata as processed
+    }
+  }
+  appendToArray(outItems, [...itemMap.values()]); //then add all remaining items without metadata
+  return outItems;
 }
 
 /** Import data into WHFS
@@ -193,26 +226,9 @@ export async function importIntoWHFS(source: UnpackArchiveResult | string, targe
     blob: () => openAsBlob(entry.fullPath)
   }));
 
-  const importer = new ImportSession(items, targetFolder);
-
+  const importer = new ImportSession(collapseMetadata(items), targetFolder);
   for (const item of importer.items.values()) {
-    if (!item.subPath.endsWith(".whfs.yml")) { //skip this
-      continue;
-    }
-
-    importer.items.delete(item.subPath); //mark as processed
-    await importer.importByMetadata(item);
-  }
-
-  for (const item of importer.items.values()) {
-    const storeFolder = await importer.ensureFolder(dirname(item.subPath));
-    const exists = await storeFolder.openFileOrFolder(item.name, { allowMissing: true });
-    if (exists) {
-      //TODO implement overwrite modes
-      importer.result.messages.push({ subPath: item.subPath, type: "error", message: `Cannot import '${item.name}' at '${storeFolder.whfsPath}' - a ${exists.isFolder ? "folder" : "file"} with that name already exists` });
-      continue;
-    }
-    await storeFolder.createFile(item.name, { data: await ResourceDescriptor.fromBlob(await item.blob()) });
+    await importer.import(item);
   }
 
   return importer.result;
