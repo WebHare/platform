4c98759c3f3efb7f95c5a5ec8e45abb1cd61a48b
Arnold Hendriks <arnold@webhare.nl>
2026-04-23 12:41:14 +0200

    Add promt/promptPassword

--- whtree/modules/platform/js/cli/cli-tools.ts
+++ whtree/modules/platform/js/cli/cli-tools.ts
@@ -1,5 +1,7 @@
 import { enumOption } from "@webhare/cli";
 import { openFileOrFolder, type WHFSObject } from "@webhare/whfs";
+import { kill } from "node:process";
+import { createInterface } from "node:readline";
 
 //Shared code for WebHare CLI tools
 export const commonFlags = {
@@ -10,6 +12,44 @@ export const commonFlags = {
 export const commonOptions = {
   resources: { resources: { description: "Export resources for fetch (default) or inline as base64", type: enumOption(["fetch", "base64"]), default: "fetch" } }
 } as const;
+
+export function prompt(question: string): Promise<string> {
+  process.stdin.setEncoding("utf8"); //and I guess we can just leave it at that?  can't restore original encoding anyway
+  const rl = createInterface({ input: process.stdin, output: process.stdout });
+  return new Promise(resolve => rl.question(question, answer => { rl.close(); resolve(answer); }));
+}
+
+export async function promptPassword(question: string): Promise<string> {
+  process.stdin.setEncoding("utf8"); //and I guess we can just leave it at that?  can't restore original encoding anyway
+
+  const defer = Promise.withResolvers<string>();
+  let password = "";
+  const rl = createInterface({ input: process.stdin, output: undefined, terminal: false });
+  const onData = (ch: string) => {
+    if (ch === "\n" || ch === "\r") {
+      process.stdout.write("\n");
+      defer.resolve(password);
+    } else if (ch === "\u007f" || ch === "\b") {
+      password = password.slice(0, -1);
+    } else if (ch === "\u0003") { // Ctrl+C
+      kill(process.pid, "SIGINT");
+    } else {
+      password += ch;
+    }
+  };
+
+  process.stdout.write(question);
+
+  process.stdin.setRawMode(true);
+  // process.stdin.resume();
+  process.stdin.on("data", onData);
+
+  return defer.promise.finally(() => {
+    process.stdin.setRawMode(false);
+    process.stdin.removeListener("data", onData);
+    rl.close();
+  });
+}
 
 export async function resolveWHFSPathArgument(path: string, options?: { allowRoot?: boolean }): Promise<WHFSObject> {
   return openFileOrFolder(parseInt(path) > 0 ? parseInt(path) : path, { allowHistoric: true, ...options });
