78ec6578244e47fa82d1df9a80eb94d825cfbda7
Arnold Hendriks <arnold@webhare.nl>
2026-06-26 13:40:41 +0200

    don't crash inspect if the yaml export generation throws

--- whtree/modules/publisher/tolliumapps/filemanager/inspect/inspect.ts
+++ whtree/modules/publisher/tolliumapps/filemanager/inspect/inspect.ts
@@ -4,7 +4,11 @@ import { openFileOrFolder } from "@webhare/whfs";
 import YAML from 'yaml';
 
 export async function inspectObject(id: number) {
-  const obj = await openFileOrFolder(id, { allowVersion: true, allowMissing: true });
-  const exp = obj ? await exportWHSFObject(obj!, "*", { export: true, exportFile: exportFileAsFetch }) : null;
-  return { yaml: YAML.stringify(exp) };
+  try {
+    const obj = await openFileOrFolder(id, { allowVersion: true, allowMissing: true });
+    const exp = obj ? await exportWHSFObject(obj!, "*", { export: true, exportFile: exportFileAsFetch }) : null;
+    return { yaml: YAML.stringify(exp) };
+  } catch (err) {
+    return { error: (err as Error).message };
+  }
 }
