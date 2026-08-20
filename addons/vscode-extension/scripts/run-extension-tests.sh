5f1a67ce14089ca94636b1ba9c3f0fcbd4cfb660
Arnold Hendriks <arnold@webhare.nl>
2026-08-20 10:37:05 +0200

    Fix vscode installation adding lsp-types

--- addons/vscode-extension/scripts/run-extension-tests.sh
+++ addons/vscode-extension/scripts/run-extension-tests.sh
@@ -13,5 +13,8 @@ cd "$WEBHARE_CHECKEDOUT_TO/addons/vscode-extension"
 mkdir -p node_modules/@webhare
 npm install --no-save
 
+# tried adding a relative link in package.json but that kept creating lsp-types folders inside the lsp-types folder
+npm install --no-save @webhare/lsp-types@../../whtree/jssdk/lsp-types
+
 "$WEBHARE_CHECKEDOUT_TO/whtree/node_modules/.bin/tsc"
 echo "No issues!"
