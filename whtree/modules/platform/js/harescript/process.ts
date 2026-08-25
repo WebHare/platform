f2e27ece57d0c8ba1b57663e67a88d8e15ff7c5c
Arnold Hendriks <arnold@webhare.nl>
2026-08-26 00:15:02 +0200

    old nodejs lack convertProcessSignalToExitCode - workaround

--- whtree/modules/platform/js/harescript/process.ts
+++ whtree/modules/platform/js/harescript/process.ts
@@ -1,13 +1,12 @@
 import { spawnSync } from "child_process";
-import { convertProcessSignalToExitCode } from "util";
 
 /** The TS helper for HS spawnProcessSync, hence the name. It's async but will be made sync in HareScript */
 export async function spawnProcessSync(cmd: string, args: string[]) {
   const proc = spawnSync(cmd, args, { encoding: "utf-8", stdio: ['ignore', 'pipe', 1] });
   return {
     error: proc.status !== 0,
-    //nodejs offsets signals by 128, but WebHare used to offset by 256, so just add another 128
-    exitcode: proc.status ?? (128 + convertProcessSignalToExitCode(proc.signal!)),
+    //node 24 lacks convertProcessSignalToExitCode so we'll just map signals to -1
+    exitcode: proc.status ?? -1,
     output: proc.stdout
   };
 }
