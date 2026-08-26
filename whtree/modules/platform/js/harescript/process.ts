93dcd46dd3f1177b093a9e2fa351a9ee43ca8b7e
Arnold Hendriks <arnold@webhare.nl>
2026-08-26 13:20:07 +0200

    Merge branch 'feature/spawn-fixes' into 'master'
    Feature/spawn fixes
    
    See merge request webhare-opensource/platform!3241
    
    (cherry picked from commit dacce42df2d20f5df599a158d9de7ce159b0f566)
    
    7ae94a70 [backport] add IfThenElse ternary operator
    8c19bd14 [backport] stderr merge fixes, support blob output, support stdin in spawnProcessSync
    
    Co-authored-by: WebHare Bot <systeembeheer+webharebot@webhare.nl>

--- whtree/modules/platform/js/harescript/process.ts
+++ whtree/modules/platform/js/harescript/process.ts
@@ -1,13 +1,37 @@
-import { spawnSync } from "child_process";
+import { WebHareMemoryBlob } from "@webhare/services/src/webhareblob";
+import { spawn } from "child_process";
 import { convertProcessSignalToExitCode } from "util";
 
 /** The TS helper for HS spawnProcessSync, hence the name. It's async but will be made sync in HareScript */
-export async function spawnProcessSync(cmd: string, args: string[]) {
-  const proc = spawnSync(cmd, args, { encoding: "utf-8", stdio: ['ignore', 'pipe', 1] });
+export async function spawnProcessSync(cmd: string, args: string[], options?: {
+  stdin?: string;
+  output_as_blob?: boolean;
+  merge_stderr?: boolean;
+}) {
+  const stdout: Buffer[] = [], stderr: Buffer[] = [];
+  const proc = spawn(cmd, args, { stdio: ['pipe', 'pipe', 'pipe'] });
+
+  proc.stdout.on("data", chunk => stdout.push(Buffer.from(chunk)));
+  if (options?.merge_stderr)
+    proc.stderr.on("data", chunk => stdout.push(Buffer.from(chunk)));
+  else
+    proc.stderr.on("data", chunk => stderr.push(Buffer.from(chunk)));
+
+  if (options?.stdin !== undefined)
+    proc.stdin.end(options.stdin);
+  else
+    proc.stdin.end();
+
+  const result = await new Promise<{ code: number | null; signal: NodeJS.Signals | null }>((resolve, reject) => {
+    proc.on("error", reject);
+    proc.on("close", (code, signal) => resolve({ code, signal }));
+  });
+
   return {
-    error: proc.status !== 0,
+    error: result.code !== 0,
     //nodejs offsets signals by 128, but WebHare used to offset by 256, so just add another 128
-    exitcode: proc.status ?? (128 + convertProcessSignalToExitCode(proc.signal!)),
-    output: proc.stdout
+    exitcode: result.code ?? (128 + convertProcessSignalToExitCode(result.signal!)),
+    output: options?.output_as_blob ? WebHareMemoryBlob.from(Buffer.concat(stdout)) : Buffer.concat(stdout).toString("utf-8"),
+    stderr: Buffer.concat(stderr).toString("utf-8"),
   };
 }
