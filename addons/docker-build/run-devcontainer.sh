411215a8326224b6bb018b93f9b32fca4238ab4e
Arnold Hendriks <arnold@webhare.nl>
2026-09-25 09:56:56 +0200

    verify the buildscripts

--- addons/docker-build/run-devcontainer.sh
+++ addons/docker-build/run-devcontainer.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
+# To reduce interference when sharing a wh tree, isolate currentinstall (multiple WHs will write to it) and isolate node_modules (mac&linux require different binaries)
+
 podman run \
   --rm \
   --privileged \
@@ -11,4 +13,5 @@ podman run \
   -v "$(pwd)/../..":/opt/wh \
   -v "devcontainer-currentinstall":/opt/wh/whtree/currentinstall \
   -v "devcontainer-node_modules":/opt/wh/whtree/node_modules \
-  localhost/webhare/platform:devcontainer
+  localhost/webhare/platform:devcontainer \
+  "$@"
